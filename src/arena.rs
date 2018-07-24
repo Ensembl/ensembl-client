bff47c27e8c2a81759d7cb8922f2496c6ae4e8f5
Dan Sheppard <dan@ebi.ac.uk>
2018-07-24 01:39:03 +0100

    Fixx geometry. First hack.

--- src/arena.rs
+++ src/arena.rs
@@ -6,6 +6,7 @@ use canvasutil;
 use wglraw;
 use hosc::HoscGeometry;
 use hofi::HofiGeometry;
+use fixx::FixxGeometry;
 
 use webgl_rendering_context::{
     WebGLRenderingContext as glctx,
@@ -13,8 +14,9 @@ use webgl_rendering_context::{
 };
 
 struct ArenaGeometries {
-	hosc: HoscGeometry,
+    hosc: HoscGeometry,
     hofi: HofiGeometry,
+    fixx: FixxGeometry,
 }
 
 pub struct ArenaData {
@@ -23,12 +25,12 @@ pub struct ArenaData {
 }
 
 pub struct Arena {
-	data: Rc<RefCell<ArenaData>>,
-	geom: ArenaGeometries
+    data: Rc<RefCell<ArenaData>>,
+    geom: ArenaGeometries
 }
 
 impl Arena {
-	pub fn new(selector: &str) -> Arena {
+    pub fn new(selector: &str) -> Arena {
         let canvas = canvasutil::prepare_canvas(selector);
         let ctx = wglraw::prepare_context(&canvas);
         let data = Rc::new(RefCell::new(ArenaData {
@@ -36,55 +38,60 @@ impl Arena {
             aspect: canvasutil::aspect_ratio(&canvas),
         }));
         let data_g = data.clone();
-		let arena = Arena { data, geom: ArenaGeometries {
+        let arena = Arena { data, geom: ArenaGeometries {
             hosc: HoscGeometry::new(data_g.clone()),
             hofi: HofiGeometry::new(data_g.clone()),
+            fixx: FixxGeometry::new(data_g.clone()),
         }};
-		arena
-	}
+        arena
+    }
 
     pub fn geom_hosc(&mut self,f: &mut FnMut(&mut HoscGeometry)) {
-		let g = &mut self.geom.hosc;
+        let g = &mut self.geom.hosc;
         f(g);
     }
     
     pub fn geom_hofi(&mut self,f: &mut FnMut(&mut HofiGeometry)) {
-		let g = &mut self.geom.hofi;
+        let g = &mut self.geom.hofi;
         f(g);
     }
+    
+    pub fn geom_fixx(&mut self,f: &mut FnMut(&mut FixxGeometry)) {
+        let g = &mut self.geom.fixx;
+        f(g);
+    }    
 
     pub fn populate(&mut self) {
         self.geom_hosc(&mut |g:&mut HoscGeometry| g.populate());
         self.geom_hofi(&mut |g:&mut HofiGeometry| g.populate());
+        self.geom_fixx(&mut |g:&mut FixxGeometry| g.populate());
     }
 
     pub fn animate(&mut self, stage: &Stage) {
-        // prepare geometries
-        self.geom_hosc(&mut |g:&mut HoscGeometry| g.link());
-        self.geom_hosc(&mut |g:&mut HoscGeometry| g.perspective(&stage));
-        self.geom_hosc(&mut |g:&mut HoscGeometry| g.draw());
         // prepare arena
         {
             let ctx = &self.data.borrow().ctx;
             ctx.enable(glctx::DEPTH_TEST);
             ctx.depth_func(glctx::LEQUAL);
-            ctx.clear_color(1.0,1.0,1.0,1.0);
-            ctx.clear_depth(1.0);
+        //    ctx.clear_color(1.0,1.0,1.0,1.0);
+        //    ctx.clear_depth(1.0);
         }
         // draw each geometry
-        self.geom_hofi(&mut |g:&mut HofiGeometry| g.link());
+        self.geom_hosc(&mut |g:&mut HoscGeometry| g.perspective(&stage));
+        self.geom_hosc(&mut |g:&mut HoscGeometry| g.draw());
         self.geom_hofi(&mut |g:&mut HofiGeometry| g.perspective(&stage));
         self.geom_hofi(&mut |g:&mut HofiGeometry| g.draw());
+        self.geom_fixx(&mut |g:&mut FixxGeometry| g.perspective(&stage));
+        self.geom_fixx(&mut |g:&mut FixxGeometry| g.draw());
     }
 }
 
 pub trait Geometry {
-	fn adata(&self) -> Ref<ArenaData>;
-	fn program<'a>(&'a self) -> &'a glprog;
+    fn adata(&self) -> Ref<ArenaData>;
+    fn program<'a>(&'a self) -> &'a glprog;
 
-	fn populate(&mut self);
-	fn link(&self);
-	fn draw(&self);
+    fn populate(&mut self);
+    fn draw(&self);
 
     fn perspective(&self,stage:&Stage) {
         let data = &self.adata();
@@ -95,6 +102,7 @@ pub trait Geometry {
         wglraw::set_uniform_1(&ctx,&prog,"uStageHpos",stage.hpos);
         wglraw::set_uniform_1(&ctx,&prog,"uStageVpos",stage.vpos);
         wglraw::set_uniform_1(&ctx,&prog,"uStageZoom",stage.zoom);
+        wglraw::set_uniform_2(&ctx,&prog,"uCursor",stage.cursor);
         wglraw::set_uniform_1(&ctx,&prog,"uAspect",aspect);
     }
 }
@@ -102,12 +110,13 @@ pub trait Geometry {
 pub struct Stage {
     pub hpos: f32,
     pub vpos: f32,
-    pub zoom: f32
+    pub zoom: f32,
+    pub cursor: [f32;2],
 }
 
 impl Stage {
     pub fn new() -> Stage {
-        Stage { hpos: 0.0, vpos: 0.0, zoom: 1.0 }
+        Stage { hpos: 0.0, vpos: 0.0, zoom: 1.0, cursor: [0.0,0.0] }
     }
 }
 
--- src/fixx.rs
+++ src/fixx.rs
@@ -14,28 +14,26 @@ use std::cell::Ref;
 use std::cell::RefCell;
 use std::rc::Rc;
 
-const HOSC_V_SRC : &str = "
-attribute vec2 aVertexPositionHosc;
-attribute vec3 aVertexColourHosc;
+const V_SRC : &str = "
+attribute vec3 aVertexPositionFixx;
+attribute vec3 aVertexColourFixx;
 
+uniform vec2 uCursor;
 uniform float uAspect;
-uniform float uStageHpos;
-uniform float uStageVpos;
-uniform float uStageZoom;
 
 varying lowp vec3 vColour;
 
 void main() {
-     gl_Position = vec4(
-          (aVertexPositionHosc.x - uStageHpos) * uStageZoom,
-          (aVertexPositionHosc.y - uStageVpos),
-          0.0, 1.0
+    gl_Position = vec4(
+        aVertexPositionFixx.x - uCursor.x,
+        ( aVertexPositionFixx.y ) - uCursor.y,
+        0.0, 1.0
     );
-    vColour = aVertexColourHosc;
+    vColour = aVertexColourFixx;
 }
 ";
 
-const HOSC_F_SRC : &str = "
+const F_SRC : &str = "
 varying lowp vec3 vColour;
 
 void main() {
@@ -43,7 +41,7 @@ void main() {
 }
 ";
 
-pub struct HoscGeometry {
+pub struct FixxGeometry {
     adata: Rc<RefCell<ArenaData>>,
     points : Vec<f32>,
     colours: Vec<f32>,
@@ -53,31 +51,30 @@ pub struct HoscGeometry {
     prog: glprog,
 }
 
-impl HoscGeometry {
-    pub fn new(adata: Rc<RefCell<ArenaData>>) -> HoscGeometry {
+impl FixxGeometry {
+    pub fn new(adata: Rc<RefCell<ArenaData>>) -> FixxGeometry {
         let adata2 = adata.clone();
         let ctx = &adata2.borrow().ctx;
-        HoscGeometry {
+        FixxGeometry {
             indices: 0,
             adata,
             points: Vec::<f32>::new(),
             colours: Vec::<f32>::new(),
-            prog: wglraw::prepare_shaders(&ctx,&HOSC_V_SRC,&HOSC_F_SRC),
+            prog: wglraw::prepare_shaders(&ctx,&V_SRC,&F_SRC),
             buffer: wglraw::init_buffer(&ctx),
             colour_buffer: wglraw::init_buffer(&ctx)
         }
     }
 
-    pub fn triangle(&mut self,points:[f32;6],colour:[f32;3]) {
+    pub fn triangle(&mut self,points:[f32;9],colour:[f32;3]) {
         self.points.extend_from_slice(&points);
-        self.colours.extend_from_slice(&colour);
-        self.colours.extend_from_slice(&colour);
-        self.colours.extend_from_slice(&colour);
+        for i in 0..3 {
+            self.colours.extend_from_slice(&colour);
+        }
         self.indices = self.indices + 3
     }
 }
-
-impl Geometry for HoscGeometry {
+impl Geometry for FixxGeometry {
     fn adata(&self) -> Ref<ArenaData> { self.adata.borrow() }
     fn program<'a>(&'a self) -> &'a glprog { &self.prog }
 
@@ -95,10 +92,8 @@ impl Geometry for HoscGeometry {
     fn draw(&self) {
         let ctx = &self.adata().ctx;
         ctx.use_program(Some(&self.prog));
-        wglraw::link_buffer(&ctx,&self.prog,"aVertexPositionHosc",2,&self.buffer);
-        wglraw::link_buffer(&ctx,&self.prog,"aVertexColourHosc",3,&self.colour_buffer);
+        wglraw::link_buffer(&ctx,&self.prog,"aVertexPositionFixx",3,&self.buffer);
+        wglraw::link_buffer(&ctx,&self.prog,"aVertexColourFixx",3,&self.colour_buffer);
         ctx.draw_arrays(glctx::TRIANGLES,0,self.indices);
-    }
-        
+    }        
 }
-
