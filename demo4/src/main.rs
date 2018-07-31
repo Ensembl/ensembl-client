2217860c8410311bcbb2285d9adfdbf3fd7c2102
Dan Sheppard <dan@ebi.ac.uk>
2018-07-31 17:29:17 +0100

    More allocator work.

--- demo4/src/main.rs
+++ demo4/src/main.rs
@@ -1,34 +1,76 @@
 mod alloc;
 use alloc::Allocation;
 
+use std::cmp::{
+    max
+};
+
 struct TicketReq {
     width: u32,
     height: u32
 }
 
 struct Allocator {
-    reqs: Vec<TicketReq>
+    reqs: Vec<TicketReq>,
+    max_width: u32,
+    threshold: u32,
 }
 
 struct Ticket {
     index: usize
 }
 
+struct AllocatorImpl {
+    big: Allocation,
+    small: Allocation,
+    threshold: u32,
+}
+
+impl AllocatorImpl {
+    fn new(threshold: u32, max_width: u32) -> AllocatorImpl {
+        let size = max_width.next_power_of_two();
+        AllocatorImpl {
+            big: Allocation::new(size),
+            small: Allocation::new(size),
+            threshold
+        }
+    }
+    
+    fn allocate_one(&mut self,width: u32,height: u32) -> (bool,u32,u32) {
+        if width < self.threshold {
+            let (x,y) = self.small.allocate(width,height).unwrap();
+            (false,x,y)
+        } else {
+            let height = ( height + self.threshold - 1) / self.threshold;
+            let (x,y) = self.big.allocate(width,height).unwrap();
+            (true,x,y)
+        }
+    }
+}
+
 impl Allocator {
-    fn new() -> Allocator {
+    fn new(threshold: u32) -> Allocator {
         Allocator {
-            reqs: Vec::<TicketReq>::new()
+            reqs: Vec::<TicketReq>::new(),
+            max_width: 0,
+            threshold,
         }
     }
     
     fn request(&mut self, width: u32, height: u32) -> Ticket {
+        self.max_width = max(width,self.max_width);
         let data = TicketReq { width, height };
         self.reqs.push(data);
         Ticket { index: self.reqs.len() }
     }
-        
+                
     fn allocate(&mut self) {
-    }    
+        let mut aimpl = AllocatorImpl::new(self.threshold,self.max_width);
+        let mut res = Vec::<(bool,u32,u32)>::new();
+        for t in &self.reqs {
+            res.push(aimpl.allocate_one(t.width,t.height));
+        }
+    }
 }
 
 fn main() {
