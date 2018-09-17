d979ffe7df543415b7b9bfd69ff2bb52c71f28f6
Dan Sheppard <dan@ebi.ac.uk>
2018-09-17 13:13:48 +0100

    Use timers in visual testcard.
    (Good test case for mouse physics).

--- src/controller/mod.rs
+++ src/controller/mod.rs
@@ -7,6 +7,6 @@ mod projector;
 mod jank;
 mod timers;
 
-pub use self::global::Global;
+pub use self::global::{ Global, CanvasGlobal };
 pub use self::runner::{ Event, EventRunner };
 pub use self::jank::JankBuster;
