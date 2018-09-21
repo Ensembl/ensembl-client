766449240d9f833c3922f2b01ccd00aab5a48da0
Imran Salam <imran@ebi.ac.uk>
2018-09-21 16:57:05 +0100

    add react refs object used for memoization

--- src/scripts/types/objects.ts
+++ src/scripts/types/objects.ts
@@ -1,4 +1,8 @@
-import { ReactEventHandler } from 'react';
+import { RefObject, ReactEventHandler } from 'react';
+
+export type ReactRefs = {
+  [key: string]: RefObject<HTMLElement>;
+};
 
 export type EventHandlers = {
   [key: string]: ReactEventHandler;
