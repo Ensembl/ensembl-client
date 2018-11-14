dc544b41c609013ef3e4045a33c5843f71e8e0a4
Dan Sheppard <dan@ebi.ac.uk>
2018-11-14 11:48:11 +0000

    Better pane for in/out.

--- src/debug/testcards/leftright.rs
+++ src/debug/testcards/leftright.rs
@@ -11,21 +11,23 @@ use types::{ Colour, cleaf, cpixel, A_TOPLEFT, area };
 
 pub fn testcard_leftright(g: Arc<Mutex<Global>>) {
     let g = &mut g.lock().unwrap();
-    let font = FCFont::new(60,"Lato",FontVariety::Normal);
+    let font = FCFont::new(20,"Lato",FontVariety::Normal);
     let cs = ClosureSource::new(move |lc,leaf| {
-        let i = leaf.get_index();
-        let (colour,offset) = if i % 2 == 0 {
-            (ColourSpec::Colour(Colour(255,0,0)),0)
-        } else {
-            (ColourSpec::Colour(Colour(0,255,0)),10)
-        };
-        let tx = text_texture(&format!("{}",i),&font,&Colour(199,208,213),&Colour(255,255,255));
-        closure_add(lc,&pin_texture(tx,&cleaf(0.,1),
-                    &cpixel(0,10),&cpixel(1,1).anchor(A_TOPLEFT)));
-        closure_add(lc,&stretch_rectangle(&area(
-                        cleaf(0.,offset),
-                        cleaf(1.,offset+10),
-                     ),&colour));        
+        for j in 0..10 {
+            let i = leaf.get_index();
+            let (colour,offset) = if i % 2 == 0 {
+                (ColourSpec::Colour(Colour(255,0,0)),0)
+            } else {
+                (ColourSpec::Colour(Colour(0,255,0)),10)
+            };
+            let tx = text_texture(&format!("{}",i*10+j),&font,&Colour(199,208,213),&Colour(255,255,255));
+            closure_add(lc,&pin_texture(tx,&cleaf(0.1*j as f32,1),
+                        &cpixel(0,20),&cpixel(1,1).anchor(A_TOPLEFT)));
+            closure_add(lc,&stretch_rectangle(&area(
+                            cleaf(0.,offset),
+                            cleaf(1.,offset+10),
+                         ),&colour));        
+        }
     });
 
     let c = Component::new(Box::new(cs.clone()),Rc::new(StateFixed(StateValue::On())));
