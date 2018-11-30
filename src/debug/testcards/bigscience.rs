28bc77b6aab199963b66b09c261fa91592ab9390
Dan Sheppard <dan@ebi.ac.uk>
2018-11-30 16:23:44 +0000

    Basic restoration of "big science" testcards in new controller.
    Issues:
    1. odd/even not working;
    2. no buttons
    3. horrible jank;
    4. doesn't scale;
    5. no animation.

--- src/debug/testcards/bigscience.rs
+++ src/debug/testcards/bigscience.rs
@@ -37,6 +37,9 @@ use drawing::{
 
 use controller::input::Event;
 
+const SW : i32 = 1000;
+const SH : i32 = 1000;
+
 fn battenberg() -> DrawingSpec {
     bitmap_texture(vec! { 0,0,255,255,
                           255,0,0,255,
@@ -73,64 +76,48 @@ fn make_rng(seed: i32) -> SmallRng {
     SmallRng::from_seed([s,s,s,s,s,s,s,s,t,t,t,t,t,t,t,t])
 }
 
-pub fn big_science(a: &mut App, onoff: bool) {
+
+fn source_odd() -> ClosureSource {
     let seed = 12345678;
     
-    let size = a.with_stage(|s| s.get_size());
-
-    let fc_font = FCFont::new(12,"Lato",FontVariety::Normal);
-
-    let mut middle = size.1 / 120;
-    if middle < 5 { middle = 5; }
-    
-    let green_col = Colour(50,255,150);
-    let green = ColourSpec::Spot(green_col);
-    let red = ColourSpec::Spot(Colour(255,100,50));
-    
-    let (sw,sh);
-    {
-        sw = size.0;
-        sh = size.1;
-    }
-    let col = Colour(200,200,200);
-    
-    let cs_odd = ClosureSource::new(0.,move |lc,leaf| {
+    let p = Palette::new();
+    ClosureSource::new(0.,enclose! { (p) move |lc,leaf| {
         let mut rng = make_rng(seed);
         for yidx in 0..20 {
             let y = yidx * 60;
-            if yidx == middle - 5 {
+            if yidx == p.middle - 5 {
                 for i in 1..10 {
                     closure_add(lc,&pin_mathsshape(&cleaf(-0.1+0.04*(i as f32),y+20),
                        A_MIDDLE,
                        10. * i as f32,None,MathsShape::Circle,
-                       &green));
+                       &p.green));
                 }
             }
-            if yidx == middle {
-                closure_add(lc,&stretch_rectangle(&area_size(cleaf(-0.2,y-20),cleaf(0.1,5)),&red));
+            if yidx == p.middle {
+                closure_add(lc,&stretch_rectangle(&area_size(cleaf(-0.2,y-20),cleaf(0.1,5)),&p.red));
                 for i in 3..8 {
                     closure_add(lc,&pin_mathsshape(&cleaf(-0.1+0.04*(i as f32),y+20),
                                    A_TOP,
                                    10., None, MathsShape::Polygon(i,0.2*i as f32),
-                                   &red));
+                                   &p.red));
                 }
                 closure_add(lc,&pin_mathsshape(
                     &cleaf(-0.2,y-15),
                     A_RIGHT,
                     5.,None,
                     MathsShape::Polygon(3,0.),
-                    &red));
+                    &p.red));
             }
-            if yidx == middle+2 || yidx == middle+4 {
+            if yidx == p.middle+2 || yidx == p.middle+4 {
                 let wiggle = wiggly(&mut rng,500,cleaf(-0.5,y-5),0.002,20);
-                closure_add(lc,&stretch_wiggle(wiggle,2,&green_col));
+                closure_add(lc,&stretch_wiggle(wiggle,2,&p.green_col));
             }
-            if yidx == middle +1 {
+            if yidx == p.middle +1 {
                 for i in (3..7).step_by(2) {
                     closure_add(lc,&pin_mathsshape(&cleaf(-0.1+0.04*(i as f32),y+20),
                                    A_TOP,
                                    10., Some(2.), MathsShape::Polygon(i,0.2*i as f32),
-                                   &red));
+                                   &p.red));
                     let colour = Colour(0,128,255);
                     closure_add(lc,&pin_mathsshape(&cleaf(-0.3+0.04*(i as f32),y+20),
                                    A_TOP,
@@ -140,13 +127,18 @@ pub fn big_science(a: &mut App, onoff: bool) {
             }
         }
         closure_done(lc,1200);
-    });
+    }})
+}
 
-    let cs_even = ClosureSource::new(0.,move |lc,leaf| {
+fn source_even() -> ClosureSource {
+    let seed = 12345678;
+    
+    let p = Palette::new();
+    ClosureSource::new(0.,enclose! { (p) move |lc,leaf| {
         let mut rng = make_rng(seed);
         for yidx in 0..20 {
             let y = yidx * 60;
-            if yidx == middle - 5 {
+            if yidx == p.middle - 5 {
                 for i in 1..10 {
                     let colour = Colour(255,0,128);
                     closure_add(lc,&pin_mathsshape(&cleaf(-0.3+0.04*(i as f32),y+20),
@@ -155,8 +147,8 @@ pub fn big_science(a: &mut App, onoff: bool) {
                                    &ColourSpec::Colour(colour)));
                 }
             }
-            if yidx == middle {
-                closure_add(lc,&stretch_rectangle(&area_size(cleaf(-0.2,y-15),cleaf(0.1,5)),&green));
+            if yidx == p.middle {
+                closure_add(lc,&stretch_rectangle(&area_size(cleaf(-0.2,y-15),cleaf(0.1,5)),&p.green));
                 for i in 3..8 {
                     let colour = Colour(0,128,255);
                     closure_add(lc,&pin_mathsshape(&cleaf(-0.3+0.04*(i as f32),y+20),
@@ -170,14 +162,14 @@ pub fn big_science(a: &mut App, onoff: bool) {
                     A_LEFT,
                     5.,None,
                     MathsShape::Polygon(3,0.5),
-                    &green));
+                    &p.green));
             }
-            if yidx == middle +1 {
+            if yidx == p.middle +1 {
                 for i in (4..8).step_by(2) {
                     closure_add(lc,&pin_mathsshape(&cleaf(-0.1+0.04*(i as f32),y+20),
                                    A_TOP,
                                    10., Some(2.), MathsShape::Polygon(i,0.2*i as f32),
-                                   &red));
+                                   &p.red));
                     let colour = Colour(0,128,255);
                     closure_add(lc,&pin_mathsshape(&cleaf(-0.3+0.04*(i as f32),y+20),
                                    A_TOP,
@@ -187,15 +179,24 @@ pub fn big_science(a: &mut App, onoff: bool) {
             }
         }
         closure_done(lc,1200);
-    });
+    }})
+}
+
+pub fn bs_source_sub(even: bool) -> ClosureSource {
+    if even { source_even() } else { source_odd() }
+}
+
+pub fn bs_source_main() -> ClosureSource {
+    let seed = 12345678;
     
-    let cs = ClosureSource::new(0.,move |ref mut lc,leaf| {
+    let p = Palette::new();
+    ClosureSource::new(0.,enclose! { (p) move |ref mut lc,leaf| {
         let mut rng = make_rng(seed);
-        measure(lc,&leaf,&red,&green);
+        measure(lc,&leaf,&p.red,&p.green);
         for yidx in 0..20 {
             let y = yidx * 60;
             let val = daft(&mut rng);
-            let tx = text_texture(&val,&fc_font,&col,&Colour(255,255,255));
+            let tx = text_texture(&val,&p.fc_font,&p.col,&Colour(255,255,255));
             
             closure_add(lc,&page_texture(tx, 
                                 &cedge(TOPLEFT,cpixel(12,y+18)),
@@ -206,11 +207,11 @@ pub fn big_science(a: &mut App, onoff: bool) {
                                 &cpixel(0,y+18).x_edge(AxisSense::Pos),
                                 A_LEFT,
                                 5.,None,MathsShape::Polygon(3,0.),
-                                &green));            
-            if yidx == middle+3 {
+                                &p.green));            
+            if yidx == p.middle+3 {
                 closure_add(lc,&pin_rectangle(&cleaf(0.,y-10),&area_size(cpixel(0,-10),cpixel(20,20)),&ColourSpec::Colour(Colour(128,0,0))));
             }
-            if yidx == middle {
+            if yidx == p.middle {
                 let tx = bitmap_texture(
                                     vec! { 0,0,255,255,
                                              255,0,0,255,
@@ -223,7 +224,7 @@ pub fn big_science(a: &mut App, onoff: bool) {
                                              0,255,0,255,
                                              255,255,0,255 },cpixel(2,2),false);
                 closure_add(lc,&pin_texture(tx,&cleaf(0.,y-25),&cpixel(0,0),&cpixel(10,10).anchor(A_TOPLEFT)));
-            } else if yidx == middle-2 {
+            } else if yidx == p.middle-2 {
                 let mut parts = Vec::<MarkSpec>::new();
                 for row in 0..8 {
                     let mut off = 0;
@@ -250,7 +251,7 @@ pub fn big_science(a: &mut App, onoff: bool) {
                 }
                 let tx = collage(parts,cpixel(1000,40));
                 closure_add(lc,&stretch_texture(tx,&area_size(cleaf(-0.7,y-25),cleaf(2.,40))));
-            } else if yidx == middle+2 || yidx == middle+4 {
+            } else if yidx == p.middle+2 || yidx == p.middle+4 {
                 // no-op, wiggles
             } else {
                 for idx in -100..100 {
@@ -277,27 +278,27 @@ pub fn big_science(a: &mut App, onoff: bool) {
                     }
                     if rng.gen_range(0,10) == 0 {
                         let val = bio_daft(&mut rng);
-                        let tx = text_texture(&val,&fc_font,&col,&Colour(255,255,255));
+                        let tx = text_texture(&val,&p.fc_font,&p.col,&Colour(255,255,255));
                         closure_add(lc,&pin_texture(tx, &cleaf(x/1000.,y-24), &cpixel(0,0), &cpixel(1,1).anchor(A_MIDDLE)));
                     }
                 }
             }
         }
             
-        closure_add(lc,&fix_rectangle(&area(cedge(TOPLEFT,cpixel(sw/2,0)),
-                                        cedge(TOPLEFT,cpixel(sw/2+1,sh))),
+        closure_add(lc,&fix_rectangle(&area(cedge(TOPLEFT,cpixel(SW/2,0)),
+                                        cedge(TOPLEFT,cpixel(SW/2+1,SH))),
                             &ColourSpec::Colour(Colour(0,0,0))));
-        closure_add(lc,&fix_rectangle(&area(cedge(TOPLEFT,cpixel(sw/2+5,0)),
-                                        cedge(TOPLEFT,cpixel(sw/2+8,sh))),
-                            &red));
+        closure_add(lc,&fix_rectangle(&area(cedge(TOPLEFT,cpixel(SW/2+5,0)),
+                                        cedge(TOPLEFT,cpixel(SW/2+8,SH))),
+                            &p.red));
         let tx = bitmap_texture(vec! { 0,0,255,255,
                                      255,0,0,255,
                                      0,255,0,255,
                                      255,255,0,255 },cpixel(1,4),false);
         closure_add(lc,&fix_texture(tx, 
-                                &cedge(TOPLEFT,cpixel(sw/2-5,0)),
+                                &cedge(TOPLEFT,cpixel(SW/2-5,0)),
                                 &cpixel(0,0),
-                                &cpixel(1,sh).anchor(A_TOPLEFT)));
+                                &cpixel(1,SH).anchor(A_TOPLEFT)));
 
         closure_add(lc,&fix_texture(battenberg(),
                                 &cedge(TOPLEFT,cpixel(0,0)),
@@ -319,21 +320,56 @@ pub fn big_science(a: &mut App, onoff: bool) {
         closure_add(lc,&fix_mathsshape(&cedge(TOPLEFT,cpixel(30,30)),
                                    A_TOPLEFT,
                                    20.,None,MathsShape::Polygon(5,0.),
-                                   &red));
+                                   &p.red));
         closure_add(lc,&fix_mathsshape(&cedge(TOPRIGHT,cpixel(30,30)),
                                    A_TOPRIGHT,
                                    20.,None,MathsShape::Polygon(5,0.),
-                                   &red));
+                                   &p.red));
         closure_add(lc,&fix_mathsshape(&cedge(BOTTOMLEFT,cpixel(30,30)),
                                    A_BOTTOMLEFT,
                                    20.,None,MathsShape::Polygon(5,0.),
-                                   &red));
+                                   &p.red));
         closure_add(lc,&fix_mathsshape(&cedge(BOTTOMRIGHT,cpixel(30,30)),
                                    A_BOTTOMRIGHT,
                                    20.,None,MathsShape::Polygon(5,0.),
-                                   &red));
+                                   &p.red));
         closure_done(lc,1200);
-    });
+    }})
+}
+
+#[derive(Clone)]
+struct Palette {
+    col: Colour,
+    green_col: Colour,
+    green: ColourSpec,
+    red: ColourSpec,
+    middle: i32,
+    fc_font: FCFont
+}
+
+impl Palette {
+    fn new() -> Palette {
+        let fc_font = FCFont::new(12,"Lato",FontVariety::Normal);
+
+        let mut middle = SH / 120;
+        if middle < 5 { middle = 5; }
+        
+        let green_col = Colour(50,255,150);
+        let green = ColourSpec::Spot(green_col);
+        let red = ColourSpec::Spot(Colour(255,100,50));
+        Palette {
+            col: Colour(200,200,200),
+            green_col, green, red, middle, fc_font
+        }
+    }
+}
+
+pub fn big_science(a: &mut App, onoff: bool) {
+    let seed = 12345678;
+    
+    let cs_odd = source_odd();
+    let cs_even = source_even();
+    let cs = bs_source_main();
     
     let c = Component::new("XXX",Box::new(cs.clone()),Rc::new(StateFixed(StateValue::On())));
 
