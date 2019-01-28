cbf5b74fe4a22e367b88a3318ef62534fd69ade6
Dan Sheppard <dan@ebi.ac.uk>
2019-01-28 18:36:51 +0000

    New browser app integration.

--- src/scripts/components/apps/browser/BrowserImage.tsx
+++ src/scripts/components/apps/browser/BrowserImage.tsx
@@ -11,31 +11,43 @@ class BrowserImage extends PureComponent<BrowserImageProps> {
     this.browserCanvas = React.createRef();
   }
 
-  public componentDidMount() {
-    const moveEvent = new CustomEvent('bpane-start', {
+  activate_if_possible(currentEl) {
+    console.log('aip');
+
+    const activateEvent = new CustomEvent('bpane-activate', {
       bubbles: true,
-      detail: {}
+      detail: {
+        key: 'main'
+      }
     });
 
-    const currentEl = this.browserCanvas.current;
-
+    let done = false;
     if (currentEl && currentEl.ownerDocument) {
       const browserEl = currentEl.ownerDocument.querySelector(
-        'body'
+        '.browser-stage'
       ) as HTMLBodyElement;
 
-      if (browserEl) {
-        browserEl.dispatchEvent(moveEvent);
+      let loaded = false;
+      let bodyEl = currentEl.ownerDocument.querySelector('body');
+      console.log('bod', bodyEl);
+      if (bodyEl && bodyEl.classList.contains('browser-app-ready')) {
+        console.log('sending');
+        browserEl.dispatchEvent(activateEvent);
+        done = true;
       }
+    }
+    if (!done) {
+      setTimeout(() => this.activate_if_possible(currentEl), 50);
     }
   }
 
+  public componentDidMount() {
+    const currentEl = this.browserCanvas.current;
+    this.activate_if_possible(currentEl);
+  }
+
   public render() {
-    return (
-      <div className="browser-image" ref={this.browserCanvas}>
-        <div id="stage" />
-      </div>
-    );
+    return <div className="browser-stage" ref={this.browserCanvas} />;
   }
 }
 
