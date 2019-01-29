53304f80b4e9cbc28d850512b0f1c8f4f851314e
Dan Sheppard <dan@ebi.ac.uk>
2019-01-29 14:37:47 +0000

    Incorporate build version in browser webapp.

--- src/assets/browser/src/main.rs
+++ src/assets/browser/src/main.rs
@@ -26,12 +26,14 @@ mod dom;
 mod webgl_rendering_context;
 mod types;
 mod print;
+mod thisbuild;
 
 use controller::global;
 
 fn main() {
     stdweb::initialize();
     global::setup_global();
-    debug!("global","starting");
+    debug!("global","{}",thisbuild::build_summary());
+    console!("{}",thisbuild::build_summary());
     stdweb::event_loop();
 }
