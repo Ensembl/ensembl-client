bb55916d5a07a4a934b79fa11a315fdd770a7b1c
Dan Sheppard <dan@ebi.ac.uk>
2019-01-29 14:47:41 +0000

    Force rebuild (for build version info update) on delpoy.

--- src/assets/browser/deploy.sh
+++ src/assets/browser/deploy.sh
@@ -9,6 +9,7 @@ popd > /dev/null
 SRC="$BASE"
 DEST="$BASE/../../ensembl/assets/browser"
 cd $SRC
+touch Cargo.lock # force build
 cargo +nightly web build --target=wasm32-unknown-unknown --release
 cargo +nightly web deploy --target=wasm32-unknown-unknown --release
 cp $SRC/target/deploy/hellostdweb.wasm $DEST/browser.wasm
