524542425109131c19e41c061884111c94f620ab
Imran Salam <imran@ebi.ac.uk>
2019-02-04 16:59:48 +0000

    add missing styling added by Dan for browser and refactor BrowserImage

--- src/ensembl/src/content/app/browser/browser-image/BrowserImage.tsx
+++ src/ensembl/src/content/app/browser/browser-image/BrowserImage.tsx
@@ -1,5 +1,7 @@
 import React, { PureComponent } from 'react';
 
+import styles from './BrowserImage.scss';
+
 type BrowserImageProps = {};
 
 class BrowserImage extends PureComponent<BrowserImageProps> {
@@ -11,7 +13,16 @@ class BrowserImage extends PureComponent<BrowserImageProps> {
     this.browserCanvas = React.createRef();
   }
 
-  activate_if_possible(currentEl: HTMLElement | null) {
+  public componentDidMount() {
+    const currentEl = this.browserCanvas.current;
+    this.activateIfPossible(currentEl);
+  }
+
+  public render() {
+    return <div className={styles.browserStage} ref={this.browserCanvas} />;
+  }
+
+  private activateIfPossible(currentEl: HTMLElement | null) {
     const activateEvent = new CustomEvent('bpane-activate', {
       bubbles: true,
       detail: {
@@ -20,29 +31,23 @@ class BrowserImage extends PureComponent<BrowserImageProps> {
     });
 
     let done = false;
+
     if (currentEl && currentEl.ownerDocument) {
       const browserEl = currentEl.ownerDocument.querySelector(
         '.browser-stage'
       ) as HTMLBodyElement;
 
       const bodyEl = currentEl.ownerDocument.querySelector('body');
+
       if (bodyEl && bodyEl.classList.contains('browser-app-ready')) {
         browserEl.dispatchEvent(activateEvent);
         done = true;
       }
     }
+
     if (!done) {
-      setTimeout(() => this.activate_if_possible(currentEl), 50);
+      setTimeout(() => this.activateIfPossible(currentEl), 50);
     }
-  }
-
-  public componentDidMount() {
-    const currentEl = this.browserCanvas.current;
-    this.activate_if_possible(currentEl);
-  }
-
-  public render() {
-    return <div className="browser-stage" ref={this.browserCanvas} />;
   }
 }
 
