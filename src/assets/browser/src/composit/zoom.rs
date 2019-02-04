0f5494c7f5e1132b761d9db096349fce0c040a6e
Dan Sheppard <dan@ebi.ac.uk>
2019-02-04 14:29:07 +0000

    Maximum zoom limit.

--- src/assets/browser/src/composit/zoom.rs
+++ src/assets/browser/src/composit/zoom.rs
@@ -7,6 +7,8 @@ pub struct Zoom {
     max_bp: f64
 }
 
+const MAX_LIMIT_BP : f64 = 10.;
+
 impl Zoom {
     pub fn new(z: f64) -> Zoom {
         let mut out = Zoom { zoom: 1., linzoom: 1., max_bp: 1. };
@@ -19,7 +21,7 @@ impl Zoom {
         self.max_bp = bp;
     }
     
-    fn check_min_limit(&mut self, val: f64) -> f64 {
+    fn check_min_limit(&self, val: f64) -> f64 {
         if val < -self.max_bp.log10() {
             return -self.max_bp.log10();
         } else {
@@ -27,8 +29,18 @@ impl Zoom {
         }
     }
     
+    fn check_max_limit(&self, val: f64) -> f64 {
+        if val > -MAX_LIMIT_BP.log10() {
+            return -MAX_LIMIT_BP.log10();
+        } else {
+            return val;
+        }
+    }
+    
     pub fn set_zoom(&mut self, val: f64) {
-        let v = self.check_min_limit(val);
+        /* min has priority over max */
+        let mut v = self.check_max_limit(val);
+        v = self.check_min_limit(v);
         self.zoom = v;
         self.linzoom = 10.0_f64.powf(-v);
     }
