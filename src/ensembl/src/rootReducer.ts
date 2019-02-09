e4b7763bcbb77a9f8b98fd882ec8be7e4c2c0576
Imran Salam <imran@ebi.ac.uk>
2019-02-09 17:13:50 +0000

    add global reducer/action for site-wide state

--- src/ensembl/src/rootReducer.ts
+++ src/ensembl/src/rootReducer.ts
@@ -1,10 +1,11 @@
 import { combineReducers } from 'redux';
 import { StateType } from 'typesafe-actions';
 
+import global from './globalReducer';
 import header from './header/headerReducer';
 import browser from './content/app/browser/browserReducer';
 
-const rootReducer = combineReducers({ header, browser });
+const rootReducer = combineReducers({ global, header, browser });
 
 export type RootState = StateType<typeof rootReducer>;
 export default rootReducer;
