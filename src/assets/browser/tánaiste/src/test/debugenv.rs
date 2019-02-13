35f2a1a7141e10b44c2081e8235e21da269f9b25
Dan Sheppard <dan@ebi.ac.uk>
2019-02-13 16:03:48 +0000

    Report pid at startup.

--- src/assets/browser/tánaiste/src/test/debugenv.rs
+++ src/assets/browser/tánaiste/src/test/debugenv.rs
@@ -6,6 +6,7 @@ use runtime::{ Environment, ProcessState };
 
 pub struct DebugEnvironmentExtern {
     start: Instant,
+    pid: Option<usize>,
     exit_state: Option<ProcessState>,
     last_exit_str: Vec<String>,
     last_exit_float: Vec<Vec<f64>>
@@ -15,12 +16,14 @@ impl DebugEnvironmentExtern {
     pub fn new() -> DebugEnvironmentExtern {
         DebugEnvironmentExtern {
             start: Instant::now(),
+            pid: None,
             exit_state: None,
             last_exit_str: Vec::<String>::new(),
             last_exit_float: Vec::<Vec<f64>>::new()
         }
     }
-        
+    
+    pub fn get_pid(&self) -> Option<usize> { self.pid }
     pub fn get_exit_str(&self) -> &Vec<String> { &self.last_exit_str }
     pub fn get_exit_float(&self) -> &Vec<Vec<f64>> { &self.last_exit_float }
     pub fn get_exit_state(&self) -> &Option<ProcessState> { &self.exit_state }
@@ -30,6 +33,10 @@ impl Environment for DebugEnvironmentExtern {
     fn get_time(&mut self) -> i64 {
         let d = Instant::now().duration_since(self.start);
         d.as_secs() as i64 * 1000 + d.subsec_millis() as i64
+    }
+    
+    fn started(&mut self, pid: usize) {
+        self.pid = Some(pid);
     }
     
     fn finished(&mut self, _pid: usize, state: ProcessState, codes: Vec<f64>, string: String) {
@@ -44,6 +51,10 @@ pub struct DebugEnvironmentBox(Rc<RefCell<DebugEnvironmentExtern>>);
 impl Environment for DebugEnvironmentBox {
     fn get_time(&mut self) -> i64 {
         self.0.borrow_mut().get_time()
+    }
+    
+    fn started(&mut self, pid: usize) {
+        self.0.borrow_mut().started(pid);
     }
     
     fn finished(&mut self, pid: usize, state: ProcessState, codes: Vec<f64>, string: String) {
@@ -72,5 +83,7 @@ impl DebugEnvironment {
     pub fn get_exit_state(&self) -> Option<ProcessState> {
         self.0.borrow_mut().get_exit_state().clone()
     }
-     
+    pub fn get_pid(&self) -> Option<usize> {
+        self.0.borrow_mut().get_pid()
+    }
 }
