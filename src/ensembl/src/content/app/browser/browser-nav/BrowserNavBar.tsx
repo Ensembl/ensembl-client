3f9a400e35f62e64292374943b9f0f632551b6bd
Imran Salam <imran@ebi.ac.uk>
2019-02-14 16:44:25 +0000

    disable browser nav icons based on max states sent by browser

--- src/ensembl/src/content/app/browser/browser-nav/BrowserNavBar.tsx
+++ src/ensembl/src/content/app/browser/browser-nav/BrowserNavBar.tsx
@@ -1,21 +1,57 @@
 import React, { FunctionComponent } from 'react';
+import { connect } from 'react-redux';
 
 import { browserNavConfig, BrowserNavItem } from '../browserConfig';
+
+import { RootState } from 'src/rootReducer';
+import { getBrowserNavStates } from '../browserSelectors';
+import { BrowserNavStates } from '../browserState';
 
 import BrowserNavIcon from './BrowserNavIcon';
 
 import styles from './BrowserNavBar.scss';
+import imageStyles from '../browser-image/BrowserImage.scss';
 
-type BrowserNavBarProps = {};
+type StateProps = {
+  browserNavStates: BrowserNavStates;
+};
 
-const BrowserNavBar: FunctionComponent<BrowserNavBarProps> = () => (
-  <div className={styles.browserNavBar}>
-    <dl>
-      {browserNavConfig.map((item: BrowserNavItem) => (
-        <BrowserNavIcon key={item.name} browserNavItem={item} />
-      ))}
-    </dl>
-  </div>
-);
+type DispatchProps = {};
 
-export default BrowserNavBar;
+type OwnProps = {};
+
+type BrowserNavBarProps = StateProps & DispatchProps & OwnProps;
+
+export const BrowserNavBar: FunctionComponent<BrowserNavBarProps> = (
+  props: BrowserNavBarProps
+) => {
+  const browserCanvas = document.querySelector(
+    `.${imageStyles.browserStage}`
+  ) as HTMLElement;
+
+  return (
+    <div className={styles.browserNavBar}>
+      <dl>
+        {browserNavConfig.map((item: BrowserNavItem, index: number) => (
+          <BrowserNavIcon
+            key={item.name}
+            browserNavItem={item}
+            browserCanvas={browserCanvas}
+            maxState={props.browserNavStates[index]}
+          />
+        ))}
+      </dl>
+    </div>
+  );
+};
+
+const mapStateToProps = (state: RootState): StateProps => ({
+  browserNavStates: getBrowserNavStates(state)
+});
+
+const mapDispatchToProps: DispatchProps = {};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(BrowserNavBar);
