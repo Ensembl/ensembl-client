0559b2e6700547cae655fe250c05312cae6e2e07
Andrey Azov <andrey.azov@gmail.com>
2019-02-21 01:09:32 +0300

    Update config files

--- src/ensembl/.storybook/webpack.config.js
+++ src/ensembl/.storybook/webpack.config.js
@@ -1,15 +1,46 @@
-const path = require("path");
+const path = require('path');
+const postcssPresetEnv = require('postcss-preset-env');
 
 module.exports = (baseConfig, env, defaultConfig) => {
   defaultConfig.module.rules.push({
-    test: /\.(ts|tsx)$/,
+    test: /\.tsx?$/,
     loader: require.resolve('babel-loader'),
     include: [
-      path.resolve(__dirname, "../src"),
-      path.resolve(__dirname, "../stories"),
+      path.resolve(__dirname, '../src'),
+      path.resolve(__dirname, '../stories'),
     ]
   });
-  defaultConfig.resolve.extensions.push(".ts", ".tsx");
+  defaultConfig.module.rules.push({
+    test: /.scss$/,
+    include: [
+      path.resolve(__dirname, '../src'),
+      path.resolve(__dirname, '../stories'),
+    ],
+    use: [
+      'style-loader',
+      {
+        loader: 'css-loader',
+        options: {
+          sourceMap: true,
+          modules: true,
+          localIdentName: '[local]__[name]__[hash:base64:5]'
+        }
+      },
+      {
+        loader: 'postcss-loader',
+        options: {
+          ident: 'postcss',
+          plugins: () => [postcssPresetEnv()]
+        }
+      },
+      'sass-loader'
+    ]
+  });
+  defaultConfig.resolve.extensions.push('.ts', '.tsx');
+  defaultConfig.resolve.alias = {
+    src: path.join(__dirname, '../src'),
+    static: path.join(__dirname, '../static')
+  };
 
   return defaultConfig;
 };
