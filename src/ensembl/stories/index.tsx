cf3c9c04fd25f137f9aa490b316a2837c4b36f59
Andrey Azov <andrey.azov@gmail.com>
2019-02-21 01:09:32 +0300

    Update config files

--- src/ensembl/stories/index.tsx
+++ src/ensembl/stories/index.tsx
@@ -1,12 +1,6 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 
-storiesOf('Button', module)
-  .add('with text', () => <button>Hello Button</button>)
-  .add('with some emoji', () => (
-    <button>
-      <span role="img" aria-label="so cool">
-        😀 😎 👍 💯
-      </span>
-    </button>
-  ));
+import SpeciesSelector from 'src/content/app/species-selector/SpeciesSelector';
+
+storiesOf('Button', module).add('Species selector', () => <SpeciesSelector />);
