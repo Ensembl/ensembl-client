cc2f474d687722c73c5162469d47e4eca5257772
Andrey Azov <andrey@ebi.ac.uk>
2019-02-21 11:10:48 +0000

    Add @svgr/webpack for storybook

--- src/ensembl/.storybook/webpack.config.js
+++ src/ensembl/.storybook/webpack.config.js
@@ -1,7 +1,18 @@
+/*
+  This config allows Storybook to use its default webpack config, with slight modifications
+*/
+
 const path = require('path');
 const postcssPresetEnv = require('postcss-preset-env');
 
 module.exports = (baseConfig, env, defaultConfig) => {
+
+  // a bit of a hack to remove svg handling from Storybook's default webpack config:
+  // find the rule that matches svg files and replace its regex with Storybook's default,
+  // but without svg
+  const defaultSvgRule = defaultConfig.module.rules.find(rule => rule.test.test('.svg'));
+  defaultSvgRule.test = /\.(ico|jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|cur|ani)(\?.*)?$/;
+
   defaultConfig.module.rules.push({
     test: /\.tsx?$/,
     loader: require.resolve('babel-loader'),
@@ -36,6 +47,11 @@ module.exports = (baseConfig, env, defaultConfig) => {
       'sass-loader'
     ]
   });
+  defaultConfig.module.rules.push({
+    test: /\.svg$/,
+    use: ['@svgr/webpack', 'file-loader'],
+  });
+
   defaultConfig.resolve.extensions.push('.ts', '.tsx');
   defaultConfig.resolve.alias = {
     src: path.join(__dirname, '../src'),
