427d315357e74f9c89f30cef284eaac74a019980
Andrey Azov <andrey@ebi.ac.uk>
2019-02-25 18:24:17 +0000

    Add StrainSelector

---
+++ src/ensembl/stories/species-selector/StrainSelector.stories.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+
+import StrainSelector from 'src/content/app/species-selector/components/strain-selector/StrainSelector';
+
+const strains = [
+  {
+    name: 'Mouse GRCm38.6',
+    isSelected: true
+  },
+  {
+    name: 'Mouse 129S1/SvimJ',
+    isSelected: false
+  },
+  {
+    name: 'Mouse A/J',
+    isSelected: false
+  },
+  {
+    name: 'Mouse AKR/J',
+    isSelected: true
+  }
+];
+
+storiesOf('Components|Species Selector/Strain Selector', module).add(
+  'default',
+  () => (
+    <div style={{ padding: '50px' }}>
+      <StrainSelector
+        strains={strains}
+        onSelect={(name) => console.log(`selected strain ${name}`)}
+      />
+    </div>
+  )
+);
