c4fca33b84e7c13ad1f32d37e8d9f9d3a67f6914
Andrey Azov <andrey.azov@gmail.com>
2019-02-27 21:40:38 +0000

    Update PopularSpeciesButton to include StrainSelector

--- src/ensembl/stories/species-selector/PopularSpeciesButton.stories.tsx
+++ src/ensembl/stories/species-selector/PopularSpeciesButton.stories.tsx
@@ -3,6 +3,8 @@ import { storiesOf } from '@storybook/react';
 
 import PopularSpeciesButton from 'src/content/app/species-selector/components/popular-species-button/PopularSpeciesButton';
 
+import { strains } from './StrainSelector.stories';
+
 storiesOf('Components|Species Selector/Popular species button', module)
   .add('not selected', () => (
     <>
@@ -10,31 +12,43 @@ storiesOf('Components|Species Selector/Popular species button', module)
         species="human"
         isSelected={false}
         onClick={() => {}}
+        onStrainSelect={() => {}}
       />
       <PopularSpeciesButton
         species="aspergillus"
         isSelected={false}
         onClick={() => {}}
+        onStrainSelect={() => {}}
       />
       <PopularSpeciesButton
         species="mouse"
         isSelected={false}
+        strains={strains}
         onClick={() => {}}
+        onStrainSelect={() => {}}
       />
     </>
   ))
   .add('selected', () => (
     <>
-      <PopularSpeciesButton species="human" isSelected onClick={() => {}} />
+      <PopularSpeciesButton
+        species="human"
+        isSelected={true}
+        onClick={() => {}}
+        onStrainSelect={() => {}}
+      />
       <PopularSpeciesButton
         species="aspergillus"
         isSelected={false}
         onClick={() => {}}
+        onStrainSelect={() => {}}
       />
       <PopularSpeciesButton
         species="mouse"
-        isSelected={false}
+        isSelected={true}
+        strains={strains}
         onClick={() => {}}
+        onStrainSelect={() => {}}
       />
     </>
   ));
