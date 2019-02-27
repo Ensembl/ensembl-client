dad1adc5f0ca183def00736189dd3281be0487ff
Imran Salam <imran@ebi.ac.uk>
2019-02-27 16:34:36 +0000

    refactor browser component events and add support to notify browser when chr location changed in site framework

---
+++ src/ensembl/src/content/app/browser/browserHelper.ts
@@ -0,0 +1,14 @@
+import { ChrLocation } from './browserState';
+
+export function getChrLocationFromStr(chrLocationStr: string): ChrLocation {
+  const [chrCode, chrRegion] = chrLocationStr.split(':');
+  const [startBp, endBp] = chrRegion.split('-');
+
+  return [chrCode, +startBp, +endBp];
+}
+
+export function getChrLocationStr(chrLocation: ChrLocation): string {
+  const [chrCode, startBp, endBp] = chrLocation;
+
+  return `${chrCode}:${startBp}-${endBp}`;
+}
