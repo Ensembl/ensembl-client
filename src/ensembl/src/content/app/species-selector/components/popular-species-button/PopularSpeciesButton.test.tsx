5047bb86c5d5d1eb2a44bd14b02e2083e66116dd
Andrey Azov <andrey.azov@gmail.com>
2019-02-27 22:19:52 +0000

    Start writing tests for PopularSpeciesButton

---
+++ src/ensembl/src/content/app/species-selector/components/popular-species-button/PopularSpeciesButton.test.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { mount } from 'enzyme';
+
+import PopularSpeciesButton from './PopularSpeciesButton';
+
+import styles from './PopularSpeciesButton.scss';
+
+const onClick = jest.fn;
+const onStrainSelect = jest.fn;
+const strains = [...new Array(4)].map((item, index) => ({
+  name: `strain-${index}`,
+  isSelected: Boolean(index % 2)
+}));
+const species = 'mouse';
+
+const commonProps = {
+  species,
+  onClick,
+  onStrainSelect
+};
+
+describe('<PopularSpeciesButton />', () => {
+  describe('not selected', () => {
+    test('has appropriate class', () => {
+      const renderedButton = (
+        <PopularSpeciesButton {...commonProps} isSelected={false} />
+      );
+      const component = mount(renderedButton);
+      console.log('component', component);
+    });
+  });
+});
