57c7de984b4e50501738c493be6912d87bc68ff2
ens-ap5 <annelyle@ebi.ac.uk>
2019-02-28 10:48:58 +0000

    Use SCSS variables in stories
    Export colours from SCSS so we don't have hard-coded hex values in the colour palette story. Also, remove Foundation default colours from story - we shouldn't be using both!

--- src/ensembl/stories/design-primitives/colors/Colors.stories.tsx
+++ src/ensembl/stories/design-primitives/colors/Colors.stories.tsx
@@ -2,96 +2,47 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 
 import ColorCard from './ColorCard';
+import variables from '../../../src/styles/_settings.scss';
 
 const colors = [
   {
-    name: 'Foundation primary',
-    variableName: '$primary-color',
-    value: '#1779ba'
-  },
-  {
-    name: 'Foundation secondary',
-    variableName: '$secondary-color',
-    value: '#767676'
-  },
-  {
-    name: 'Foundation success',
-    variableName: '$success-color',
-    value: '#3adb76'
-  },
-  {
-    name: 'Foundation warning',
-    variableName: '$warning-color',
-    value: '#ffae00'
-  },
-  {
-    name: 'Foundation alert',
-    variableName: '$alert-color',
-    value: '#cc4b37'
-  },
-  {
-    name: 'Light gray',
-    variableName: '$light-gray',
-    value: '#e6e6e6'
-  },
-  {
-    name: 'Medium gray',
-    variableName: '$medium-gray',
-    value: '#cacaca'
-  },
-  {
-    name: 'Dark gray',
-    variableName: '$dark-gray',
-    value: '#8a8a8a'
-  },
-  {
-    name: 'Black',
-    variableName: '$black',
-    value: '#0a0a0a'
-  },
-  {
-    name: 'White',
-    variableName: '$white',
-    value: '#fefefe'
+    name: 'Ensembl blue',
+    variableName: '$ens-blue',
+    value: variables['ens-blue']
   },
   {
     name: 'Ensembl black',
     variableName: '$ens-black',
-    value: '#162f45'
-  },
-  {
-    name: 'Ensembl blue',
-    variableName: '$ens-blue',
-    value: '#3894fb'
+    value: variables['ens-black']
   },
   {
     name: 'Ensembl dark grey',
     variableName: '$ens-dark-grey',
-    value: '#999'
+    value: variables['ens-dark-grey']
   },
   {
     name: 'Ensembl grey',
     variableName: '$ens-grey',
-    value: '#bdc5cd'
+    value: variables['ens-grey']
   },
   {
     name: 'Ensembl medium grey',
     variableName: '$ens-medium-grey',
-    value: '#d5dadf'
+    value: variables['ens-medium-grey']
   },
   {
     name: 'Ensembl light grey',
     variableName: '$ens-light-grey',
-    value: '#f1f3f5'
+    value: variables['ens-light-grey']
   },
   {
     name: 'Ensembl white',
     variableName: '$ens-white',
-    value: '#fff'
+    value: variables['ens-white']
   }
 ];
 
-storiesOf('Design Primitives|Colors', module).add('color pallette', () => (
+storiesOf('Design Primitives|Colors', module).add('color palette', () => (
   <>
     {colors.map((colorData) => (
       <ColorCard {...colorData} />
