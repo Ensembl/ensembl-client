761f039e87ef30671193e2c1d34e1d91e57552ef
Imran Salam <imran@ebi.ac.uk>
2019-02-28 15:19:45 +0000

    add content and styling to home

--- src/ensembl/src/content/home/Home.tsx
+++ src/ensembl/src/content/home/Home.tsx
@@ -7,21 +7,48 @@ type HomeProps = {};
 
 const Home: FunctionComponent<HomeProps> = (props: HomeProps) => {
   return (
-    <section class={styles.home}>
-      <h2>Previously viewed</h2>
-      <dl>
-        <dd>
-          <Link to="/app/browser/human/BRCA2/13:32315474-32400266">
-            Human gene BRCA2
-          </Link>
-        </dd>
-        <dd>
-          <Link to="/app/browser/human/TTN/2:178525989-178830802">
-            Human transcript TTN
-          </Link>
-        </dd>
-      </dl>
-    </section>
+    <div className={styles.home}>
+      <section className={styles.search}>
+        <h2>Find</h2>
+        <p>
+          <input type="text" placeholder="Name, symbol or ID" />
+          <button disabled={true}>Go</button>
+        </p>
+        <div className={styles.filter}>
+          <h2>Refine results</h2>
+        </div>
+      </section>
+      <section className={styles.previouslyViewed}>
+        <h2>Previously viewed</h2>
+        <dl>
+          <dd>
+            <Link to="/app/browser/human/BRCA2/13:32315474-32400266">
+              Human gene BRCA2
+            </Link>
+          </dd>
+          <dd>
+            <Link to="/app/browser/human/TTN/2:178525989-178830802">
+              Human transcript TTN
+            </Link>
+          </dd>
+        </dl>
+      </section>
+      <section className={styles.siteMessage}>
+        <h4>Using the site</h4>
+        <p>
+          A very limited data set has been made available for this first
+          release.
+        </p>
+        <p>
+          Blue icons and text are clickable and will usually &lsquo;do&rsquo;
+          something.
+        </p>
+        <p>
+          Grey icons indicate apps &amp; functionality that is planned, but not
+          available yet.
+        </p>
+      </section>
+    </div>
   );
 };
 
