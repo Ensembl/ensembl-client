e44683856bd69b5618813806cd9f481dca3e1e85
ens-ap5 <annelyle@ebi.ac.uk>
2019-02-28 12:02:01 +0000

    Use absolute path for robustness

--- src/ensembl/stories/design-primitives/colors/Colors.stories.tsx
+++ src/ensembl/stories/design-primitives/colors/Colors.stories.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 
 import ColorCard from './ColorCard';
-import variables from '../../../src/styles/_settings.scss';
+import variables from 'src/styles/_settings.scss';
 
 const colors = [
   {
