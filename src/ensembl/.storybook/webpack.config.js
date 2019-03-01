e8fb964cff8e63acaae6f83f60e55a583c092d6e
ens-ap5 <annelyle@ebi.ac.uk>
2019-03-01 12:43:41 +0000

    Remove unnecessary loader

--- src/ensembl/.storybook/webpack.config.js
+++ src/ensembl/.storybook/webpack.config.js
@@ -22,13 +22,6 @@ module.exports = (baseConfig, env, defaultConfig) => {
     ]
   });
   defaultConfig.module.rules.push({
-    test: /\.json?$/,
-    loader: require.resolve('json-loader'),
-    include: [
-      path.resolve(__dirname, '../src/data/json')
-    ]
-  });
-  defaultConfig.module.rules.push({
     test: /.scss$/,
     include: [
       path.resolve(__dirname, '../src'),
