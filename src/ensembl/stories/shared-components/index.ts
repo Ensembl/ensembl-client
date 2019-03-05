798dcbb54917b62658f0dcf5f3e21db15bd526c5
Andrey Azov <andrey@ebi.ac.uk>
2019-03-05 16:55:50 +0000

    Update styles for various components (with Andrea)

--- src/ensembl/stories/shared-components/index.ts
+++ src/ensembl/stories/shared-components/index.ts
@@ -1,3 +1,4 @@
 import './close-button/CloseButton.stories';
+import './question-button/QuestionButton.stories';
 import './dropdown/Dropdown.stories';
 import './input/Input.stories';
--- src/ensembl/stories/species-selector/SelectedSpecies.stories.tsx
+++ src/ensembl/stories/species-selector/SelectedSpecies.stories.tsx
@@ -5,20 +5,22 @@ import SelectedSpecies from 'src/content/app/species-selector/components/selecte
 
 storiesOf('Components|Species Selector/Selected Species', module)
   .add('enabled', () => (
-    <SelectedSpecies
-      name="Mouse C57BL/6NJ"
-      assembly="C57BL_6NJ_v1"
-      isEnabled
-      onEnable={() => {
-        console.log('enable');
-      }}
-      onDisable={() => {
-        console.log('disable');
-      }}
-      onRemove={() => {
-        console.log('remove');
-      }}
-    />
+    <div style={{ padding: '40px' }}>
+      <SelectedSpecies
+        name="Mongoose C57BL/6NJ"
+        assembly="C57BL_6NJ_v1"
+        isEnabled
+        onEnable={() => {
+          console.log('enable');
+        }}
+        onDisable={() => {
+          console.log('disable');
+        }}
+        onRemove={() => {
+          console.log('remove');
+        }}
+      />
+    </div>
   ))
   .add('disabled', () => (
     <SelectedSpecies
