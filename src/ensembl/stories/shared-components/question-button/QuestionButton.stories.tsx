757ead42b97fa3e13f67947bd5db2ad404730444
Andrey Azov <andrey@ebi.ac.uk>
2019-03-06 17:20:29 +0000

    Remove the svg of a question mark with the circle

--- src/ensembl/stories/shared-components/question-button/QuestionButton.stories.tsx
+++ src/ensembl/stories/shared-components/question-button/QuestionButton.stories.tsx
@@ -1,21 +1,9 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 
-import { ReactComponent as QuestionButtonIcon } from './icon_tooltip_question.svg';
-
 import QuestionButton from 'src/shared/question-button/QuestionButton';
 
-storiesOf('Components|Shared Components/Question button', module)
-  // .add('default', () => <QuestionButton onHover={() => {}} />)
-  .add('default', () => (
-    <>
-      <div>
-        <QuestionButton onHover={() => {}} />
-      </div>
-      <div>
-        <QuestionButtonIcon
-          style={{ width: '18px', height: '18px', fill: '#33adff' }}
-        />
-      </div>
-    </>
-  ));
+storiesOf('Components|Shared Components/Question button', module).add(
+  'default',
+  () => <QuestionButton onHover={() => {}} />
+);
