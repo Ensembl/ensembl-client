ae2f489c410cd7e0a265d36abb94fcfea8028cde
Manoj <manojpandian@ebi.ac.uk>
2019-03-06 12:04:00 +0000

    Add google analytics to track all the pageviews, navigations and events
    - Using custom middleware and react-ga track the events
    - A warning is displayed in the console if the google analytics meta data is not defined when a new event is created

--- src/ensembl/src/content/app/browser/browserActions.ts
+++ src/ensembl/src/content/app/browser/browserActions.ts
@@ -4,60 +4,122 @@ import { BrowserNavStates, ChrLocation } from './browserState';
 export const updateBrowserActivated = createAction(
   'browser/update-browser-activated',
   (resolve) => {
-    return (browserActivated: boolean) => resolve(browserActivated);
+    return (browserActivated: boolean) =>
+      resolve(browserActivated, {
+        ga: {
+          category: 'Browser',
+          label: 'Default Action'
+        }
+      });
   }
 );
 
 export const toggleTrackPanel = createAction(
   'browser/toggle-track-panel',
   (resolve) => {
-    return (trackPanelOpened?: boolean) => resolve(trackPanelOpened);
+    return (trackPanelOpened?: boolean) =>
+      resolve(trackPanelOpened, {
+        disable_tracking: true,
+        ga: {
+          category: 'Track Panel',
+          label: 'User Interaction'
+        }
+      });
   }
 );
 
 export const changeDrawerView = createAction(
   'browser/change-drawer-view',
   (resolve) => {
-    return (drawerView: string) => resolve(drawerView);
+    return (drawerView: string) =>
+      resolve(drawerView, {
+        ga: {
+          category: 'Drawer',
+          label: 'User Interaction'
+        }
+      });
   }
 );
 
 export const toggleDrawer = createAction('browser/toggle-drawer', (resolve) => {
-  return (drawerOpened?: boolean) => resolve(drawerOpened);
+  return (drawerOpened?: boolean) =>
+    resolve(drawerOpened, {
+      ga: {
+        category1: 'Drawer',
+        label: 'User Interaction'
+      }
+    });
 });
 
 export const toggleBrowserNav = createAction(
-  'browser/toggle-browser-navigation'
+  'browser/toggle-browser-navigation',
+  (resolve) => {
+    return () =>
+      resolve(undefined, {
+        ga: {
+          category: 'Browser',
+          label: 'Navigation'
+        }
+      });
+  }
 );
 
 export const updateBrowserNavStates = createAction(
   'browser/update-browser-nav-states',
   (resolve) => {
-    return (browserNavStates: BrowserNavStates) => resolve(browserNavStates);
+    return (browserNavStates: BrowserNavStates) =>
+      resolve(browserNavStates, {
+        ga: {
+          category: 'Browser',
+          label: 'Navigation'
+        }
+      });
   }
 );
 
 export const updateChrLocation = createAction(
   'browser/update-chromosome-location',
   (resolve) => {
-    return (chrLocation: ChrLocation) => resolve(chrLocation);
+    return (chrLocation: ChrLocation) =>
+      resolve(chrLocation, { ga: { disable_tracking: true } });
   }
 );
 
 export const updateDefaultChrLocation = createAction(
   'browser/update-default-chromosome-location',
   (resolve) => {
-    return (chrLocation: ChrLocation) => resolve(chrLocation);
+    return (chrLocation: ChrLocation) =>
+      resolve(chrLocation, {
+        ga: {
+          category: 'Browser',
+          label: 'User Interaction'
+        }
+      });
   }
 );
 
 export const openTrackPanelModal = createAction(
   'browser/open-track-panel-modal',
   (resolve) => {
-    return (trackPanelModalView: string) => resolve(trackPanelModalView);
+    return (trackPanelModalView: string) =>
+      resolve(trackPanelModalView, {
+        ga: {
+          category: 'Track Panel Modal',
+          label: 'User Interaction'
+        }
+      });
   }
 );
 
 export const closeTrackPanelModal = createAction(
-  'browser/close-track-panel-modal'
+  'browser/close-track-panel-modal',
+  (resolve) => {
+    return () =>
+      resolve(undefined, {
+        ga: {
+          category: 'Track Panel Modal',
+          label: 'Navigation'
+        }
+      });
+  }
 );
--- src/ensembl/src/store.ts
+++ src/ensembl/src/store.ts
@@ -4,6 +4,41 @@ import { composeWithDevTools } from 'redux-devtools-extension';
 
 import reducers from './rootReducer';
 
+import ReactGA from 'react-ga';
+
+ReactGA.initialize('UA-58710484-17');
+
 const composeEnhancers = composeWithDevTools({});
 
-export default createStore(reducers, composeEnhancers(applyMiddleware(thunk)));
+const googleAnalyticsMiddleWare = (store: any) => (next: any) => (
+  action: any
+) => {
+  try {
+    // If we have the google anlytics meta data passed in, and
+    // if disable_ga_tracking flag is not set to true,
+    // We need to track this event
+    if (!action.meta.ga.disable_tracking && action.meta.ga.category.length) {
+      // The action and category fields are mandatory
+      ReactGA.event({
+        action: action.type,
+        category: action.meta.ga.category,
+        label: action.meta.ga.label,
+        value: action.meta.ga.value,
+        nonInteraction: action.meta.ga.nonInteraction
+      });
+    }
+  } catch {
+    console.warn(
+      `Google Analytics meta data not correctly defined for the action: ${
+        action.type
+      }. \n Please define the meta data or pass 'meta.ga.disable_tracking = true' to disable tracking.`
+    );
+  }
+
+  next(action);
+};
+
+export default createStore(
+  reducers,
+  composeEnhancers(applyMiddleware(thunk, googleAnalyticsMiddleWare))
+);
