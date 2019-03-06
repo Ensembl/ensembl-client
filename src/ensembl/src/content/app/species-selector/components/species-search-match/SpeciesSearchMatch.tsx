5bbac9a73b67f95c28f45225ff1f8ba5d9876d4a
Andrey Azov <andrey@ebi.ac.uk>
2019-03-06 18:26:04 +0000

    Add match splitter logic to the SpeciesSearchMatch component

--- src/ensembl/src/content/app/species-selector/components/species-search-match/SpeciesSearchMatch.tsx
+++ src/ensembl/src/content/app/species-selector/components/species-search-match/SpeciesSearchMatch.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
+import zip from 'lodash/zip';
+import last from 'lodash/last';
 
-import { SearchMatch } from 'src/content/app/species-selector/types/species-search';
+import {
+  SearchMatch,
+  MatchedSubstring
+} from 'src/content/app/species-selector/types/species-search';
 
 import styles from './SpeciesSearchMatch.scss';
 
@@ -8,8 +13,66 @@ type Props = {
   match: SearchMatch;
 };
 
+type SplitterProps = {
+  string: string;
+  matchedSubsctrings: MatchedSubstring[];
+};
+
 const SpeciesSearchMatch = ({ match }: Props) => {
-  return <div className={styles.speciesSearchMatch}>{match.description}</div>;
+  const commonName = <CommonName match={match} />;
+  return (
+    <div className={styles.speciesSearchMatch}>
+      {match.description}
+      {commonName}
+    </div>
+  );
+};
+
+const CommonName = ({ match }: Props) => {
+  const { description, matched_substrings } = match;
+  console.log(
+    splitMatch({ string: description, matchedSubsctrings: matched_substrings })
+  );
+  return <p />;
+};
+
+const splitMatch = ({ string, matchedSubsctrings }: SplitterProps) => {
+  const matchStartIndices = matchedSubsctrings.map(({ offset }) => offset);
+  const matchEndIndices = matchedSubsctrings.map(
+    ({ offset, length }) => offset + length
+  );
+  const matchIndices = zip(matchStartIndices, matchEndIndices);
+  return matchIndices.reduce((result, current, index, array) => {
+    if (index === 0 && current[0] > 0) {
+      result = [
+        ...result,
+        {
+          start: 0,
+          end: current[0],
+          isMatch: false
+        }
+      ];
+    }
+    result = [
+      ...result,
+      {
+        start: current[0],
+        end: current[1],
+        isMatch: true
+      }
+    ];
+    if (index === array.length - 1) {
+      result = [
+        ...result,
+        {
+          start: current[1],
+          end: string.length - 1,
+          isMatch: false
+        }
+      ];
+    }
+    return result;
+  }, []);
 };
 
 export default SpeciesSearchMatch;
