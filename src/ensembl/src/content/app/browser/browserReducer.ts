26ba957e4d7cd59865e05d878d6ba9ea65068601
Imran Salam <imran@ebi.ac.uk>
2019-03-07 13:49:40 +0000

    add browser info api call to actions

--- src/ensembl/src/content/app/browser/browserReducer.ts
+++ src/ensembl/src/content/app/browser/browserReducer.ts
@@ -1,58 +1,99 @@
+import { combineReducers } from 'redux';
 import { ActionType, getType } from 'typesafe-actions';
 
-import * as browser from './browserActions';
+import * as browserActions from './browserActions';
 import {
   BrowserState,
-  defaultState,
+  defaultBrowserState,
   trackPanelState,
-  drawerState
+  drawerState,
+  ObjectInfoState,
+  defaultObjectInfoState
 } from './browserState';
 
-export default (
-  state: BrowserState = defaultState,
-  action: ActionType<typeof browser>
-): BrowserState => {
+function browserInfo(
+  state: BrowserState = defaultBrowserState,
+  action: ActionType<typeof browserActions>
+): BrowserState {
   switch (action.type) {
-    case getType(browser.updateBrowserActivated):
+    case getType(browserActions.updateBrowserActivated):
       return { ...state, browserActivated: action.payload };
-    case getType(browser.toggleTrackPanel):
+    case getType(browserActions.toggleTrackPanel):
       return trackPanelState(state, action.payload);
-    case getType(browser.changeDrawerView):
+    case getType(browserActions.changeDrawerView):
       return {
         ...state,
         drawerView: action.payload
       };
-    case getType(browser.toggleDrawer):
+    case getType(browserActions.toggleDrawer):
       return drawerState(state, action.payload);
-    case getType(browser.toggleBrowserNav):
+    case getType(browserActions.toggleBrowserNav):
       return { ...state, browserNavOpened: !state.browserNavOpened };
-    case getType(browser.updateBrowserNavStates):
+    case getType(browserActions.updateBrowserNavStates):
       return { ...state, browserNavStates: action.payload };
-    case getType(browser.updateChrLocation):
+    case getType(browserActions.updateChrLocation):
       return { ...state, chrLocation: action.payload };
-    case getType(browser.updateDefaultChrLocation):
+    case getType(browserActions.updateDefaultChrLocation):
       return {
         ...state,
         chrLocation: action.payload,
         defaultChrLocation: action.payload
       };
-    case getType(browser.openTrackPanelModal):
+    case getType(browserActions.openTrackPanelModal):
       return {
         ...state,
         trackPanelModalOpened: true,
         trackPanelModalView: action.payload
       };
-    case getType(browser.closeTrackPanelModal):
+    case getType(browserActions.closeTrackPanelModal):
       return {
         ...state,
         trackPanelModalOpened: false,
         trackPanelModalView: ''
       };
-    case getType(browser.toggleGenomeSelector):
+    case getType(browserActions.toggleGenomeSelector):
       return { ...state, genomeSelectorActive: action.payload };
-    case getType(browser.selectBrowserTab):
+    case getType(browserActions.selectBrowserTab):
       return { ...state, selectedBrowserTab: action.payload };
     default:
       return state;
   }
-};
+}
+
+function objectInfo(
+  state: ObjectInfoState = defaultObjectInfoState,
+  action: ActionType<typeof browserActions>
+): ObjectInfoState {
+  switch (action.type) {
+    case getType(browserActions.fetchObjectInfo.failure):
+      return { ...state, browserInfoFetchFailed: true };
+    case getType(browserActions.fetchObjectInfo.request):
+      return {
+        ...state,
+        browserInfoFetchFailed: false,
+        browserInfoFetching: true
+      };
+    case getType(browserActions.fetchObjectInfo.success):
+      type BrowserInfo = {
+        object_info: {};
+        track_categories: [];
+      };
+
+      const json = action.payload as BrowserInfo;
+
+      return {
+        ...state,
+        browserInfoFetchFailed: false,
+        browserInfoFetching: false,
+        objectInfo: json.object_info,
+        trackCategories: json.track_categories
+      };
+    default:
+      return state;
+  }
+}
+
+export default combineReducers({
+  browserInfo,
+  objectInfo
+});
