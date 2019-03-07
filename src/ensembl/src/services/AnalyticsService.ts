b1aaaf817e9116ca0dc061e815dbdf86ecf5c627
Manoj <manojpandian@ebi.ac.uk>
2019-03-07 16:50:41 +0000

    Add the missing files from my previous commit.
    - Added the missing Analytics service wrapper
    - Added the ADR for google analytics tracking

---
+++ src/ensembl/src/services/AnalyticsService.ts
@@ -0,0 +1,31 @@
+import ReactGA from 'react-ga';
+
+// TODO this needs to be loaded from the config
+const googleTrackingID = 'UA-58710484-17';
+
+class AnalyticsTracking {
+  constructor() {
+    this.reactGA = ReactGA.initialize(googleTrackingID);
+    this.reactGA = ReactGA;
+  }
+
+  // Track a pageview
+  trackPageView(pathToTrack: string) {
+    this.reactGA.pageview(pathToTrack);
+  }
+
+  // Track an event
+  trackEvent(action: any) {
+    this.reactGA.event({
+      action: action.meta.ga.action ? action.meta.ga.action : action.type,
+      category: action.meta.ga.category,
+      label: action.meta.ga.label,
+      value: action.meta.ga.value,
+      nonInteraction: action.meta.ga.nonInteraction
+    });
+  }
+}
+
+const GoogleAnalyticsTracking = new AnalyticsTracking();
+
+export default GoogleAnalyticsTracking;
