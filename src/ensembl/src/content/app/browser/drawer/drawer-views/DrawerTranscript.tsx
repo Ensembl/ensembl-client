ca54edd2d9c36410c73cae21f4321830b0bb1de1
Imran Salam <imran@ebi.ac.uk>
2019-03-07 21:09:03 +0000

    use data from API for drawer content

--- src/ensembl/src/content/app/browser/drawer/drawer-views/DrawerTranscript.tsx
+++ src/ensembl/src/content/app/browser/drawer/drawer-views/DrawerTranscript.tsx
@@ -1,16 +1,34 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, Fragment } from 'react';
 
 import styles from '../Drawer.scss';
 
-const DrawerTranscript: FunctionComponent = () => {
+type DrawerTranscriptProps = {
+  objectInfo: any;
+};
+
+const DrawerTranscript: FunctionComponent = (props: DrawerTranscriptProps) => {
+  const { objectInfo } = props;
+
+  let transcriptStableId = objectInfo.associated_object.stable_id;
+  let selectedInfo = objectInfo.associated_object.selected_info;
+  let geneSymbol = objectInfo.obj_symbol;
+  let geneStableId = objectInfo.stable_id;
+
+  if (objectInfo.obj_type === 'transcript') {
+    transcriptStableId = objectInfo.stable_id;
+    selectedInfo = objectInfo.selected_info;
+    geneSymbol = objectInfo.associated_object.obj_symbol;
+    geneStableId = objectInfo.associated_object.stable_id;
+  }
+
   return (
     <dl className={styles.drawerView}>
       <dd className="clearfix">
         <label htmlFor="">Transcript</label>
         <div className={styles.details}>
           <p>
-            <span className={styles.mainDetail}>ENST00000380152.7</span>
-            <span className={styles.secondaryInfo}>MANE Select</span>
+            <span className={styles.mainDetail}>{transcriptStableId}</span>
+            <span className={styles.secondaryInfo}>{selectedInfo}</span>
           </p>
         </div>
       </dd>
@@ -19,8 +37,8 @@ const DrawerTranscript: FunctionComponent = () => {
         <label htmlFor="">Gene</label>
         <div className={styles.details}>
           <p>
-            <span>BRCA2</span>
-            <span className={styles.secondaryInfo}>ENSG00000139618</span>
+            <span>{geneSymbol}</span>
+            <span className={styles.secondaryInfo}>{geneStableId}</span>
           </p>
         </div>
       </dd>
@@ -28,16 +46,27 @@ const DrawerTranscript: FunctionComponent = () => {
       <dd className="clearfix">
         <label htmlFor="">Description</label>
         <div className={styles.details}>
-          <p>
-            MANE Select transcripts match GRCh38 and are 100% identical between
-            Ensembl-GENCODE and RefSeq for 5' UTR, CDS, splicing and 3' UTR.
-          </p>
-          <p>
-            The MANE project (Matched Annotation from NCBI and EMBL-EBI) is
-            collaboration betwen Ensembl-GENCODE and RefSeq to select a default
-            transcript per human protein coding locus that is representative of
-            biology, and is well-supported, expressed and conserved.
-          </p>
+          {selectedInfo === 'MANE Select' ? (
+            <Fragment>
+              <p>
+                MANE Select transcripts match GRCh38 and are 100% identical
+                between Ensembl-GENCODE and RefSeq for 5' UTR, CDS, splicing and
+                3' UTR.
+              </p>
+              <p>
+                The MANE project (Matched Annotation from NCBI and EMBL-EBI) is
+                collaboration betwen Ensembl-GENCODE and RefSeq to select a
+                default transcript per human protein coding locus that is
+                representative of biology, and is well-supported, expressed and
+                conserved.
+              </p>
+            </Fragment>
+          ) : (
+            <p>
+              The Selected transcript is defined as either the longest CDS (if
+              the gene has translated transcripts) or the longest cDNA
+            </p>
+          )}
         </div>
       </dd>
     </dl>
