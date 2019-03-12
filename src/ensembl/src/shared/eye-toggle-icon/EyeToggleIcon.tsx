682807ee3494f7abf6247def8044609202093cc1
Manoj <manojpandian@ebi.ac.uk>
2019-03-12 15:37:06 +0000

    Create storybook entry for EyeToggleIcon

--- src/ensembl/src/shared/eye-toggle-icon/EyeToggleIcon.tsx
+++ src/ensembl/src/shared/eye-toggle-icon/EyeToggleIcon.tsx
@@ -1,28 +1,37 @@
 import React from 'react';
+import classNames from 'classnames';
 
 import eyeOnIcon from './eye-on.svg';
 import eyeOffIcon from './eye-off.svg';
 
+import styles from './EyeToggleIcon.scss';
+
 type Props = {
   iconStatus: string;
   description: string;
-  callbackFunction: Function;
+  callbackFunction: () => void;
 };
 
 const EyeToggleIcon = (props: Props) => {
+  const handleClick = () => {
+    props.callbackFunction();
+  };
+
+  const className = classNames(styles.eyeToggleIcon);
+
   return (
-    <button onClick={props.callbackFunction()}>
-      <img
-        src={props.iconStatus === 'on' ? eyeOnIcon : eyeOffIcon}
-        alt={props.description}
-      />
-    </button>
+    <img
+      onClick={handleClick}
+      className={className}
+      src={props.iconStatus === 'on' ? eyeOnIcon : eyeOffIcon}
+      alt={props.description}
+    />
   );
 };
 
 EyeToggleIcon.defaultProps = {
-  iconStatus: 'off',
-  description: ''
+  description: '',
+  iconStatus: 'off'
 };
 
 export default EyeToggleIcon;
