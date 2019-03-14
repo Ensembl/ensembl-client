aea7b63c2f817ed8b65a2de13a5028fd8d654a18
Dan Sheppard <dan@ebi.ac.uk>
2019-03-14 16:11:51 +0000

    Remove unnecessary, non-performant, awkward cache layer.

--- src/assets/browser/app/src/composit/compmanager.rs
+++ src/assets/browser/app/src/composit/compmanager.rs
@@ -5,26 +5,20 @@ const MAX_PENDING : i32 = 0;
 const CACHE_SIZE : usize = 10;
 
 use composit::{
-    ActiveSource, Leaf, Carriage, SourceSched
+    ActiveSource, Leaf, Carriage
 };
 
 pub struct ComponentManager {
-    components: HashMap<String,ActiveSource>,
-    source_factory: SourceSched
+    components: HashMap<String,ActiveSource>
 }
 
 impl ComponentManager {
     pub fn new() -> ComponentManager {
         ComponentManager {
-            components: HashMap::<String,ActiveSource>::new(),
-            source_factory: SourceSched::new(MAX_PENDING,CACHE_SIZE)
+            components: HashMap::<String,ActiveSource>::new()
         }
     }
-    
-    pub fn tick(&mut self, t: f64) {
-        self.source_factory.tick(t);
-    }
-    
+        
     pub fn add(&mut self, c: ActiveSource) {
         let name = c.get_name().to_string();
         if let Entry::Vacant(e) = self.components.entry(name) {
@@ -38,10 +32,10 @@ impl ComponentManager {
     
     pub fn make_comp_carriages(&mut self, c: &ActiveSource, leaf: &Leaf) -> Vec<Carriage> {
         let mut lcomps = Vec::<Carriage>::new();
-        lcomps.push(c.make_carriage(&mut self.source_factory,&None,&leaf));
+        lcomps.push(c.make_carriage(&None,&leaf));
         for part in c.list_parts() {            
             debug!("redraw","make_carriages {:?} for {}",leaf,part);
-            lcomps.push(c.make_carriage(&mut self.source_factory,&Some(part),&leaf));
+            lcomps.push(c.make_carriage(&Some(part),&leaf));
         }
         lcomps
     }
--- src/assets/browser/app/src/composit/source/drawnresponse.rs
+++ src/assets/browser/app/src/composit/source/drawnresponse.rs
@@ -7,7 +7,6 @@ use print::Programs;
 use shape::{ ShapeSpec, Shape };
 
 pub struct DrawnResponse {
-    shapes: Option<Vec<ShapeSpec>>,
     drawings: Vec<Option<Drawing>>,
     sr: SourceResponse,
     part: Option<String>
@@ -17,29 +16,24 @@ impl DrawnResponse {
     pub fn new(sr: SourceResponse, part: &Option<String>) -> DrawnResponse {
         DrawnResponse { 
             sr, 
-            shapes: None, 
             drawings: Vec::<Option<Drawing>>::new(),
             part: part.clone()
         }
     }
 
-    fn populate(&mut self) {
-        let specs = self.sr.get_shapes(&self.part);
-        self.shapes = Some(specs);
+    fn shapes(&self) -> Vec<ShapeSpec> {
+        self.sr.get_shapes(&self.part)
     }
     
     #[allow(unused)]
-    pub fn size(&self) -> usize { self.shapes.as_ref().unwrap().len() }
+    pub fn size(&self) -> usize { self.shapes().len() }
     
     pub fn is_done(&self) -> bool { self.sr.is_done() }
     
     pub fn redraw(&mut self, ds: &mut DrawingSession) {
         if self.sr.is_done() {
-            if self.shapes.is_none() {
-                self.populate();
-            }
             self.drawings.clear();
-            for mut s in self.shapes.as_mut().unwrap().iter_mut() {
+            for mut s in self.shapes() {
                 if let Some(a) = s.get_artist() {
                     let ocm = a.select_canvas(ds);
                     self.drawings.push(Some(ocm.add_request(a)));
@@ -53,11 +47,8 @@ impl DrawnResponse {
     pub fn into_objects(&mut self, progs: &mut Programs,
                   ds: &mut DrawingSession, e: &mut PrintEdition) {
         if self.sr.is_done() {
-            if self.shapes.is_none() {
-                self.populate();
-            }
             let mut di = self.drawings.iter();
-            for mut s in self.shapes.as_mut().unwrap().iter_mut() {
+            for mut s in self.shapes().iter() {
                 let d = di.next();
                 let geom_name = s.get_geometry();
                 if let Some(geom) = progs.map.get_mut(&geom_name) {
