b42a62367e8ee2e9d39421b8a8f8d2adb4a80f2e
Manoj <manojpandian@ebi.ac.uk>
2019-03-15 17:05:49 +0000

    Storybook entry to display all the available static images

--- src/ensembl/stories/index.tsx
+++ src/ensembl/stories/index.tsx
@@ -3,3 +3,4 @@ import 'src/styles/main.scss';
 import './design-primitives/index.ts';
 import './shared-components/index.ts';
 import './species-selector/index.ts';
+import './static-images/index.ts';
