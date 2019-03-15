6b75a0f6e5efc76a06c03bc4627aa4d2bb83befc
Andrey Azov <andrey@ebi.ac.uk>
2019-03-15 10:53:57 +0000

    Feature/species search match (#10)
    - Add lodash (because we are loading it as a sub-dependency anyway, and it will make our life easier)
    - Add the SpeciesSearchMatch element whose responsibility it is to markup the matched substrings in the search results
    - Add tests for SpeciesSearchMatch

--- src/ensembl/src/content/app/species-selector/components/species-autosuggestion-panel/SpeciesAutosuggestionPanel.tsx
+++ src/ensembl/src/content/app/species-selector/components/species-autosuggestion-panel/SpeciesAutosuggestionPanel.tsx
@@ -4,15 +4,24 @@ import SpeciesSearchMatch from '../species-search-match/SpeciesSearchMatch';
 
 import styles from './SpeciesAutosuggestionPanel.scss';
 
-import { SearchMatches } from 'src/content/app/species-selector/types/species-search';
+import { SearchMatch } from 'src/content/app/species-selector/types/species-search';
 
 type Props = {
-  matches: SearchMatches;
+  matches: SearchMatch[];
+  onMatchSelected: (match: SearchMatch) => void;
 };
 
 const SpeciesAutosuggestionPanel = (props: Props) => {
+  const onMatchSelected = (match: SearchMatch) => () => {
+    props.onMatchSelected(match);
+  };
+
   const matches = props.matches.map((match) => (
-    <SpeciesSearchMatch match={match} key={match.description} />
+    <SpeciesSearchMatch
+      match={match}
+      onClick={onMatchSelected(match)}
+      key={match.description}
+    />
   ));
 
   return <div className={styles.speciesAutosuggestionPanel}>{matches}</div>;
