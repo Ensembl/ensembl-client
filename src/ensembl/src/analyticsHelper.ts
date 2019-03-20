a0443075218c517f98f6e60f6bb52adec6f5025a
Imran Salam <imran@ebi.ac.uk>
2019-03-20 17:21:25 +0000

    add types for analytics objects and give user the option to pass either label string or option object

--- src/ensembl/src/analyticsHelper.ts
+++ src/ensembl/src/analyticsHelper.ts
@@ -2,12 +2,30 @@ export enum AnalyticsCategory {
   GLOBAL = 'Global'
 }
 
-export const buildAnalyticsObject = (category: string) => (data: {}) => ({
+export type AnalyticsOptions = {
+  action?: string;
+  label: string;
+  value?: string;
+};
+
+export type AnalyticsType = {
   ga: {
-    category,
-    ...data
-  }
-});
+    category: AnalyticsCategory;
+  } & AnalyticsOptions;
+};
+
+export const buildAnalyticsObject = (category: AnalyticsCategory) => (
+  data: AnalyticsOptions | string
+): AnalyticsType => {
+  data = typeof data === 'string' ? { label: data } : data;
+
+  return {
+    ga: {
+      category,
+      ...data
+    }
+  };
+};
 
 export const getGlobalAnalyticsObject = buildAnalyticsObject(
   AnalyticsCategory.GLOBAL
