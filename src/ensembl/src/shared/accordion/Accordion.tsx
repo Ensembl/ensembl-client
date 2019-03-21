076c97c126fb103ff0786c8c4bb6cfd5ab2d44e0
Manoj <manojpandian@ebi.ac.uk>
2019-03-21 17:35:29 +0000

    Some updates to the accordion component

--- src/ensembl/src/shared/accordion/Accordion.tsx
+++ src/ensembl/src/shared/accordion/Accordion.tsx
@@ -1,24 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react';
+
 import styles from './Accordion.scss';
 
-class Accordion extends Component {
-  public render() {
-    return (
-      <section>
-        <ul className="accordion" data-accordion>
-          <li className="accordion-item is-active" data-accordion-item>
-            <a href="#" className="accordion-title">
-              Accordion 1
-            </a>
-            <div className="accordion-content" data-tab-content>
-              <p>Panel 1. Lorem ipsum dolor</p>
-              <a href="#">Nowhere to Go</a>
-            </div>
-          </li>
-        </ul>
-      </section>
-    );
-  }
-}
+type AccordionProps = React.HTMLAttributes<HTMLDivElement>;
+
+const Accordion = ({ ...rest }: AccordionProps): JSX.Element => {
+  return (
+    <section className={styles.accordionContainer}>
+      <div {...rest} />
+    </section>
+  );
+};
 
 export default Accordion;
--- src/ensembl/stories/shared-components/accordion/Accordion.stories.tsx
+++ src/ensembl/stories/shared-components/accordion/Accordion.stories.tsx
@@ -3,7 +3,25 @@ import { storiesOf } from '@storybook/react';
 
 import Accordion from 'src/shared/accordion/Accordion';
 
+import AccordionItem from 'src/shared/accordion/accordion-item/AccordionItem';
+import AccordionItemHeader from 'src/shared/accordion/accordion-item/accordion-item-header/AccordionItemHeader';
+import styles from './Accordion.stories.scss';
+
 storiesOf('Components|Shared Components/Accordion', module).add(
   'default',
-  () => <Accordion />
+  () => {
+    return (
+      <div className={styles.accordionContainer}>
+        <Accordion>
+          <AccordionItem>
+            <AccordionItemHeader title="Item 1" />
+          </AccordionItem>
+
+          <AccordionItem>
+            <AccordionItemHeader title="Item 2" />
+          </AccordionItem>
+        </Accordion>
+      </div>
+    );
+  }
 );
