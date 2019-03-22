db90149a6ba395723ab735b43e1a2b59886c976c
Imran Salam <32512909+imran-ebi@users.noreply.github.com>
2019-03-22 10:10:19 +0000

    use factory function to generate analytics objects in Redux actions

---
+++ src/ensembl/src/analyticsHelper.ts
@@ -0,0 +1,33 @@
+export enum AnalyticsCategory {
+  GLOBAL = 'Global'
+}
+
+export type AnalyticsOptions = {
+  action?: string;
+  label: string;
+  nonInteraction?: boolean;
+  value?: number;
+};
+
+export type AnalyticsType = {
+  ga: {
+    category: AnalyticsCategory;
+  } & AnalyticsOptions;
+};
+
+export const buildAnalyticsObject = (category: AnalyticsCategory) => (
+  data: AnalyticsOptions | string
+): AnalyticsType => {
+  data = typeof data === 'string' ? { label: data } : data;
+
+  return {
+    ga: {
+      category,
+      ...data
+    }
+  };
+};
+
+export const getGlobalAnalyticsObject = buildAnalyticsObject(
+  AnalyticsCategory.GLOBAL
+);
