c1e25c56989bb584dbd525785735124b1fdec847
Imran Salam <imran@ebi.ac.uk>
2019-03-22 11:22:51 +0000

    add GA using analytics object function to all header actions

--- src/ensembl/src/header/headerActions.ts
+++ src/ensembl/src/header/headerActions.ts
@@ -1,17 +1,25 @@
 import { createAction } from 'typesafe-actions';
 
-export const toggleAccount = createAction('header/toggle-account');
-export const toggleLaunchbar = createAction('header/toggle-launchbar');
+import { getHeaderAnalyticsObject } from 'src/analyticsHelper';
+
+export const toggleAccount = createAction(
+  'header/toggle-account',
+  (resolve) => {
+    return () => resolve(undefined, getHeaderAnalyticsObject('Navigation'));
+  }
+);
+
+export const toggleLaunchbar = createAction(
+  'header/toggle-launchbar',
+  (resolve) => {
+    return () => resolve(undefined, getHeaderAnalyticsObject('Navigation'));
+  }
+);
 
 export const changeCurrentApp = createAction(
   'header/change-current-app',
   (resolve) => {
     return (currentApp: string) =>
-      resolve(currentApp, {
-        ga: {
-          category: 'Header',
-          label: 'Navigation'
-        }
-      });
+      resolve(currentApp, getHeaderAnalyticsObject('Navigation'));
   }
 );
