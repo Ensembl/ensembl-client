09efe4a6a5055928abe53af0052cbf98598c35a0
Imran Salam <imran@ebi.ac.uk>
2019-03-25 10:53:09 +0000

    add the newly added reducers to root reducer

--- src/ensembl/src/root/rootReducer.ts
+++ src/ensembl/src/root/rootReducer.ts
@@ -1,16 +1,22 @@
 import { combineReducers } from 'redux';
 import { connectRouter } from 'connected-react-router';
 
+import browser from '../content/app/browser/browserReducer';
+import drawer from '../content/app/browser/drawer/drawerReducer';
 import global from '../global/globalReducer';
 import header from '../header/headerReducer';
-import browser from '../content/app/browser/browserReducer';
+import object from '../object/objectReducer';
+import trackPanel from '../content/app/browser/track-panel/trackPanelReducer';
 
 const rootReducer = (history: any) =>
   combineReducers({
     browser,
+    drawer,
     global,
     header,
-    router: connectRouter(history)
+    object,
+    router: connectRouter(history),
+    trackPanel
   });
 
 export default rootReducer;
