ec9e514835dbd504455f62db02f54ab89f7f58b4
Andrey Azov <andrey.azov@gmail.com>
2019-03-25 22:22:53 +0000

    Add tests for SearchField

--- src/ensembl/src/shared/search-field/SearchField.test.tsx
+++ src/ensembl/src/shared/search-field/SearchField.test.tsx
@@ -11,6 +11,10 @@ describe('<SearchField />', () => {
     onSubmit: jest.fn()
   };
 
+  afterEach(() => {
+    jest.resetAllMocks();
+  });
+
   const getMountedComponent = (props: any) => mount(<SearchField {...props} />);
 
   describe('rendering', () => {
@@ -21,18 +25,26 @@ describe('<SearchField />', () => {
   });
 
   describe('behaviour', () => {
-    test('calls onSubmit passing it the search value if enter is pressed', () => {
+    test('calls onChange handler for every search change', () => {
       const mountedComponent = getMountedComponent(commonProps);
-      const event = new CustomEvent('keypress', {
-        detail: { key: 'Enter', keyCode: 13 }
-      });
-      console.log(mountedComponent.find('input').getDOMNode());
+      const newSearch = 'foo';
       mountedComponent
         .find('input')
-        .getDOMNode()
-        .dispatchEvent(event);
+        .simulate('change', { target: { value: newSearch } });
+      expect(commonProps.onChange).toHaveBeenCalled();
+      expect(commonProps.onChange.mock.calls[0][0]).toBe(newSearch);
+    });
 
-      console.log(commonProps.onSubmit.mock.calls);
+    test('calls onSubmit passing it the search value if enter is pressed', () => {
+      // cheating here slightly by directly simulating the submit event
+      const search = 'foo';
+      const mountedComponent = getMountedComponent({
+        ...commonProps,
+        search
+      });
+      mountedComponent.simulate('submit');
+      expect(commonProps.onSubmit).toHaveBeenCalled();
+      expect(commonProps.onSubmit.mock.calls[0][0]).toBe(search);
     });
   });
 });
