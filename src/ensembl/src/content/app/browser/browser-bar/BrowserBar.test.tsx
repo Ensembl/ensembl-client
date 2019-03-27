e9ef68008af76325c61afebb37c9ff4a94afb109
Andrey Azov <andrey.azov@gmail.com>
2019-03-27 23:46:23 +0000

    Fix BrowserBar tests

--- src/ensembl/src/content/app/browser/browser-bar/BrowserBar.test.tsx
+++ src/ensembl/src/content/app/browser/browser-bar/BrowserBar.test.tsx
@@ -3,19 +3,56 @@ import { shallow } from 'enzyme';
 
 import { BrowserBar } from './BrowserBar';
 
+import { TrackType } from '../track-panel/trackPanelConfig';
+
 describe('<BrowserBar />', () => {
-  let toggleBrowserNavFn: () => void;
+  const dispatchBrowserLocation: any = jest.fn();
+  const selectBrowserTab: any = jest.fn();
+  const toggleBrowserNav: any = jest.fn();
+  const toggleDrawer: any = jest.fn();
+  const toggleGenomeSelector: any = jest.fn();
+
+  const objectInfo = {
+    assembly: {
+      name: 'GRCh38',
+      patch: 'p12'
+    },
+    associated_object: {
+      obj_type: 'transcript',
+      selected_info: 'MANE Select',
+      spliced_length: 84793,
+      stable_id: 'ENST00000380152.7'
+    },
+    bio_type: 'Protein coding',
+    obj_symbol: 'BRCA2',
+    obj_type: 'gene',
+    spliced_length: 84793,
+    stable_id: 'ENSG00000139618',
+    strand: 'forward'
+  };
+
+  const defaultProps = {
+    browserActivated: true,
+    browserNavOpened: false,
+    chrLocation: ['13', 32275301, 32433493] as [string, number, number],
+    defaultChrLocation: ['13', 32271473, 32437359] as [string, number, number],
+    drawerOpened: false,
+    genomeSelectorActive: false,
+    ensObjectInfo: objectInfo,
+    selectedBrowserTab: TrackType.GENOMIC,
+    trackPanelModalOpened: false,
+    trackPanelOpened: false,
+    dispatchBrowserLocation,
+    selectBrowserTab,
+    toggleBrowserNav,
+    toggleDrawer,
+    toggleGenomeSelector
+  };
+
   let wrapper: any;
 
   beforeEach(() => {
-    toggleBrowserNavFn = jest.fn();
-    wrapper = shallow(
-      <BrowserBar
-        browserNavOpened={true}
-        expanded={true}
-        toggleBrowserNav={toggleBrowserNavFn}
-      />
-    );
+    wrapper = shallow(<BrowserBar {...defaultProps} />);
   });
 
   test('has a left bar', () => {
