916e7a51b74328d1b55adde19ffda32028ac262b
Andrey Azov <andrey@ebi.ac.uk>
2019-04-02 09:48:53 +0100

    Update the Button stories

--- src/ensembl/stories/shared-components/button/Button.stories.tsx
+++ src/ensembl/stories/shared-components/button/Button.stories.tsx
@@ -4,20 +4,31 @@ import { action } from '@storybook/addon-actions';
 
 import { PrimaryButton, SecondaryButton } from 'src/shared/button/Button';
 
+import styles from './Button.stories.scss';
+
 const onClick = action('button-click');
 
 storiesOf('Components|Shared Components/Button/PrimaryButton', module)
   .add('default', () => (
-    <div style={{ padding: '40px' }}>
+    <div className={styles.wrapper}>
       <PrimaryButton onClick={onClick}>Primary button</PrimaryButton>
     </div>
   ))
   .add('disabled', () => (
-    <div style={{ padding: '40px' }}>
-      <PrimaryButton onClick={onClick} isDisabled={true}>
-        Primary button
-      </PrimaryButton>
-    </div>
+    <>
+      <p>On white background</p>
+      <div className={styles.wrapper}>
+        <PrimaryButton onClick={onClick} isDisabled={true}>
+          Primary button
+        </PrimaryButton>
+      </div>
+      <p>On light-grey background</p>
+      <div className={`${styles.wrapper} ${styles.lightGreyWrapper}`}>
+        <PrimaryButton onClick={onClick} isDisabled={true}>
+          Primary button
+        </PrimaryButton>
+      </div>
+    </>
   ));
 
 storiesOf('Components|Shared Components/Button/SecondaryButton', module).add(
