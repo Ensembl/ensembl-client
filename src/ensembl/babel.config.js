1928fbc810572d223d5919b5c82f97d36c2fee2a
Imran Salam <imran@ebi.ac.uk>
2019-04-18 16:11:26 +0100

    remove env settings from babel config as ts-jest doesn't rely on it

--- src/ensembl/babel.config.js
+++ src/ensembl/babel.config.js
@@ -16,22 +16,5 @@ module.exports = {
     '@babel/plugin-proposal-object-rest-spread',
     '@babel/plugin-proposal-class-properties',
     'babel-plugin-react-css-modules-sass'
-  ],
-  env: {
-    test: {
-      presets: [
-        '@babel/react',
-        '@babel/typescript',
-        [
-          '@babel/env',
-          {
-            modules: 'commonjs'
-          }
-        ]
-      ],
-      plugins: [
-        '@babel/plugin-transform-modules-commonjs'
-      ]
-    }
-  }
+  ]
 };
