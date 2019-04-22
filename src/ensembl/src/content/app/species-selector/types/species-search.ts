63ecbe3367b10144ddd2dc7516d3e3de1f130aab
Andrey Azov <andrey.azov@gmail.com>
2019-04-22 16:51:58 +0100

    Update types for Species Selector

--- src/ensembl/src/content/app/species-selector/types/species-search.ts
+++ src/ensembl/src/content/app/species-selector/types/species-search.ts
@@ -3,11 +3,12 @@
 export type SearchMatches = SearchMatch[];
 
 export type SearchMatch = {
-  description: string;
-  scientific_name: string | null;
-  assembly_name: string | null;
+  production_name: string; // unique identifier; not a very good field name
+  reference_species_name: string | null; // indicates whether the match is a strain of a reference species
+  common_name: string | null; // not every species has a common name
+  scientific_name: string; // every species has a scientific name
+  subtype: string | null; // any extra information about the species (e.g. assembly name for Human)
   matched_substrings: MatchedSubstring[];
-  // genome: string;
 };
 
 export type SearchMatchGroup = {
@@ -18,16 +19,12 @@ export type SearchMatchGroup = {
 export type MatchedSubstring = {
   length: number;
   offset: number;
-  match: 'description' | 'scientific_name';
+  match: 'description' | 'scientific_name' | 'subtype';
 };
 
 /*
 
 NOTES/QUESTIONS:
-
-- see Kieron's comment to our api docs in Confluence:
-> I don't like "parent species name" as a key. Given your comment context, I propose "reference_species_name".
-
 
 - what data needs to be passed to the genome browser after one/more species are selected
 (current pathname in genome browser: /app/browser/GRCh38_demo/ENSG00000139618?region=13:32274159-32434627)
@@ -35,18 +32,18 @@ NOTES/QUESTIONS:
 */
 
 export type Strain = {
-  id: string;
+  production_name: string;
   display_name: string;
-  parent_species_name: string;
 };
 
 export type Assembly = {
-  display_name: string;
+  assembly_name: string;
 };
 
 export type CommittedItem = {
-  display_name: string;
+  common_name: string | null;
+  scientific_name: string;
   assembly_name: string;
   isEnabled: boolean;
-  parentSpecies: string; // ???
+  reference_species_name: string; // because in Species Selector we need to keep track of how many strains have been selected
 };
