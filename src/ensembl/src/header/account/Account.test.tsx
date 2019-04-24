afda497295d65ec2086a74c6e08e5ca43c8c2522
Imran Salam <32512909+imran-ebi@users.noreply.github.com>
2019-04-24 09:29:32 +0100

    Improve test suite speed (#40)

--- src/ensembl/src/header/account/Account.test.tsx
+++ src/ensembl/src/header/account/Account.test.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import toJson from 'enzyme-to-json';
 
 import { Account } from './Account';
 
@@ -20,9 +19,5 @@ describe('<Account />', () => {
       wrapper.setProps({ accountExpanded: false });
       expect(wrapper.find('.account')).toHaveLength(0);
     });
-  });
-
-  test('renders correctly', () => {
-    expect(toJson(wrapper)).toMatchSnapshot();
   });
 });
