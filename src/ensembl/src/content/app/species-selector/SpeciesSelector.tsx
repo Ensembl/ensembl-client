7c66ff9bdfcf26f4c28c6a71b436e405d1a9e95b
Andrey Azov <andrey@ebi.ac.uk>
2019-05-01 09:54:22 +0100

    Remove outdated comment from SpeciesSelector component

--- src/ensembl/src/content/app/species-selector/SpeciesSelector.tsx
+++ src/ensembl/src/content/app/species-selector/SpeciesSelector.tsx
@@ -2,9 +2,6 @@ import React, { Component } from 'react';
 
 import SpeciesSearchPanel from 'src/content/app/species-selector/containers/species-search-panel/SpeciesSearchPanel';
 
-// TODO:
-// - AppBar should belong to a particular app (e.g. to Species Selector)
-
 class SpeciesSelector extends Component {
   public render() {
     return <SpeciesSearchPanel />;
