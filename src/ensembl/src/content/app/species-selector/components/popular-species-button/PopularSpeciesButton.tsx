65dea51f58e13e49d8e6b8b42ec791edc77a4d72
Andrey Azov <andrey@ebi.ac.uk>
2019-05-17 11:08:56 +0100

    Account for committed species in the popular species button

--- src/ensembl/src/content/app/species-selector/components/popular-species-button/PopularSpeciesButton.tsx
+++ src/ensembl/src/content/app/species-selector/components/popular-species-button/PopularSpeciesButton.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import classNames from 'classnames';
+import find from 'lodash/find';
 
 import { handleSelectedSpecies } from 'src/content/app/species-selector/state/speciesSelectorActions';
-import { getCurrentSpeciesGenomeId } from 'src/content/app/species-selector/state/speciesSelectorSelectors';
+import {
+  getCurrentSpeciesGenomeId,
+  getCommittedSpecies
+} from 'src/content/app/species-selector/state/speciesSelectorSelectors';
 
-import { PopularSpecies } from 'src/content/app/species-selector/types/species-search';
+import {
+  CommittedItem,
+  PopularSpecies
+} from 'src/content/app/species-selector/types/species-search';
 
 // import StrainSelector, {
 //   Strain
@@ -22,6 +29,7 @@ type OwnProps = {
 type Props = {
   species: PopularSpecies;
   isSelected: boolean;
+  isCommitted: boolean;
   handleSelectedSpecies: (species: PopularSpecies) => void;
   // strains: Strain[];
   // onStrainSelect: () => void;
@@ -30,14 +38,11 @@ type Props = {
 // named export is for testing purposes
 // use default export for development
 export const PopularSpeciesButton = (props: Props) => {
-  const { isSelected, species } = props;
+  const { isSelected, isCommitted, species } = props;
 
   const handleClick = () => {
-    const {
-      isSelected,
-      species: { isAvailable }
-    } = props;
-    if (isAvailable && !isSelected) {
+    const { isAvailable } = species;
+    if (isAvailable && !isSelected && !isCommitted) {
       props.handleSelectedSpecies(props.species);
     }
   };
@@ -46,7 +51,7 @@ export const PopularSpeciesButton = (props: Props) => {
 
   const className = classNames(styles.popularSpeciesButton, {
     [styles.popularSpeciesButtonDisabled]: !species.isAvailable,
-    [styles.popularSpeciesButtonActive]: isSelected
+    [styles.popularSpeciesButtonActive]: isSelected || isCommitted
   });
 
   // {isSelected && Boolean(strains.length) && (
@@ -65,7 +70,14 @@ export const PopularSpeciesButton = (props: Props) => {
 // };
 
 const mapStateToProps = (state: RootState, ownProps: OwnProps) => ({
-  isSelected: getCurrentSpeciesGenomeId(state) === ownProps.species.genome_id
+  isSelected: getCurrentSpeciesGenomeId(state) === ownProps.species.genome_id,
+  isCommitted: Boolean(
+    find(
+      getCommittedSpecies(state),
+      (committedItem: CommittedItem) =>
+        committedItem.genome_id === ownProps.species.genome_id
+    )
+  )
 });
 
 const mapDispatchToProps = {
