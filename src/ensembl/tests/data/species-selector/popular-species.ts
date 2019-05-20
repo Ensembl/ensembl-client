6f6236115620f39ba6187ccc8ad57aa78d7a7aed
Andrey Azov <andrey@ebi.ac.uk>
2019-05-20 18:35:25 +0100

    Update svgs

--- src/ensembl/tests/data/species-selector/popular-species.ts
+++ src/ensembl/tests/data/species-selector/popular-species.ts
@@ -22,7 +22,7 @@ export default [
     common_name: 'Human',
     scientific_name: 'Homo sapiens',
     assembly_name: 'GRCh38',
-    image: require('./popular-species/human38_k.svg').default,
+    image: require('./popular-species/human38.svg').default,
     division_ids: [],
     isAvailable: true
   },
@@ -32,7 +32,7 @@ export default [
     common_name: 'Mouse',
     scientific_name: 'Mus musculus',
     assembly_name: 'GRCm38.p6',
-    image: require('./popular-species/mouse_k.svg').default,
+    image: require('./popular-species/mouse.svg').default,
     division_ids: [],
     isAvailable: false
   },
@@ -42,7 +42,7 @@ export default [
     common_name: 'Wheat',
     scientific_name: 'Triticum aestivum',
     assembly_name: 'IWGSC',
-    image: require('./popular-species/wheat_k.svg').default,
+    image: require('./popular-species/wheat.svg').default,
     division_ids: [],
     isAvailable: true
   },
@@ -52,7 +52,7 @@ export default [
     common_name: 'Human',
     scientific_name: 'Homo sapiens',
     assembly_name: 'GRCh37',
-    image: require('./popular-species/human37_k.svg').default,
+    image: require('./popular-species/human37.svg').default,
     division_ids: [],
     isAvailable: true
   },
@@ -62,7 +62,7 @@ export default [
     common_name: 'Zebrafish',
     scientific_name: 'Danio rerio',
     assembly_name: 'GRCz11',
-    image: require('./popular-species/fish_k.svg').default,
+    image: require('./popular-species/fish.svg').default,
     division_ids: [],
     isAvailable: false
   },
@@ -72,7 +72,7 @@ export default [
     common_name: 'Barley',
     scientific_name: 'Hordeum vulgare',
     assembly_name: 'IBSC_v2',
-    image: require('./popular-species/barley_k.svg').default,
+    image: require('./popular-species/barley.svg').default,
     division_ids: [],
     isAvailable: false
   },
@@ -82,7 +82,7 @@ export default [
     common_name: null,
     scientific_name: 'Arabidopsis thaliana',
     assembly_name: 'TAIR10',
-    image: require('./popular-species/arabidopsis_k.svg').default,
+    image: require('./popular-species/arabidopsis.svg').default,
     division_ids: [],
     isAvailable: false
   },
@@ -92,7 +92,7 @@ export default [
     common_name: 'Maize',
     scientific_name: 'Zea mays',
     assembly_name: 'B73_RefGen_v4',
-    image: require('./popular-species/maize_k.svg').default,
+    image: require('./popular-species/maize.svg').default,
     division_ids: [],
     isAvailable: false
   },
@@ -102,7 +102,7 @@ export default [
     common_name: 'Rice',
     scientific_name: 'Oryza sativa Japonica Group',
     assembly_name: 'IRGSP-1.0',
-    image: require('./popular-species/rice_k.svg').default,
+    image: require('./popular-species/rice.svg').default,
     division_ids: [],
     isAvailable: false
   },
@@ -112,7 +112,7 @@ export default [
     common_name: 'Chicken',
     scientific_name: 'Gallus gallus',
     assembly_name: 'GRCg6a',
-    image: require('./popular-species/chicken_k.svg').default,
+    image: require('./popular-species/chicken.svg').default,
     division_ids: [],
     isAvailable: false
   },
@@ -122,7 +122,7 @@ export default [
     common_name: 'Rat',
     scientific_name: 'Rattus norvegicus',
     assembly_name: 'Rnor_6.0',
-    image: require('./popular-species/rat_k.svg').default,
+    image: require('./popular-species/rat.svg').default,
     division_ids: [],
     isAvailable: false
   },
@@ -132,7 +132,7 @@ export default [
     common_name: 'Pig',
     scientific_name: 'Sus scrofa',
     assembly_name: 'Sscrofa11.1',
-    image: require('./popular-species/pig_k.svg').default,
+    image: require('./popular-species/pig.svg').default,
     division_ids: [],
     isAvailable: false
   },
@@ -142,7 +142,7 @@ export default [
     common_name: 'Cow',
     scientific_name: 'Bos taurus',
     assembly_name: 'ARS-UCD1.2',
-    image: require('./popular-species/cow_k.svg').default,
+    image: require('./popular-species/cow.svg').default,
     division_ids: [],
     isAvailable: false
   },
@@ -152,7 +152,7 @@ export default [
     common_name: null,
     scientific_name: 'Magnaporthe oryzae',
     assembly_name: 'MG8',
-    image: require('./popular-species/magnaporthe_k.svg').default,
+    image: require('./popular-species/magnaporthe.svg').default,
     division_ids: [],
     isAvailable: false
   },
@@ -162,7 +162,7 @@ export default [
     common_name: null,
     scientific_name: 'Botrytis cinerea',
     assembly_name: 'ASM83294v1',
-    image: require('./popular-species/botrytis_k.svg').default,
+    image: require('./popular-species/botrytis.svg').default,
     division_ids: [],
     isAvailable: false
   },
@@ -172,7 +172,7 @@ export default [
     common_name: 'Dog',
     scientific_name: 'Canis lupus familiaris',
     assembly_name: 'CanFam3.1',
-    image: require('./popular-species/dog_k.svg').default,
+    image: require('./popular-species/dog.svg').default,
     division_ids: [],
     isAvailable: false
   },
@@ -182,7 +182,7 @@ export default [
     common_name: 'Fission yeast',
     scientific_name: 'Schizosaccharomyces pombe',
     assembly_name: 'ASM294v2',
-    image: require('./popular-species/fission_yeast_k.svg').default,
+    image: require('./popular-species/fission_yeast.svg').default,
     division_ids: [],
     isAvailable: false
   },
@@ -192,7 +192,7 @@ export default [
     common_name: null,
     scientific_name: 'Phaeodactylum tricornutum',
     assembly_name: 'ASM15095v2',
-    image: require('./popular-species/phaeodactylum_k.svg').default,
+    image: require('./popular-species/phaeodactylum.svg').default,
     division_ids: [],
     isAvailable: false
   },
@@ -202,7 +202,7 @@ export default [
     common_name: null,
     scientific_name: 'Drosophila melanogaster',
     assembly_name: 'BDGP6.22',
-    image: require('./popular-species/fruitfly_k.svg').default,
+    image: require('./popular-species/fruitfly.svg').default,
     division_ids: [],
     isAvailable: false
   },
@@ -212,7 +212,7 @@ export default [
     common_name: null,
     scientific_name: 'Escherichia coli str. K-12 substr. MG1655',
     assembly_name: 'ASM584v2',
-    image: require('./popular-species/bacteria_k.svg').default,
+    image: require('./popular-species/bacteria.svg').default,
     division_ids: [],
     isAvailable: true
   },
@@ -222,7 +222,7 @@ export default [
     common_name: null,
     scientific_name: 'Fusarium graminearum str. PH-1',
     assembly_name: 'RR1',
-    image: require('./popular-species/fusarium_graminearum_k.svg').default,
+    image: require('./popular-species/fusarium_graminearum.svg').default,
     division_ids: [],
     isAvailable: false
   },
@@ -232,7 +232,7 @@ export default [
     common_name: 'Sheep',
     scientific_name: 'Ovis aries',
     assembly_name: 'Oar_v3.1',
-    image: require('./popular-species/sheep_k.svg').default,
+    image: require('./popular-species/sheep.svg').default,
     division_ids: [],
     isAvailable: false
   },
@@ -242,7 +242,7 @@ export default [
     common_name: 'Japanese medaka',
     scientific_name: 'Oryzias latipes',
     assembly_name: 'ASM223467v1',
-    image: require('./popular-species/fish_k.svg').default,
+    image: require('./popular-species/fish.svg').default,
     division_ids: [],
     isAvailable: false
   },
@@ -252,7 +252,7 @@ export default [
     common_name: null,
     scientific_name: 'Lactobacillus delbrueckii subsp. bulgaricus 2038',
     assembly_name: 'ASM19116v1',
-    image: require('./popular-species/bacteria_k.svg').default,
+    image: require('./popular-species/bacteria.svg').default,
     division_ids: [],
     isAvailable: false
   },
@@ -262,7 +262,7 @@ export default [
     common_name: 'Brewers’ yeast',
     scientific_name: 'Saccharomyces cerevisiae',
     assembly_name: 'R64-1-1',
-    image: require('./popular-species/brewers_yeast_k.svg').default,
+    image: require('./popular-species/brewers_yeast.svg').default,
     division_ids: [],
     isAvailable: true
   },
@@ -272,7 +272,7 @@ export default [
     common_name: null,
     scientific_name: 'Caenorhabditis elegans',
     assembly_name: 'WBcel235',
-    image: require('./popular-species/c_elegans_k.svg').default,
+    image: require('./popular-species/c_elegans.svg').default,
     division_ids: [],
     isAvailable: true
   },
@@ -282,7 +282,7 @@ export default [
     common_name: null,
     scientific_name: 'Zymoseptoria tritici',
     assembly_name: 'IWGSC',
-    image: require('./popular-species/zymoseptoria_k.svg').default,
+    image: require('./popular-species/zymoseptoria.svg').default,
     division_ids: [],
     isAvailable: false
   },
@@ -292,7 +292,7 @@ export default [
     common_name: 'Horse',
     scientific_name: 'Equus caballus',
     assembly_name: 'EquCab3.0',
-    image: require('./popular-species/horse_k.svg').default,
+    image: require('./popular-species/horse.svg').default,
     division_ids: [],
     isAvailable: false
   },
@@ -302,7 +302,7 @@ export default [
     common_name: 'Red flour beetle',
     scientific_name: 'Tribolium castaneum',
     assembly_name: 'Tcas5.2',
-    image: require('./popular-species/red_flour_beetle_k.svg').default,
+    image: require('./popular-species/red_flour_beetle.svg').default,
     division_ids: [],
     isAvailable: false
   },
@@ -312,7 +312,7 @@ export default [
     common_name: 'Silkworm',
     scientific_name: 'Bombyx mori',
     assembly_name: 'ASM15162v1',
-    image: require('./popular-species/silkworm_k.svg').default,
+    image: require('./popular-species/silkworm.svg').default,
     division_ids: [],
     isAvailable: false
   },
@@ -322,7 +322,7 @@ export default [
     common_name: null,
     scientific_name: 'Aspergillus fumigatus A1163',
     assembly_name: 'ASM15014v1',
-    image: require('./popular-species/aspergillus_k.svg').default,
+    image: require('./popular-species/aspergillus.svg').default,
     division_ids: [],
     isAvailable: false
   },
@@ -332,7 +332,7 @@ export default [
     common_name: null,
     scientific_name: 'Neurospora crassa',
     assembly_name: 'NC12',
-    image: require('./popular-species/neurospora_k.svg').default,
+    image: require('./popular-species/neurospora.svg').default,
     division_ids: [],
     isAvailable: false
   },
@@ -342,7 +342,7 @@ export default [
     common_name: 'Dampwood termite',
     scientific_name: 'Zootermopsis nevadensis',
     assembly_name: 'ZooNev1.0',
-    image: require('./popular-species/termite_k.svg').default,
+    image: require('./popular-species/termite.svg').default,
     division_ids: [],
     isAvailable: false
   },
@@ -352,7 +352,7 @@ export default [
     common_name: null,
     scientific_name: 'Anopheles gambiae',
     assembly_name: 'AgamP4',
-    image: require('./popular-species/mosquito_k.svg').default,
+    image: require('./popular-species/mosquito.svg').default,
     division_ids: [],
     isAvailable: false
   },
@@ -362,7 +362,7 @@ export default [
     common_name: 'Honey bee',
     scientific_name: 'Apis mellifera',
     assembly_name: 'Amel_4.5',
-    image: require('./popular-species/bee_k.svg').default,
+    image: require('./popular-species/bee.svg').default,
     division_ids: [],
     isAvailable: false
   },
@@ -372,7 +372,7 @@ export default [
     common_name: null,
     scientific_name: 'Schizosaccharomyces japonicus',
     assembly_name: 'GCA_000149845.2',
-    image: require('./popular-species/fission_yeast_k.svg').default,
+    image: require('./popular-species/fission_yeast.svg').default,
     division_ids: [],
     isAvailable: false
   },
@@ -382,7 +382,7 @@ export default [
     common_name: 'Macaque',
     scientific_name: 'Macaca mulatta',
     assembly_name: 'Mmul_8.0.1',
-    image: require('./popular-species/macaque_k.svg').default,
+    image: require('./popular-species/macaque.svg').default,
     division_ids: [],
     isAvailable: false
   },
@@ -392,7 +392,7 @@ export default [
     common_name: null,
     scientific_name: 'Phytophtora infestans',
     assembly_name: 'ASM14294v1',
-    image: require('./popular-species/phytophthora_k.svg').default,
+    image: require('./popular-species/phytophthora.svg').default,
     division_ids: [],
     isAvailable: false
   },
@@ -402,7 +402,7 @@ export default [
     common_name: null,
     scientific_name: 'Fusarium oxysporum f.sp. lycopersici',
     assembly_name: 'FO2',
-    image: require('./popular-species/fusarium_oxysporum_k.svg').default,
+    image: require('./popular-species/fusarium_oxysporum.svg').default,
     division_ids: [],
     isAvailable: false
   },
@@ -412,7 +412,7 @@ export default [
     common_name: 'Tilapia',
     scientific_name: 'Oreochromis niloticus',
     assembly_name: 'Orenil1.0',
-    image: require('./popular-species/tilapia_k.svg').default,
+    image: require('./popular-species/tilapia.svg').default,
     division_ids: [],
     isAvailable: false
   },
@@ -422,7 +422,7 @@ export default [
     common_name: null,
     scientific_name: 'Salpingoeca rosetta',
     assembly_name: 'IRGSP-1.0',
-    image: require('./popular-species/salpingoeca_k.svg').default,
+    image: require('./popular-species/salpingoeca.svg').default,
     division_ids: [],
     isAvailable: false
   },
@@ -432,7 +432,7 @@ export default [
     common_name: null,
     scientific_name: 'Plasmodium falciparum 3D7',
     assembly_name: 'EPr1',
-    image: require('./popular-species/plasmodium_k.svg').default,
+    image: require('./popular-species/plasmodium.svg').default,
     division_ids: [],
     isAvailable: false
   }
