0b9acc46b821e7c6c29176b8fbfe009e48abf0b3
Andrey Azov <andrey@ebi.ac.uk>
2019-05-22 17:11:46 +0100

    Start work on the general error screen

--- src/ensembl/src/shared/error-screen/ErrorScreen.tsx
+++ src/ensembl/src/shared/error-screen/ErrorScreen.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 
+import { HomeLink, ReleaseVersion, Copyright } from 'src/header/Header';
+
 import styles from './ErrorScreen.scss';
+import headerStyles from 'src/header/Header.scss';
 
 export const NewTechError = () => {
   return (
     <div className={styles.newTechErrorWrapper}>
       <div className={styles.newTechMessage}>
-        Sorry, but it seems your browser can’t display the new site
+        Sorry, but it seems your browser can’t display the newq site
       </div>
       <a href="https://www.ensembl.org" className={styles.errorLinkButton}>
         Go to Ensembl classic
@@ -14,3 +17,14 @@ export const NewTechError = () => {
     </div>
   );
 };
+
+export const GeneralErrorScreen = () => (
+  <section>
+    <header className={headerStyles.topBar}>
+      <HomeLink />
+      <ReleaseVersion />
+      <Copyright />
+    </header>
+    <div />
+  </section>
+);
