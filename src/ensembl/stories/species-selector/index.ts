69645045526e327cb718d434999b455bc4f3338c
Andrey Azov <andrey@ebi.ac.uk>
2019-05-22 16:13:37 +0100

    Update storybook (remove PopularSpeciesButton story)

--- src/ensembl/stories/species-selector/index.ts
+++ src/ensembl/stories/species-selector/index.ts
@@ -1,4 +1,3 @@
-import './popular-species-button/PopularSpeciesButton.stories';
 import './StrainSelector.stories';
 import './SelectedSpecies.stories';
 import './species-search-field/SpeciesSearchField.stories';
