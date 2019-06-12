7340e948e7c3ee30ef9f3dddb921540b6c193d19
Dan Sheppard <dan@ebi.ac.uk>
2019-06-12 12:53:56 +0100

    Support multi-string marshalling (still supporting old for now).

--- src/assets/browser/app/src/data/httpxferclerk.rs
+++ src/assets/browser/app/src/data/httpxferclerk.rs
@@ -134,7 +134,15 @@ impl PendingXferBatch {
                 out.push(Value::new_from_float(row));
             } else if val.is_string() {
                 out.push(Value::new_from_string(vec![val.as_str().unwrap().to_string()]));
-            }            
+            } else if val.is_object() {
+                if let Some(string) = val.as_object().unwrap().get("string") {
+                    let values : Vec<String> = 
+                            string.as_array().unwrap().iter()
+                            .map(|x| x.as_str().unwrap().to_string())
+                            .collect();
+                    out.push(Value::new_from_string(values));
+                }
+            }
         }
         out
     }
