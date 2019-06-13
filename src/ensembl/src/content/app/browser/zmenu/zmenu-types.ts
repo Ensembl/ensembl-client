6ac1bafa71a241aa3bba41a5d8b0a7f09760278f
Andrey Azov <andrey@ebi.ac.uk>
2019-06-13 10:51:15 +0100

    Update zmenu event types

--- src/ensembl/src/content/app/browser/zmenu/zmenu-types.ts
+++ src/ensembl/src/content/app/browser/zmenu/zmenu-types.ts
@@ -1,3 +1,19 @@
+// relation of the point of interest to the central point of the canvas;
+// a gentle hint by genome browser about where there is the most available space
+// e.g. if the reported side of a point is 'left', zmenu will position itself
+// to the right of this point
+enum Side {
+  LEFT = 'left',
+  RIGHT = 'right',
+  MIDDLE = 'middle'
+}
+
+// coordinates of the point at which zmenu should be pointing
+type AnchorCoordinates = {
+  x: number;
+  y: number;
+};
+
 enum Markup {
   STRONG = 'strong',
   EMPHASIS = 'emphasis',
@@ -52,47 +68,61 @@ type ZmenuLeaveEvent = {
   action: 'zmenu-leave';
 };
 
-// EXAMPLE:
-// [
-//   {
-//     id: "transrcipt:ENST00000380152.7", // (4)
-//     lines: [
-//       [ // first line
-//         [ // "Transcript ENST....."
-//           { text: "Transcript", markup: [] },
-//           { text: "ENST00000380152.7", markup: ["strong"] }
-//         ],
-//         [ // protein coding
-//           { text: "protein coding", markup: [] }
-//         ],
-//         [ // forward strand
-//           { text: "forward strand", markup: [] }
-//         ]
-//       ],
-//       [ // second line
-//         [ // ENST....
-//           { text: "ENST00000380152.7", markup: ["focus"] },
-//           { text: "MANE select", markup: [] }
-//         ]
-//       ]
-//     ]
-//   },{
-//     id: "gene:ENSG00000139618", // (4)
-//     lines: [
-//       [
-//         [ // "Gene BRCA2"
-//           { text: "Gene", markup: [] },
-//           { text: "BRCA2", markup: ["strong"] }
-//         ],
-//         [ // DNA-repair associated
-//           { text: "DNA-repair associated", markup: [] },
-//         ]
-//       ],
-//       [ // fourth line
-//         [
-//           { text: "ENSG00000139618", markup: [] }
-//         ]
-//       ]
-//     ]
-//   }
-// ]
+type ZmenuRepositionEvent = {
+  id: string;
+  action: 'zmenu-update-position';
+  anchor_coordinates: {
+    x: number;
+    y: number;
+  };
+};
+
+/*
+
+======= EXAMPLE OF ZMENU CONTENT ========
+
+[
+  {
+    id: "transrcipt:ENST00000380152.7", // (4)
+    lines: [
+      [ // first line
+        [ // "Transcript ENST....."
+          { text: "Transcript", markup: [] },
+          { text: "ENST00000380152.7", markup: ["strong"] }
+        ],
+        [ // protein coding
+          { text: "protein coding", markup: [] }
+        ],
+        [ // forward strand
+          { text: "forward strand", markup: [] }
+        ]
+      ],
+      [ // second line
+        [ // ENST....
+          { text: "ENST00000380152.7", markup: ["focus"] },
+          { text: "MANE select", markup: [] }
+        ]
+      ]
+    ]
+  },{
+    id: "gene:ENSG00000139618", // (4)
+    lines: [
+      [
+        [ // "Gene BRCA2"
+          { text: "Gene", markup: [] },
+          { text: "BRCA2", markup: ["strong"] }
+        ],
+        [ // DNA-repair associated
+          { text: "DNA-repair associated", markup: [] },
+        ]
+      ],
+      [ // fourth line
+        [
+          { text: "ENSG00000139618", markup: [] }
+        ]
+      ]
+    ]
+  }
+]
+
+*/
