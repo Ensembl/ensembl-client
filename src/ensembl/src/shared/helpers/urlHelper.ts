6bcb5473d7058201aca8ad5483622aeb17a171db
Imran Salam <imran@ebi.ac.uk>
2019-06-14 10:06:19 +0100

    fix broken browser url in urlHelper

--- src/ensembl/src/shared/helpers/urlHelper.ts
+++ src/ensembl/src/shared/helpers/urlHelper.ts
@@ -1,4 +1,4 @@
 export const speciesSelector = () => '/app/species-selector';
 export const customDownload = () => '/app/custom-download';
-export const browser = (genomeId: string, focus: string, region: string) =>
-  `/app/browser/${genomeId}/focus=${focus}&region=${region}`;
+export const browser = (genomeId: string, focus: string, location: string) =>
+  `/app/browser/${genomeId}?focus=${focus}&location=${location}`;
