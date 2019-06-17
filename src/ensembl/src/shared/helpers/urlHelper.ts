07d0b07410c3e50dc9cd761162b17169aca7f4a1
Andrey Azov <andrey.azov@gmail.com>
2019-06-17 23:27:33 +0100

    Fix link to species selector in urlHelper

--- src/ensembl/src/shared/helpers/urlHelper.ts
+++ src/ensembl/src/shared/helpers/urlHelper.ts
@@ -1,3 +1,3 @@
-export const speciesSelector = () => '/app/speicies-selector';
+export const speciesSelector = () => '/app/species-selector';
 export const customDownload = () => '/app/custom-download';
 export const browser = () => '/app/browser';
