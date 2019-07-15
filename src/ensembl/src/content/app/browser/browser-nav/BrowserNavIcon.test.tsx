fd9844dac15ecf321491557347c18c3139f7ae47
Andrey Azov <andrey@ebi.ac.uk>
2019-07-15 09:38:20 +0100

    Update tests for BrowserNavIcon

--- src/ensembl/src/content/app/browser/browser-nav/BrowserNavIcon.test.tsx
+++ src/ensembl/src/content/app/browser/browser-nav/BrowserNavIcon.test.tsx
@@ -2,20 +2,22 @@ import React from 'react';
 import { mount } from 'enzyme';
 
 import { BrowserNavIcon } from './BrowserNavIcon';
-
+import browserMessagingService from 'src/content/app/browser/browser-messaging-service';
 import { browserNavConfig } from '../browserConfig';
 
 describe('<BrowserNavIcon />', () => {
   const browserNavItem = browserNavConfig[0];
 
-  test('fires navigation event when clicked', () => {
-    const mockCallback = jest.fn();
+  test('sends navigation message when clicked', () => {
+    jest.spyOn(browserMessagingService, 'send');
 
     const renderedNavIcon = mount(
       <BrowserNavIcon browserNavItem={browserNavItem} maxState={false} />
     );
 
     renderedNavIcon.find('button').simulate('click');
-    expect(mockCallback).toHaveBeenCalledTimes(1);
+    expect(browserMessagingService.send).toHaveBeenCalledTimes(1);
+
+    (browserMessagingService.send as any).mockRestore();
   });
 });
