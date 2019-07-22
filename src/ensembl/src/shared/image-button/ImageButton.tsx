a7c18a5c937f90b44ad4aae68ac2cb14038a97a0
Andrey Azov <andrey@ebi.ac.uk>
2019-07-22 15:15:17 +0100

    Remove unused useRef from ImageButton component

--- src/ensembl/src/shared/image-button/ImageButton.tsx
+++ src/ensembl/src/shared/image-button/ImageButton.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react';
+import React, { useState } from 'react';
 import defaultStyles from './ImageButton.scss';
 import ImageHolder from './ImageHolder';
 
