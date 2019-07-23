42d82e264ac855b20f7dea1708f4a7827e131dc4
Dan Sheppard <dan@ebi.ac.uk>
2019-07-23 13:40:49 +0100

    Updated WASM.

--- src/ensembl/static/browser/browser.js
+++ src/ensembl/static/browser/browser.js
@@ -19,13 +19,13 @@ if( typeof Rust === "undefined" ) {
         if( typeof process === "object" && typeof process.versions === "object" && typeof process.versions.node === "string" ) {
             var fs = require( "fs" );
             var path = require( "path" );
-            var wasm_path = path.join( __dirname, "/static/browser/browser-81509f3919995d82c425ebf428c15cf5.wasm" );
+            var wasm_path = path.join( __dirname, "/static/browser/browser-b8916c5e268504f39ea319a1f620086e.wasm" );
             var buffer = fs.readFileSync( wasm_path );
             var mod = new WebAssembly.Module( buffer );
             var wasm_instance = new WebAssembly.Instance( mod, instance.imports );
             return instance.initialize( wasm_instance );
         } else {
-            var file = fetch( "/static/browser/browser-81509f3919995d82c425ebf428c15cf5.wasm", {credentials: "same-origin"} );
+            var file = fetch( "/static/browser/browser-b8916c5e268504f39ea319a1f620086e.wasm", {credentials: "same-origin"} );
 
             var wasm_instance = ( typeof WebAssembly.instantiateStreaming === "function"
                 ? WebAssembly.instantiateStreaming( file, instance.imports )
