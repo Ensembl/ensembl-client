71a51168aa04a1c03cb01ad2642ae53cb23049ff
Dan Sheppard <dan@ebi.ac.uk>
2019-07-23 13:25:27 +0100

    Versioned API.

--- src/assets/browser/app/src/util/mod.rs
+++ src/assets/browser/app/src/util/mod.rs
@@ -15,6 +15,6 @@ pub use self::changedetect::ChangeDetect;
 pub use self::rulergenerator::RulerGenerator;
 pub use self::smallest::Smallest;
 pub use self::text::truncate;
-pub use self::thisbuild::build_summary;
+pub use self::thisbuild::{ build_summary, API_VERSION };
 pub use self::thisinstance::{ set_instance_id, get_instance_id };
 pub use self::valuestore::ValueStore;
--- src/assets/browser/app/src/util/thisbuild.rs
+++ src/assets/browser/app/src/util/thisbuild.rs
@@ -1,5 +1,7 @@
 include!(concat!(env!("OUT_DIR"), "/thisbuild.rs"));
 
+pub const API_VERSION: u32 = 1;
+
 use util::get_instance_id;
 
 pub fn build_summary() -> String {
@@ -7,12 +9,13 @@ pub fn build_summary() -> String {
 Built by {} in {} on {} on {}
 Git version {}
 Local changes list: {}
-deploy={} console={} instance_id={}
+deploy={} console={} instance_id={} API_VERSION={}
 {}
 "##,
             VERSION_USER,VERSION_DIR,VERSION_HOSTNAME,VERSION_DATE,
             VERSION_GIT,VERSION_CHANGES,
             cfg!(deploy),cfg!(console),
+            API_VERSION,
             get_instance_id(),
             VERSION_POM
             )
