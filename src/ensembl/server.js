89135ea8be17ade30c58aa0d615b7b129f683911
Andrey Azov <andrey.azov@gmail.com>
2019-07-31 23:02:19 +0100

    Add proxy to local version of production server

--- src/ensembl/server.js
+++ src/ensembl/server.js
@@ -6,8 +6,19 @@ const Koa = require('koa');
 const serve = require('koa-static');
 const history = require('connect-history-api-fallback');
 const convert = require('koa-connect');
+const proxy = require('koa-proxy');
 
 const app = new Koa();
+
+app.use(proxy({
+  host: 'https://staging-2020.ensembl.org',
+  match: /^\/api\//
+}));
+
+app.use(proxy({
+  host: 'https://staging-2020.ensembl.org',
+  match: /^\/browser\//
+}));
 
 app.use(convert(history()));
 app.use(serve(path.join(__dirname, 'dist'), { br: true, gzip: false }));
