0354ff8127c3e6913edd223b503b4f60710a1c99
Andrey Azov <andrey@ebi.ac.uk>
2019-08-01 18:24:42 +0100

    Update workbox config to prevent caching html files

--- src/ensembl/webpack/webpack.config.prod.js
+++ src/ensembl/webpack/webpack.config.prod.js
@@ -103,7 +103,12 @@ const plugins = [
   new WorkboxPlugin.GenerateSW({
     swDest: '../service-worker.js', // save service worker in the root folder (/dist) instead of /dist/static
     clientsClaim: true,
-    skipWaiting: true
+    skipWaiting: true,
+    exclude: [/index.html/],
+    runtimeCaching: [{
+      urlPattern: ({ event }) => event.request.mode === 'navigate',
+      handler: 'NetworkOnly'
+    }]
   }),
 
   new RobotstxtPlugin()
