1193ac2f85247d7659d57a09eca182dfb0df14a3
Manoj <manojpandian@ebi.ac.uk>
2019-08-05 11:49:09 +0100

    Use session storage

--- src/ensembl/src/content/app/custom-download/services/custom-download-storage-service.tsx
+++ src/ensembl/src/content/app/custom-download/services/custom-download-storage-service.tsx
@@ -1,5 +1,6 @@
 import storageService, {
-  StorageServiceInterface
+  StorageServiceInterface,
+  StorageType
 } from 'src/services/storage-service';
 
 import JSONValue from 'src/shared/types/JSON';
@@ -20,6 +21,10 @@ export enum StorageKeys {
   SHOW_PREVIEW = 'customDownload.previewDownload.showSummary'
 }
 
+const options = {
+  storage: StorageType.SESSION_STORAGE
+};
+
 export class CustomDownloadStorageService {
   private storageService: StorageServiceInterface;
 
@@ -28,89 +33,105 @@ export class CustomDownloadStorageService {
   }
 
   public getFilters(): JSONValue {
-    return this.storageService.get(StorageKeys.FILTERS) || {};
+    return this.storageService.get(StorageKeys.FILTERS, options) || {};
   }
 
   public saveFilters(filters: JSONValue) {
-    this.storageService.save(StorageKeys.FILTERS, filters);
+    this.storageService.save(StorageKeys.FILTERS, filters, options);
   }
 
   public getSelectedFilters(): JSONValue {
-    return this.storageService.get(StorageKeys.SELECTED_FILTERS) || {};
+    return this.storageService.get(StorageKeys.SELECTED_FILTERS, options) || {};
   }
 
   public saveSelectedFilters(selectedFilters: JSONValue) {
-    this.storageService.save(StorageKeys.SELECTED_FILTERS, selectedFilters);
+    this.storageService.save(
+      StorageKeys.SELECTED_FILTERS,
+      selectedFilters,
+      options
+    );
   }
 
   public getFiltersUi(): JSONValue {
-    return this.storageService.get(StorageKeys.FILTERS_UI) || {};
+    return this.storageService.get(StorageKeys.FILTERS_UI, options) || {};
   }
 
   public saveFiltersUi(uiState: JSONValue) {
-    this.storageService.save(StorageKeys.FILTERS_UI, uiState);
+    this.storageService.save(StorageKeys.FILTERS_UI, uiState, options);
   }
 
   public getAttributes(): JSONValue {
-    return this.storageService.get(StorageKeys.ATTRIBUTES) || {};
+    return this.storageService.get(StorageKeys.ATTRIBUTES, options) || {};
   }
 
   public saveAttributes(attributes: JSONValue) {
-    this.storageService.save(StorageKeys.ATTRIBUTES, attributes);
+    this.storageService.save(StorageKeys.ATTRIBUTES, attributes, options);
   }
 
   public getSelectedAttributes(): JSONValue {
-    return this.storageService.get(StorageKeys.SELECTED_ATTRIBUTES) || {};
+    return (
+      this.storageService.get(StorageKeys.SELECTED_ATTRIBUTES, options) || {}
+    );
   }
 
   public saveSelectedAttributes(selectedAttributes: JSONValue) {
     this.storageService.save(
       StorageKeys.SELECTED_ATTRIBUTES,
-      selectedAttributes
+      selectedAttributes,
+      options
     );
   }
 
   public getAttributesUi(): JSONValue {
-    return this.storageService.get(StorageKeys.ATTRIBUTES_UI) || {};
+    return this.storageService.get(StorageKeys.ATTRIBUTES_UI, options) || {};
   }
 
   public saveAttributesUi(uiState: JSONValue) {
-    this.storageService.save(StorageKeys.ATTRIBUTES_UI, uiState);
+    this.storageService.save(StorageKeys.ATTRIBUTES_UI, uiState, options);
   }
 
   public getSelectedPreFilter(): string {
-    return this.storageService.get(StorageKeys.SELECTED_PRE_FILTER);
+    return this.storageService.get(StorageKeys.SELECTED_PRE_FILTER, options);
   }
 
   public saveSelectedPreFilter(preFilter: string) {
-    this.storageService.save(StorageKeys.SELECTED_PRE_FILTER, preFilter);
+    this.storageService.save(
+      StorageKeys.SELECTED_PRE_FILTER,
+      preFilter,
+      options
+    );
   }
 
   public getShowPreFilterPanel(): boolean {
-    return this.storageService.get(StorageKeys.SHOW_PRE_FILTER_PANEL);
+    return this.storageService.get(StorageKeys.SHOW_PRE_FILTER_PANEL, options);
   }
 
   public saveShowPreFilterPanel(shouldShowPreFilterPanel: boolean) {
     this.storageService.save(
       StorageKeys.SHOW_PRE_FILTER_PANEL,
-      shouldShowPreFilterPanel
+      shouldShowPreFilterPanel,
+      options
     );
   }
 
   public getSelectedTab(): string {
-    return this.storageService.get(StorageKeys.SELECTED_TAB);
+    return this.storageService.get(StorageKeys.SELECTED_TAB, options);
   }
 
   public saveSelectedTab(selectedTab: string) {
-    this.storageService.save(StorageKeys.SELECTED_TAB, selectedTab);
+    this.storageService.save(StorageKeys.SELECTED_TAB, selectedTab, options);
   }
 
   public getShowPreview(): boolean {
-    return this.storageService.get(StorageKeys.SHOW_PREVIEW) || false;
+    return this.storageService.get(StorageKeys.SHOW_PREVIEW, options) || false;
   }
 
   public saveShowPreview(shouldShowPreview: boolean) {
-    this.storageService.save(StorageKeys.SHOW_PREVIEW, shouldShowPreview);
+    this.storageService.save(
+      StorageKeys.SHOW_PREVIEW,
+      shouldShowPreview,
+      options
+    );
   }
 }
 
