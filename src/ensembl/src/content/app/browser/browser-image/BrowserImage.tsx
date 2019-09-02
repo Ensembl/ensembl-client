a2b8630e4db296f3040f2d4711e85166b3be70ac
Andrey Azov <andrey@ebi.ac.uk>
2019-09-02 13:52:43 +0100

    Remove refs from Browser component (#149)
    - remove lastGenomeIdRef entirely
    - move browserRef from Browser component to BrowserImage
    - move code related to reporting track states to BrowserCogList,
       and use postMessage for communication with GenomeBrowser

--- src/ensembl/src/content/app/browser/browser-image/BrowserImage.tsx
+++ src/ensembl/src/content/app/browser/browser-image/BrowserImage.tsx
@@ -1,6 +1,6 @@
 import React, {
   FunctionComponent,
-  RefObject,
+  useRef,
   useEffect,
   useCallback
 } from 'react';
@@ -13,8 +13,6 @@ import { ZmenuController } from 'src/content/app/browser/zmenu';
 
 import browserMessagingService from 'src/content/app/browser/browser-messaging-service';
 import {
-  getTrackConfigNames,
-  getTrackConfigLabel,
   getBrowserCogTrackList,
   getBrowserNavOpened,
   getBrowserActivated
@@ -42,8 +40,6 @@ import { BROWSER_CONTAINER_ID } from '../browser-constants';
 type StateProps = {
   browserCogTrackList: CogList;
   browserNavOpened: boolean;
-  trackConfigNames: any;
-  trackConfigLabel: any;
   browserActivated: boolean;
 };
 
@@ -59,7 +55,6 @@ type DispatchProps = {
 };
 
 type OwnProps = {
-  browserRef: RefObject<HTMLDivElement>;
   trackStates: TrackStates;
 };
 
@@ -83,6 +78,7 @@ const parseLocation = (location: ChrLocation) => {
 export const BrowserImage: FunctionComponent<BrowserImageProps> = (
   props: BrowserImageProps
 ) => {
+  const browserRef = useRef<HTMLDivElement>(null);
   const listenBpaneOut = useCallback((payload: BpaneOutPayload) => {
     const ensObjectId = payload.focus;
     const navIconStates = payload.bumper as BrowserNavStates;
@@ -123,54 +119,12 @@ export const BrowserImage: FunctionComponent<BrowserImageProps> = (
   }, []);
 
   useEffect(() => {
-    const currentEl: HTMLDivElement = props.browserRef
-      .current as HTMLDivElement;
     props.activateBrowser();
 
     return function cleanup() {
-      if (currentEl && currentEl.ownerDocument) {
-        props.updateBrowserActivated(false);
-      }
+      props.updateBrowserActivated(false);
     };
-  }, [props.browserRef]);
-
-  useEffect(() => {
-    if (props.browserCogTrackList) {
-      const ons: string[] = [];
-      const offs: string[] = [];
-
-      /* what the frontend and backend call labels and names is flipped */
-      Object.keys(props.browserCogTrackList).map((name) => {
-        /* undefined means not seen means on for names */
-        if (props.trackConfigNames[name]) {
-          ons.push(name + ':label');
-        } else {
-          offs.push(name + ':label');
-        }
-        /* undefined means not seen means off for labels */
-        if (props.trackConfigLabel[name] !== false) {
-          ons.push(name + ':names');
-        } else {
-          offs.push(name + ':names');
-        }
-      });
-      const stateEvent = new CustomEvent('bpane', {
-        bubbles: true,
-        detail: {
-          off: offs,
-          on: ons
-        }
-      });
-      if (props.browserRef.current) {
-        props.browserRef.current.dispatchEvent(stateEvent);
-      }
-    }
-  }, [
-    props.trackConfigNames,
-    props.trackConfigLabel,
-    props.browserRef,
-    props.browserCogTrackList
-  ]);
+  }, []);
 
   return (
     <>
@@ -183,10 +137,10 @@ export const BrowserImage: FunctionComponent<BrowserImageProps> = (
         <div
           id={BROWSER_CONTAINER_ID}
           className={getBrowserImageClasses(props.browserNavOpened)}
-          ref={props.browserRef}
+          ref={browserRef}
         />
         <BrowserCogList />
-        <ZmenuController browserRef={props.browserRef} />
+        <ZmenuController browserRef={browserRef} />
       </div>
     </>
   );
@@ -205,8 +159,6 @@ function getBrowserImageClasses(browserNavOpened: boolean): string {
 const mapStateToProps = (state: RootState): StateProps => ({
   browserCogTrackList: getBrowserCogTrackList(state),
   browserNavOpened: getBrowserNavOpened(state),
-  trackConfigLabel: getTrackConfigLabel(state),
-  trackConfigNames: getTrackConfigNames(state),
   browserActivated: getBrowserActivated(state)
 });
 
