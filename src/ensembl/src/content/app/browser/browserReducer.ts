e09f816239d1e109ffee5f25ab795ca4dc074c56
Andrey Azov <andrey@ebi.ac.uk>
2019-09-23 09:42:27 +0100

    Fix initialization of track panel state (#169)

--- src/ensembl/src/content/app/browser/browserReducer.ts
+++ src/ensembl/src/content/app/browser/browserReducer.ts
@@ -47,8 +47,6 @@ export function browserEntity(
       }
       return newState;
     }
-    case getType(browserActions.updateBrowserActiveGenomeId):
-      return { ...state, activeGenomeId: action.payload };
     case getType(browserActions.updateBrowserActiveEnsObjectIds):
       return { ...state, activeEnsObjectIds: action.payload };
     case getType(browserActions.updateTrackStates):
