6ad1b2a3ecebabf451c05404a0aa8d0f6b4b9a6a
Manoj <manojpandian@ebi.ac.uk>
2019-10-29 13:23:38 +0000

    USe DEPLOYENV to build the host

--- src/ensembl/robots-txt.config.js
+++ src/ensembl/robots-txt.config.js
@@ -1,4 +1,4 @@
 module.exports = {
   policy: [{ userAgent: '*', disallow: '/' }],
-  host: 'http://2020.ensembl.org'
+  host: process.env.DEPLOYENV === 'prod' ? '2020.ensembl.org' : process.env.DEPLOYENV + '-2020.ensembl.org'
 };
