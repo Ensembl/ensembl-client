fad2ddead938ad39ba348edc88fc5e604b6799d4
Andrey Azov <andrey@ebi.ac.uk>
2019-11-06 16:25:46 +0000

    Remove IE11-specific code (#194)

--- src/ensembl/webpack/webpack.common.js
+++ src/ensembl/webpack/webpack.common.js
@@ -35,13 +35,6 @@ module.exports = (isDev, moduleRules, plugins) => ({
         exclude: /node_modules/
       },
 
-      // these libraries are distributed in a way that does not support IE11, and so require compilation
-      {
-        test: /.js$/,
-        loader: 'babel-loader',
-        include: /react-spring/
-      },
-
       // the loaders for styling
       // there are two sets of them: for global and component styles
       // a scss file will first be loaded via sass loader and transpiled
