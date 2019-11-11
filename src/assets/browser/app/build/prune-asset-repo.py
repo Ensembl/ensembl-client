4623910b5e2b20bb0b9dbca8f122372bfb4d2a3b
Dan Sheppard <dan@ebi.ac.uk>
2019-11-11 19:13:31 +0000

    Half-finished prune script.

---
+++ src/assets/browser/app/build/prune-asset-repo.py
@@ -0,0 +1,114 @@
+#! /usr/bin/env python3
+
+import os, re, subprocess, sys, tempfile
+
+CLIENT_REPO_NAME="ensembl-client"
+CLIENT_REPO="https://github.com/Ensembl/{0}.git"
+
+ASSET_REPO_NAME="throwaway-ensembl-static-assets"
+ASSET_REPO="git@github.com:azangru/{0}.git"
+
+depline_re = re.compile(r'.*"ensembl-genome-browser":.*#(.*)"')
+tag_re = re.compile(r'tag: (wasm/.*)')
+
+# Generic means of running stuff
+
+def run(*args,**kwargs):
+  if 'stderr' not in kwargs:
+    kwargs['stderr'] = subprocess.PIPE
+  fail_ok = False
+  if 'fail_ok' in kwargs:
+    fail_ok = kwargs['fail_ok']
+    del kwargs['fail_ok']
+  print(args,kwargs)
+  proc = subprocess.run(*args,**kwargs)
+  if proc.stderr != None:
+    sys.stderr.write(proc.stderr.decode("utf8"))
+  if proc.returncode != 0 and not fail_ok:
+    sys.stderr.write("Cannot run {0} {1}".format(args,kwargs))
+    sys.exit(1)
+  return proc.stdout.decode("utf8") if proc.stdout else ""
+
+# Get list of tags in use
+tags_in_use = set()
+with tempfile.TemporaryDirectory() as tmpdirname:
+  os.chdir(tmpdirname)
+  repo = CLIENT_REPO.format(CLIENT_REPO_NAME)
+  print(tmpdirname)
+  run(["git","clone",repo])
+  os.chdir(CLIENT_REPO_NAME)
+  blobs = run(["git","rev-list","--all","--objects","src/ensembl/package.json"],stdout=subprocess.PIPE)
+  for blob in blobs.split("\n"):
+    parts = blob.strip().split(' ',1)
+    if len(parts) < 2 or parts[1] != "src/ensembl/package.json":
+      continue
+    contents = run(["git","cat-file","-p",parts[0]],stdout=subprocess.PIPE)
+    for line in contents.split("\n"):
+      m = depline_re.match(line)
+      if m:
+        tags_in_use.add(m.group(1))
+
+print('tags in use',tags_in_use)
+  
+# Ability to restore tags to rightful commits after rebase (kills tags)
+browser_re = re.compile(r'browser-(.*)\.wasm')
+def restore_tags():
+  commits=run(["git","log","--pretty=format:%H"],stdout=subprocess.PIPE)
+  seen = set()
+  for commit in commits.split("\n"):
+    files = run(["git","ls-tree",commit],stdout=subprocess.PIPE)
+    for line in files.split("\n"):
+      parts = line.split(None,3)
+      if len(parts) < 4:
+        continue
+      m = browser_re.match(parts[3])
+      if m:
+        tagname = "wasm/{0}".format(m.group(1))
+        run(["git","tag","-d",tagname],fail_ok=True)
+        run(["git","tag",tagname,commit])
+
+with tempfile.TemporaryDirectory() as tmpdirname:
+#if 0:
+  #tmpdirname = "/tmp/c"
+  # Checkout asset repo
+  os.chdir(tmpdirname)
+  repo = ASSET_REPO.format(ASSET_REPO_NAME)
+  print(tmpdirname)
+  run(["git","clone",repo])
+  os.chdir(ASSET_REPO_NAME)
+
+  restore_tags()
+
+  # get squash commit list
+  origin = None
+  logs=run(["git","log","--pretty=format:%H %D"],stdout=subprocess.PIPE)
+  unused = set()
+  sed = []
+  for log in logs.split("\n"):
+    in_use = False
+    parts = log.split(" ",1)
+    for ref in parts[1].split(', '):
+      m = tag_re.match(ref) 
+      if m: # and m.group(1) in tags_in_use:
+        in_use = True
+      if 'tag: root' in ref:
+        origin = parts[0]
+        in_use = True
+        break
+    if not in_use:
+      unused.add(parts[0])
+  print('unused',unused)
+  if origin == None:
+    sys.stderr.write("Could not find origin tag")
+    sys.exit(1)
+
+  # 
+
+ 
+  # recommit old assets
+#  for tag in tag_order:
+#    cmd = """GIT_SEQUENCE_EDITOR="sed -i -e 's/^pick e751a6/squash e751a6/'"  git rebase -i e751a6^^""";
+#    print(cmd)
+#  run(["git","log"])
+
+
