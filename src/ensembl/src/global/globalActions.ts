2462617d8c13060025f91a1372e0785bd86f76c8
Andrey Azov <andrey@ebi.ac.uk>
2019-11-19 18:29:10 +0000

    Replace third-party useResizeObserver with our own hook

--- src/ensembl/src/global/globalActions.ts
+++ src/ensembl/src/global/globalActions.ts
@@ -1,7 +1,28 @@
 import { createAction } from 'typesafe-actions';
+import { ActionCreator, Action } from 'redux';
+import { ThunkAction } from 'redux-thunk';
 
 import { BreakpointWidth } from './globalConfig';
+import { getBreakpointWidth } from './globalSelectors';
+import { getBreakpoint } from './globalHelper';
 
-export const updateBreakpointWidth = createAction(
+import { RootState } from 'src/store';
+
+export const setBreakpointWidth = createAction(
   'browser/update-breakpoint-width'
 )<BreakpointWidth>();
+
+export const updateBreakpointWidth: ActionCreator<ThunkAction<
+  void,
+  any,
+  null,
+  Action<string>
+>> = (width: number) => async (dispatch, getState: () => RootState) => {
+  const state = getState();
+  const currentBreakpointWidth = getBreakpointWidth(state);
+  const newBreakpointWidth = getBreakpoint(width);
+
+  if (newBreakpointWidth !== currentBreakpointWidth) {
+    dispatch(setBreakpointWidth(newBreakpointWidth));
+  }
+};
