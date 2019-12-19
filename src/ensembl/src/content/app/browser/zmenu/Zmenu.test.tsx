295550764c64b8706adc476616d75f744ad40e13
Imran Salam <imran@ebi.ac.uk>
2019-12-19 17:02:04 +0000

    remove unused imports

--- src/ensembl/src/content/app/browser/zmenu/Zmenu.test.tsx
+++ src/ensembl/src/content/app/browser/zmenu/Zmenu.test.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { mount } from 'enzyme';
 import faker from 'faker';
 
-import Zmenu, { ZmenuProps, Tip } from './Zmenu';
+import Zmenu, { ZmenuProps } from './Zmenu';
 import ZmenuContent from './ZmenuContent';
 
 import { createZmenuContent } from 'tests/fixtures/browser';
