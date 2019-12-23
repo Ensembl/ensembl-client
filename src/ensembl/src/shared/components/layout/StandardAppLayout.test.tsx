7551f520b685434d13b5ee4e15b6f697b84df219
Andrey Azov <andrey@ebi.ac.uk>
2019-12-23 13:01:41 +0000

    Add standard app layout (#220)
    Add a common grid-like layout to be shared by different applications (e.g. Genome browser
    and Entity viewer)

---
+++ src/ensembl/src/shared/components/layout/StandardAppLayout.test.tsx
@@ -0,0 +1,223 @@
+import React from 'react';
+import { mount, render } from 'enzyme';
+import faker from 'faker';
+
+import StandardAppLayout from './StandardAppLayout';
+
+const MainContent = () => (
+  <div className="mainContent">This is main content</div>
+);
+const SidebarContent = () => (
+  <div className="sidebarContent">This is sidebar content</div>
+);
+const TopbarContent = () => (
+  <div className="topbarContent">This is topbar content</div>
+);
+
+const SidebarToolstripContent = () => (
+  <div className="toolstripContent">This is topbar content</div>
+);
+
+const DrawerContent = () => (
+  <div className="drawerContent">This is drawer content</div>
+);
+
+const sidebarNavigationLinks = [...Array(3)].map(() => ({
+  label: faker.random.word(),
+  isActive: false
+}));
+const activeSidebarNavLinkIndex = Math.floor(
+  Math.random() * sidebarNavigationLinks.length
+);
+sidebarNavigationLinks[activeSidebarNavLinkIndex].isActive = true;
+
+const minimalProps = {
+  mainContent: <MainContent />,
+  sidebarContent: <SidebarContent />,
+  sidebarToolstripContent: <SidebarToolstripContent />,
+  topbarContent: <TopbarContent />,
+  isSidebarOpen: true,
+  sidebarNavigation: {
+    links: sidebarNavigationLinks,
+    onChange: jest.fn()
+  },
+  onSidebarToggle: jest.fn()
+};
+
+describe('StandardAppLayout', () => {
+  afterEach(() => {
+    jest.resetAllMocks();
+  });
+
+  describe('rendering', () => {
+    it('renders the main content in the main section', () => {
+      const wrapper = render(<StandardAppLayout {...minimalProps} />);
+      expect(wrapper.find('.main').find('.mainContent').length).toBe(1);
+    });
+
+    it('renders the top bar content in the top bar', () => {
+      const wrapper = render(<StandardAppLayout {...minimalProps} />);
+      expect(wrapper.find('.topBar').find('.topbarContent').length).toBe(1);
+    });
+
+    it('renders the sidebar content in the sidebar', () => {
+      const wrapper = render(<StandardAppLayout {...minimalProps} />);
+      expect(wrapper.find('.sideBar').find('.sidebarContent').length).toBe(1);
+    });
+
+    it('renders sidebar toolstrip content in the sidebar toolstrip', () => {
+      const wrapper = render(<StandardAppLayout {...minimalProps} />);
+      expect(
+        wrapper.find('.sideBarToolstripContent').find('.toolstripContent')
+          .length
+      ).toBe(1);
+    });
+
+    describe('with drawer', () => {
+      const props = {
+        ...minimalProps,
+        isDrawerOpen: false,
+        drawerContent: <DrawerContent />,
+        onDrawerClose: jest.fn()
+      };
+
+      it('renders drawer content in the drawer', () => {
+        const wrapper = render(<StandardAppLayout {...props} />);
+        expect(wrapper.find('.drawer').find('.drawerContent').length).toBe(1);
+      });
+
+      it('applies correct classes to the sidebar/drawer wrapper', () => {
+        let wrapper, sidebarWrapper;
+        const closedSidebarProps = {
+          ...props,
+          isSidebarOpen: false
+        };
+        wrapper = render(<StandardAppLayout {...closedSidebarProps} />);
+        sidebarWrapper = wrapper.find('.sideBarWrapper');
+        expect(sidebarWrapper.hasClass('sideBarWrapperOpen')).toBe(false);
+        expect(sidebarWrapper.hasClass('sideBarWrapperClosed')).toBe(true);
+        expect(sidebarWrapper.hasClass('sideBarWrapperDrawerOpen')).toBe(false);
+
+        const openSidebarProps = props;
+        wrapper = render(<StandardAppLayout {...openSidebarProps} />);
+        sidebarWrapper = wrapper.find('.sideBarWrapper');
+        expect(sidebarWrapper.hasClass('sideBarWrapperOpen')).toBe(true);
+        expect(sidebarWrapper.hasClass('sideBarWrapperClosed')).toBe(false);
+        expect(sidebarWrapper.hasClass('sideBarWrapperDrawerOpen')).toBe(false);
+
+        const openDrawerProps = {
+          ...props,
+          isDrawerOpen: true
+        };
+        wrapper = render(<StandardAppLayout {...openDrawerProps} />);
+        sidebarWrapper = wrapper.find('.sideBarWrapper');
+        expect(sidebarWrapper.hasClass('sideBarWrapperOpen')).toBe(true);
+        expect(sidebarWrapper.hasClass('sideBarWrapperClosed')).toBe(false);
+        expect(sidebarWrapper.hasClass('sideBarWrapperDrawerOpen')).toBe(true);
+      });
+    });
+
+    describe('navigation tabs', () => {
+      it('renders navigation tabs', () => {
+        const wrapper = render(<StandardAppLayout {...minimalProps} />);
+        expect(wrapper.find('.sidebarTab').length).toBe(
+          minimalProps.sidebarNavigation.links.length
+        );
+      });
+
+      it('adds active class name to a tab if sidebar is open', () => {
+        const wrapper = render(<StandardAppLayout {...minimalProps} />);
+        wrapper
+          .find(`.sidebarTab`)
+          .toArray()
+          .forEach((element, index) => {
+            const shouldBeActive = index === activeSidebarNavLinkIndex;
+            expect(element.attribs.class.includes('sidebarTabActive')).toBe(
+              shouldBeActive
+            );
+          });
+      });
+
+      it('does not add active class name to tabs when sidebar is closed', () => {
+        const wrapper = render(
+          <StandardAppLayout {...{ ...minimalProps, isSidebarOpen: false }} />
+        );
+        wrapper
+          .find(`.sidebarTab`)
+          .toArray()
+          .forEach((element) => {
+            expect(element.attribs.class.includes('sidebarTabActive')).toBe(
+              false
+            );
+          });
+      });
+
+      it('does not add active class name to tabs when drawer is opened', () => {
+        const wrapper = render(
+          <StandardAppLayout {...{ ...minimalProps, isDrawerOpen: true }} />
+        );
+        wrapper
+          .find(`.sidebarTab`)
+          .toArray()
+          .forEach((element) => {
+            expect(element.attribs.class.includes('sidebarTabActive')).toBe(
+              false
+            );
+          });
+      });
+    });
+  });
+
+  describe('behaviour', () => {
+    const commonProps = {
+      ...minimalProps,
+      isDrawerOpen: false,
+      drawerContent: <DrawerContent />,
+      onDrawerClose: jest.fn()
+    };
+
+    describe('with closed drawer', () => {
+      const props = commonProps;
+
+      it('calls onSidebarToggle when sidebar toggle button is clicked', () => {
+        const wrapper = mount(<StandardAppLayout {...props} />);
+        wrapper.find('.sidebarModeToggleChevron').simulate('click');
+
+        expect(props.onSidebarToggle).toHaveBeenCalledTimes(1);
+
+        // do the same for closed sidebar
+        wrapper.setProps({ isSidebarOpen: false });
+        wrapper.find('.sidebarModeToggleChevron').simulate('click');
+
+        expect(props.onSidebarToggle).toHaveBeenCalledTimes(2);
+        expect(props.onDrawerClose).not.toHaveBeenCalled();
+      });
+    });
+
+    describe('with open drawer', () => {
+      const props = { ...commonProps, isDrawerOpen: true };
+
+      it('closes drawer when sidebar toggle button is clicked', () => {
+        const wrapper = mount(<StandardAppLayout {...props} />);
+        wrapper.find('.sidebarModeToggleChevron').simulate('click');
+
+        expect(props.onDrawerClose).toHaveBeenCalledTimes(1);
+        expect(props.onSidebarToggle).not.toHaveBeenCalled();
+      });
+
+      it('closes drawer when drawer close button is clicked', () => {
+        const wrapper = mount(<StandardAppLayout {...props} />);
+        wrapper.find('.drawerClose').simulate('click');
+
+        expect(props.onDrawerClose).toHaveBeenCalledTimes(1);
+      });
+
+      it('closes drawer when drawer’s transparent window is clicked', () => {
+        const wrapper = mount(<StandardAppLayout {...props} />);
+        wrapper.find('.drawerWindow').simulate('click');
+
+        expect(props.onDrawerClose).toHaveBeenCalledTimes(1);
+      });
+    });
+  });
+});
---
+++ src/ensembl/src/shared/components/layout/StandardAppLayout.tsx
@@ -0,0 +1,151 @@
+import React, { ReactNode } from 'react';
+import classNames from 'classnames';
+import noop from 'lodash/noop';
+
+import { ReactComponent as Chevron } from 'static/img/shared/chevron-right.svg';
+import { ReactComponent as CloseIcon } from 'static/img/shared/close.svg';
+
+import styles from './StandardAppLayout.scss';
+
+enum SidebarModeToggleAction {
+  OPEN = 'open',
+  CLOSE = 'close'
+}
+
+type SidebarModeToggleProps = {
+  onClick: () => void;
+  showAction: SidebarModeToggleAction;
+};
+
+type SidebarNavigationProps = {
+  links: Array<{
+    label: string;
+    isActive: boolean;
+  }>;
+  onChange: (index: number) => void;
+};
+
+type StandardAppLayoutProps = {
+  mainContent: ReactNode;
+  sidebarContent: ReactNode;
+  sidebarToolstripContent?: ReactNode;
+  sidebarNavigation: SidebarNavigationProps;
+  topbarContent: ReactNode;
+  isSidebarOpen: boolean;
+  onSidebarToggle: () => void;
+  isDrawerOpen: boolean;
+  drawerContent?: ReactNode;
+  onDrawerClose: () => void;
+};
+
+const StandardAppLayout = (props: StandardAppLayoutProps) => {
+  const mainClassnames = classNames(
+    styles.main,
+    { [styles.mainDefault]: props.isSidebarOpen },
+    { [styles.mainFullWidth]: !props.isSidebarOpen }
+  );
+
+  const sidebarWrapperClassnames = classNames(
+    styles.sideBarWrapper,
+    { [styles.sideBarWrapperOpen]: props.isSidebarOpen },
+    { [styles.sideBarWrapperClosed]: !props.isSidebarOpen },
+    {
+      [styles.sideBarWrapperDrawerOpen]: props.isDrawerOpen ?? false
+    }
+  );
+
+  return (
+    <div className={styles.standardAppLayout}>
+      <div className={styles.topBar}>
+        {props.topbarContent}
+        <SidebarTabs {...props} />
+      </div>
+      <div className={styles.mainWrapper}>
+        <div className={mainClassnames}>{props.mainContent}</div>
+        <div className={sidebarWrapperClassnames}>
+          {props.isDrawerOpen && <DrawerWindow onClick={props.onDrawerClose} />}
+          <div className={styles.sideBarToolstrip}>
+            <SidebarModeToggle
+              onClick={
+                props.isDrawerOpen ? props.onDrawerClose : props.onSidebarToggle
+              }
+              showAction={
+                props.isSidebarOpen
+                  ? SidebarModeToggleAction.CLOSE
+                  : SidebarModeToggleAction.OPEN
+              }
+            />
+            <div className={styles.sideBarToolstripContent}>
+              {props.sidebarToolstripContent}
+            </div>
+          </div>
+          <div className={styles.sideBar}>{props.sidebarContent}</div>
+          <div className={styles.drawer}>
+            <CloseIcon
+              className={styles.drawerClose}
+              onClick={props.onDrawerClose}
+            />
+            {props.drawerContent || null}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+StandardAppLayout.defaultProps = {
+  isDrawerOpen: false,
+  onDrawerClose: noop
+};
+
+const SidebarModeToggle = (props: SidebarModeToggleProps) => {
+  const chevronClasses = classNames(styles.sidebarModeToggleChevron, {
+    [styles.sidebarModeToggleChevronOpen]:
+      props.showAction === SidebarModeToggleAction.OPEN
+  });
+
+  return (
+    <div className={styles.sidebarModeToggle}>
+      <Chevron className={chevronClasses} onClick={props.onClick} />
+    </div>
+  );
+};
+
+const SidebarTabs = (props: StandardAppLayoutProps) => {
+  const links = props.sidebarNavigation.links.map((link, index) => {
+    const isLinkActive = shouldLinkBeActive(
+      link,
+      props.isSidebarOpen,
+      props.isDrawerOpen
+    );
+    return (
+      <span
+        key={index}
+        className={classNames(styles.sidebarTab, {
+          [styles.sidebarTabActive]: isLinkActive
+        })}
+        {...(!isLinkActive
+          ? { onClick: () => props.sidebarNavigation.onChange(index) }
+          : null)}
+      >
+        {link.label}
+      </span>
+    );
+  });
+
+  return <div className={styles.sidebarTabs}>{links}</div>;
+};
+
+const shouldLinkBeActive = (
+  link: { isActive: boolean },
+  isSidebarOpen: boolean,
+  isDrawerOpen: boolean
+) => link.isActive && isSidebarOpen && !isDrawerOpen;
+
+// left-most transparent part of the drawer allowing the user to see what element is behind the drawer;
+// when clicked, will close the drawer
+const DrawerWindow = (props: { onClick: () => void }) => {
+  return <div className={styles.drawerWindow} onClick={props.onClick} />;
+};
+
+export default StandardAppLayout;
