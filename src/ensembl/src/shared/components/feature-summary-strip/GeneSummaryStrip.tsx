54257c6d550b204a8aaa37d4f2cf6e023a32e50f
Andrey Azov <andrey@ebi.ac.uk>
2020-01-07 16:22:50 +0000

    Add several variants of gene summary strip

--- src/ensembl/src/shared/components/feature-summary-strip/GeneSummaryStrip.tsx
+++ src/ensembl/src/shared/components/feature-summary-strip/GeneSummaryStrip.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import classNames from 'classnames';
 
+import { BreakpointWidth } from 'src/global/globalConfig';
 import { getDisplayStableId } from 'src/shared/state/ens-object/ensObjectHelpers';
 import { getFormattedLocation } from 'src/shared/helpers/regionFormatter';
 
@@ -11,25 +12,60 @@ import { EnsObject } from 'src/shared/state/ens-object/ensObjectTypes';
 type Props = {
   gene: EnsObject;
   isGhosted?: boolean;
+  viewportWidth: BreakpointWidth;
 };
 
-const GeneSummaryStrip = ({ gene, isGhosted }: Props) => {
+const GeneSummaryStrip = ({ gene, isGhosted, viewportWidth }: Props) => {
   const stripClasses = classNames(styles.featureSummaryStrip, {
     [styles.featureSummaryStripGhosted]: isGhosted
   });
 
-  return (
-    <div className={stripClasses}>
-      <div>
-        <span className={styles.featureSummaryStripLabel}>Gene</span>
-        <span className={styles.featureNameEmphasized}>{gene.label}</span>
-        <span>{getDisplayStableId(gene)}</span>
-      </div>
-      {gene.bio_type && <div>{gene.bio_type.toLowerCase()}</div>}
-      <div>{gene.strand} strand</div>
-      <div>{getFormattedLocation(gene.location)}</div>
-    </div>
-  );
+  const content =
+    viewportWidth > BreakpointWidth.LAPTOP ? (
+      <FullContent gene={gene} />
+    ) : viewportWidth > BreakpointWidth.TABLET ? (
+      <CompactContent gene={gene} />
+    ) : (
+      <MinimalContent gene={gene} />
+    );
+
+  return <div className={stripClasses}>{content}</div>;
 };
+
+const MinimalContent = (props: { gene: EnsObject }) => (
+  <div>
+    <span className={styles.featureSummaryStripLabel}>Gene</span>
+    {props.gene.label ? (
+      <span className={styles.featureNameEmphasized}>{props.gene.label}</span>
+    ) : (
+      <span>{getDisplayStableId(props.gene)}</span>
+    )}
+  </div>
+);
+
+const CompactContent = (props: { gene: EnsObject }) => (
+  <div>
+    <span className={styles.featureSummaryStripLabel}>Gene</span>
+    {props.gene.label && (
+      <span className={styles.featureNameEmphasized}>{props.gene.label}</span>
+    )}
+    <span>{getDisplayStableId(props.gene)}</span>
+  </div>
+);
+
+const FullContent = ({ gene }: { gene: EnsObject }) => (
+  <>
+    <div>
+      <span className={styles.featureSummaryStripLabel}>Gene</span>
+      {gene.label && (
+        <span className={styles.featureNameEmphasized}>{gene.label}</span>
+      )}
+      <span>{getDisplayStableId(gene)}</span>
+    </div>
+    {gene.bio_type && <div>{gene.bio_type.toLowerCase()}</div>}
+    <div>{gene.strand} strand</div>
+    <div>{getFormattedLocation(gene.location)}</div>
+  </>
+);
 
 export default GeneSummaryStrip;
