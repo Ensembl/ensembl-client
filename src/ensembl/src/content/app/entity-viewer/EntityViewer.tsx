500975d89899e7b5cc9cfcebbe981411a86f81ec
Andrey Azov <andrey.azov@gmail.com>
2020-01-12 23:11:28 +0000

    Show example links for EntityViewer

--- src/ensembl/src/content/app/entity-viewer/EntityViewer.tsx
+++ src/ensembl/src/content/app/entity-viewer/EntityViewer.tsx
@@ -1,10 +1,13 @@
 import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 
 import { BreakpointWidth } from 'src/global/globalConfig';
+import * as urlHelper from 'src/shared/helpers/urlHelper';
 
 import { getBreakpointWidth } from 'src/global/globalSelectors';
+import { getExampleGenes } from 'src/shared/state/ens-object/ensObjectSelectors';
+import { getEntityViewerActiveGenomeId } from 'src/content/app/entity-viewer/state/entityViewerSelectors';
 
 import { fetchGenomeData } from 'src/shared/state/genome/genomeActions';
 import { setDataFromUrl } from 'src/content/app/entity-viewer/state/entityViewerActions';
@@ -15,8 +18,11 @@ import EntityViewerAppBar from 'src/content/app/entity-viewer/components/entity-
 import styles from './EntityViewer.scss';
 
 import { RootState } from 'src/store';
+import { EnsObject } from 'src/shared/state/ens-object/ensObjectTypes';
 
 type Props = {
+  activeGenomeId: string | null;
+  exampleGenes: EnsObject[];
   viewportWidth: BreakpointWidth;
   setDataFromUrl: (params: EntityViewerParams) => void;
   fetchGenomeData: (genomeId: string) => void;
@@ -37,23 +43,53 @@ const EntityViewer = (props: Props) => {
   return (
     <div className={styles.entityViewer}>
       <EntityViewerAppBar />
-      <StandardAppLayout
-        mainContent={<div>Main content is coming...</div>}
-        sidebarContent={<div>Sidebar content is coming...</div>}
-        sidebarNavigation={<div>Sidebar navigation goes here</div>}
-        topbarContent={<div>Entity info summary goes here</div>}
-        isSidebarOpen={true}
-        onSidebarToggle={() => console.log('not now')}
-        isDrawerOpen={false}
-        viewportWidth={props.viewportWidth}
-      />
+      {params.entityId ? (
+        <StandardAppLayout
+          mainContent={<div>Main content is coming...</div>}
+          sidebarContent={<div>Sidebar content is coming...</div>}
+          sidebarNavigation={<div>Sidebar navigation goes here</div>}
+          topbarContent={<div>Entity info summary goes here</div>}
+          isSidebarOpen={true}
+          onSidebarToggle={() => console.log('not now')}
+          isDrawerOpen={false}
+          viewportWidth={props.viewportWidth}
+        />
+      ) : (
+        <ExampleLinks {...props} />
+      )}
     </div>
   );
 };
 
-const mapStateToProps = (state: RootState) => ({
-  viewportWidth: getBreakpointWidth(state)
-});
+const ExampleLinks = (props: Props) => {
+  const links = props.exampleGenes.map((gene) => {
+    const path = urlHelper.entityViewer({
+      genomeId: props.activeGenomeId,
+      entityId: gene.object_id
+    });
+
+    return (
+      <Link key={gene.object_id} to={path}>
+        {gene.label}
+      </Link>
+    );
+  });
+
+  return <div>{links}</div>;
+};
+
+const mapStateToProps = (state: RootState) => {
+  const activeGenomeId = getEntityViewerActiveGenomeId(state);
+  const exampleGenes = activeGenomeId
+    ? getExampleGenes(activeGenomeId, state)
+    : [];
+
+  return {
+    activeGenomeId,
+    exampleGenes,
+    viewportWidth: getBreakpointWidth(state)
+  };
+};
 
 const mapDispatchToProps = {
   setDataFromUrl,
--- src/ensembl/src/shared/state/ens-object/ensObjectSelectors.ts
+++ src/ensembl/src/shared/state/ens-object/ensObjectSelectors.ts
@@ -1,6 +1,7 @@
 import get from 'lodash/get';
 
 import { getBrowserActiveGenomeInfo } from 'src/content/app/browser/browserSelectors';
+import { getGenomeInfoById } from 'src/shared/state/genome/genomeSelectors';
 
 import { LoadingState } from 'src/shared/types/loading-state';
 import { RootState } from '../../../store';
@@ -30,4 +31,16 @@ export const getExampleEnsObjects = (state: RootState): EnsObject[] => {
         .map((id) => state.ensObjects[id] && state.ensObjects[id].data)
         .filter(Boolean) as EnsObject[]) // make sure there are no undefineds in the returned array
     : [];
+};
+
+export const getExampleGenes = (
+  genomeId: string,
+  state: RootState
+): EnsObject[] => {
+  const genomeInfo = getGenomeInfoById(state, genomeId); // FIXME change arguments order in getGenomeInfoById
+  const exampleObjectIds = genomeInfo?.example_objects || [];
+  return exampleObjectIds
+    .map((id) => getEnsObjectById(state, id))
+    .filter((item): item is EnsObject => item !== null)
+    .filter((entity) => entity.object_type === 'gene');
 };
