26a10fbcb61e49f078459ad98b9fab94087ce83e
Andrey Azov <andrey.azov@gmail.com>
2020-01-13 23:55:11 +0000

    Use CSS grid for global layout

--- src/ensembl/src/header/launchbar/Launchbar.tsx
+++ src/ensembl/src/header/launchbar/Launchbar.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import SlideDown from 'react-slidedown';
 
 import ensemblIcon from 'static/img/launchbar/ensembl-logo.png'; // <-- note it's a png
 
@@ -97,11 +96,7 @@ const LaunchbarContent = (props: LaunchbarProps) => (
 );
 
 const Launchbar = (props: LaunchbarProps) => {
-  return (
-    <SlideDown transitionOnAppear={false}>
-      {props.launchbarExpanded ? <LaunchbarContent {...props} /> : null}
-    </SlideDown>
-  );
+  return props.launchbarExpanded ? <LaunchbarContent {...props} /> : null;
 };
 
 export default Launchbar;
