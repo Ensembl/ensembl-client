9ba83adec9501a29b6fea307d66fcfc8a368e3fe
Imran Salam <imran@ebi.ac.uk>
2020-01-22 16:39:29 +0000

    fix storybook issues

--- src/ensembl/stories/shared-components/layout/Layout.stories.tsx
+++ src/ensembl/stories/shared-components/layout/Layout.stories.tsx
@@ -46,6 +46,24 @@ const SidebarToolstripContent = () => (
   <div className={styles.sidebarToolstripContent}>Here be icons</div>
 );
 
+const SidebarNavigation = () => {
+  const [activeLinkIndex, setActiveLinkIndex] = useState(0);
+
+  return (
+    <div className={styles.sidebarNavigation}>
+      {['Link 1', 'Link 2'].map((linkStr, index) => (
+        <span
+          key={linkStr}
+          className={activeLinkIndex === index ? styles.activeLink : ''}
+          onClick={() => setActiveLinkIndex(index)}
+        >
+          {linkStr}
+        </span>
+      ))}
+    </div>
+  );
+};
+
 const DrawerContent = () => (
   <div className={styles.drawerContent}>This is drawer content</div>
 );
@@ -58,22 +76,13 @@ const Wrapper = (props: {
   onSidebarToggle: () => void;
   onDrawerClose?: () => void;
 }) => {
-  const [activeLinkIndex, setActiveLinkIndex] = useState(0);
-  const sidebarNavigation = {
-    links: [...Array(2)].map((_, index) => ({
-      label: `Link ${index + 1}`,
-      isActive: index === activeLinkIndex
-    })),
-    onChange: (index: number) => setActiveLinkIndex(index)
-  };
-
   return (
     <div className={styles.wrapper}>
       <StandardAppLayout
         mainContent={<MainContent />}
         topbarContent={<TopbarContent />}
         sidebarContent={props.sidebarContent}
-        sidebarNavigation={sidebarNavigation}
+        sidebarNavigation={<SidebarNavigation />}
         sidebarToolstripContent={<SidebarToolstripContent />}
         drawerContent={props.drawerContent || null}
         isSidebarOpen={props.isSidebarOpen}
