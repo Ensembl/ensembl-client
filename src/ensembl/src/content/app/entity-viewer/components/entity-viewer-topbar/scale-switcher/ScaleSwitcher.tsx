3aaed20d96824f65347eecfd99c07e2a69d6ab2a
Ridwan Amode <ridwan@ebi.ac.uk>
2020-02-04 11:29:57 +0000

    Entity viewer top bar showing the feature information (#245)
    * Entity viewer top bar showing the feature information
    
    * code cleanup and implemented changes from review
    
    * special fix for deploying branches with capital letters, not needed for this branch but might be useful in the future

---
+++ src/ensembl/src/content/app/entity-viewer/components/entity-viewer-topbar/scale-switcher/ScaleSwitcher.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import styles from './ScaleSwitcher.scss';
+
+const ScaleSwitcher = () => {
+  return (
+    <div className={styles.container}>
+      This is placeholder for scaleswitcher
+    </div>
+  );
+};
+
+export default ScaleSwitcher;
--- src/ensembl/src/content/app/entity-viewer/state/general/entityViewerGeneralState.ts
+++ src/ensembl/src/content/app/entity-viewer/state/general/entityViewerGeneralState.ts
@@ -1,7 +1,9 @@
 export type EntityViewerGeneralState = Readonly<{
   activeGenomeId: string | null;
+  activeEnsObjectIds: { [genomeId: string]: string };
 }>;
 
 export const initialState: EntityViewerGeneralState = {
-  activeGenomeId: null // FIXME add entity viewer storage service
+  activeGenomeId: null, // FIXME add entity viewer storage service
+  activeEnsObjectIds: {}
 };
