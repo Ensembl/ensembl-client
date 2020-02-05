4f2fa2f9b6d55bea8e09b972323c8bcc29359c17
Ridwan Amode <ridwan@ebi.ac.uk>
2020-02-05 14:39:07 +0000

    function to render the empty dots

--- src/ensembl/src/content/app/entity-viewer/components/entity-viewer-topbar/scale-switcher/ScaleSwitcher.tsx
+++ src/ensembl/src/content/app/entity-viewer/components/entity-viewer-topbar/scale-switcher/ScaleSwitcher.tsx
@@ -5,15 +5,15 @@ import styles from './ScaleSwitcher.scss';
 const ScaleSwitcher = () => {
   return (
     <div className={styles.container}>
-      <div className={styles.emptyNode}></div>
-      <div className={styles.emptyNode}></div>
-      <div className={styles.emptyNode}></div>
+      {renderEmptyDots(3)}
       <div className={styles.selectedNode}>Gene</div>
-      <div className={styles.emptyNode}></div>
-      <div className={styles.emptyNode}></div>
-      <div className={styles.emptyNode}></div>
+      {renderEmptyDots(3)}
     </div>
   );
 };
+
+const renderEmptyDots = (number: number) => {
+  return [...Array(number)].map((_, index) => <div key={index} className={styles.emptyNode} />);
+}
 
 export default ScaleSwitcher;
