b27002637d01539f8e80a695632145a39ab21b26
Manoj <manojpandian@ebi.ac.uk>
2020-02-20 14:05:18 +0000

    Randomly generate storybook entry options

--- src/ensembl/stories/entity-viewer/transcript-filter/TranscriptFilter.stories.tsx
+++ src/ensembl/stories/entity-viewer/transcript-filter/TranscriptFilter.stories.tsx
@@ -1,59 +1,39 @@
 import React, { useState } from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
+import times from 'lodash/times';
+import faker from 'faker';
 
-import EntityViewerTranscriptFilter, {
-  OptionsGroup
-} from 'src/content/app/entity-viewer/components/transcript-filter/EntityViewerTranscriptFilter';
+import EntityViewerTranscriptFilter from 'src/content/app/entity-viewer/components/transcript-filter/EntityViewerTranscriptFilter';
 
 import styles from './TranscriptFilter.stories.scss';
 
-const options: OptionsGroup[] = [
-  [
-    {
-      value: 'Option 1',
-      label: 'Option 1'
-    },
-    {
-      value: 'Option 2',
-      label: 'Option 2'
-    },
-    {
-      value: 'Option 3',
-      label: 'Option 3'
-    }
-  ],
-  [
-    {
-      value: 'Option 4',
-      label: 'Option 4'
-    },
-    {
-      value: 'Option 5',
-      label: 'Option 5'
-    },
-    {
-      value: 'Option 6',
-      label: 'Option 6'
-    }
-  ],
-  [
-    {
-      value: 'Option 7',
-      label: 'Option 7'
-    },
-    {
-      value: 'Option 8',
-      label: 'Option 8'
-    },
-    {
-      value: 'Option 9',
-      label: 'Option 9'
-    }
-  ]
-];
+const createOption = () => {
+  return {
+    value: faker.random.uuid(),
+    label: faker.random.words()
+  };
+};
 
-const selectedValues = ['Option 1', 'Option 5', 'Option 8'];
+const createOptionGroup = () => {
+  return times(5, () => createOption());
+};
+
+const optionGroups = times(3, () => createOptionGroup());
+
+const getRandomSelectedValues = () => {
+  const randomSelectedValues: string[] = [];
+
+  optionGroups.forEach((options) =>
+    randomSelectedValues.push(
+      options[Math.floor(Math.random() * options.length)].value
+    )
+  );
+
+  return randomSelectedValues;
+};
+
+const selectedValues = getRandomSelectedValues();
 
 const Wrapper = (props: any) => {
   const [values, setValues] = useState(selectedValues);
@@ -67,7 +47,7 @@ const Wrapper = (props: any) => {
     <div className={styles.defaultWrapper}>
       {
         <EntityViewerTranscriptFilter
-          options={options}
+          options={optionGroups}
           onChange={onChange}
           selectedValues={values}
           {...props}
