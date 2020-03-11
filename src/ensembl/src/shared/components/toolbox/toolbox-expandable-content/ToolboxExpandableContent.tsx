0d1cb13d2d71d823f2026b212f7bb8b31033d30e
Andrey Azov <andrey@ebi.ac.uk>
2020-03-11 17:24:38 +0000

    useImperativeHandle (doesn't work)

--- src/ensembl/src/shared/components/toolbox/toolbox-expandable-content/ToolboxExpandableContent.tsx
+++ src/ensembl/src/shared/components/toolbox/toolbox-expandable-content/ToolboxExpandableContent.tsx
@@ -1,45 +1,84 @@
-import React, { useState, ReactNode } from 'react';
+import React, {
+  useState,
+  useRef,
+  useImperativeHandle,
+  forwardRef,
+  ReactNode,
+  RefForwardingComponent,
+  MutableRefObject
+} from 'react';
+import noop from 'lodash/noop';
 
 import { ReactComponent as CloseIcon } from 'static/img/shared/close.svg';
 import { ReactComponent as DownloadIcon } from 'static/img/sidebar/download.svg';
 
 import styles from './ToolboxExpandableContent.scss';
 
-type HoverboxExpandableContentProps = {
+export type ToolboxExpandableContentHandles = {
+  getIsExpanded: () => boolean;
+  toggleExpanded: () => boolean
+};
+
+type ToolboxExpandableContentProps = {
   mainContent: ReactNode;
   footerContent: ReactNode;
 };
 
 type ToggleButtonProps = {
-  isOpen: boolean;
-  onClick: () => void;
+  // isOpen: boolean;
+  toolboxContentHandles: ToolboxExpandableContentHandles | null
+  // onClick: () => void;
 };
 
-const HoverboxExpandableContent = (props: HoverboxExpandableContentProps) => {
+const ToolboxExpandableContent: RefForwardingComponent<ToolboxExpandableContentHandles, ToolboxExpandableContentProps> = (props, ref) => {
   const [isExpanded, setIsExpanded] = useState(false);
+  const elementRef = useRef<HTMLDivElement>(null);
+  console.log('before useImperativeHandle', ref);
+
+  useImperativeHandle(ref, () => ({
+    foo: () => elementRef,
+    getIsExpanded: () => isExpanded,
+    toggleExpanded: () => toggleExpanded()
+  }), [isExpanded]);
 
   const toggleExpanded = () => {
-    setIsExpanded(!isExpanded);
+    const updated = !isExpanded;
+    console.log('toggling to', updated);
+    setIsExpanded(updated);
+    return updated;
   };
 
   return (
-    <div>
+    <div ref={elementRef}>
       <div className={styles.main}>
         {props.mainContent}
-        <ToggleButton isOpen={isExpanded} onClick={toggleExpanded} />
       </div>
       {isExpanded && <div>{props.footerContent}</div>}
     </div>
   );
 };
 
-const ToggleButton = (props: ToggleButtonProps) => {
-  const Icon = props.isOpen ? CloseIcon : DownloadIcon;
+export const ToggleButton = (props: ToggleButtonProps) => {
+  const {
+    getIsExpanded = noop,
+    toggleExpanded = noop
+  } = props.toolboxContentHandles || {};
+  console.log('props.toolboxContentHandles', props.toolboxContentHandles);
+  const [isOpen, setIsOpen] = useState(getIsExpanded());
+
+  const handleClick = () => {
+    console.log(toggleExpanded);
+    const isOpen = toggleExpanded();
+    console.log(isOpen);
+    setIsOpen(isOpen);
+  }
+
+  const Icon = isOpen ? CloseIcon : DownloadIcon;
   return (
-    <div className={styles.toggleButton} onClick={props.onClick}>
+    <div className={styles.toggleButton} onClick={handleClick}>
       <Icon />
     </div>
   );
 };
 
-export default HoverboxExpandableContent;
+export default forwardRef(ToolboxExpandableContent);
--- src/ensembl/stories/shared-components/toolbox/Toolbox.stories.tsx
+++ src/ensembl/stories/shared-components/toolbox/Toolbox.stories.tsx
@@ -1,9 +1,11 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { storiesOf } from '@storybook/react';
 
 import {
   Toolbox,
-  ToolboxExpandableContent
+  ToolboxExpandableContent,
+  ToggleButton as ToolboxToggleButton,
+  ToolboxExpandableContentHandles
 } from 'src/shared/components/toolbox';
 
 import styles from './Toolbox.stories.scss';
@@ -11,17 +13,47 @@ import styles from './Toolbox.stories.scss';
 const DefaultStory = () => {
   const [isShowing, setIsShowing] = useState(false);
   const buttonRef = useRef<HTMLButtonElement>(null);
+  const [isToolboxContentRefSet, setToolboxContentRef] = useState(false);
+  const toolboxContentRef = useRef<ToolboxExpandableContentHandles>(null);
 
   const toggleHoverbox = () => {
     setIsShowing(!isShowing);
   };
 
-  const mainHoverboxContent = (
-    <div>
-      <p>This is main hoverbox content</p>
-      <p>This is the second paragraph</p>
-    </div>
-  );
+  useEffect(() => {
+    toolboxContentRef.current && setToolboxContentRef(true);
+  }, [toolboxContentRef.current]);
+
+  const onRef = (node) => toolboxContentRef.current = node;
+
+  const getMainHoverboxContent = () => {
+    console.log('will pass', toolboxContentRef.current);
+    return (
+      <div>
+        <p>This is main hoverbox content</p>
+        <p>This is the second paragraph</p>
+        {
+          isToolboxContentRefSet &&
+          <ToolboxToggleButton
+            toolboxContentHandles={toolboxContentRef.current}
+          />
+        }
+      </div>
+    );
+  }
+
+  // const mainHoverboxContent = (
+  //   <div>
+  //     <p>This is main hoverbox content</p>
+  //     <p>This is the second paragraph</p>
+  //     {
+  //       isToolboxContentRefSet &&
+  //       <ToolboxToggleButton
+  //         toolboxContentHandles={toolboxContentRef.current}
+  //       />
+  //     }
+  //   </div>
+  // );
 
   const hoverboxFooterContent = (
     <div>
@@ -42,7 +74,8 @@ const DefaultStory = () => {
       {isShowing && buttonRef.current && (
         <Toolbox anchor={buttonRef.current}>
           <ToolboxExpandableContent
-            mainContent={mainHoverboxContent}
+            ref={(node) => { console.log ('node', node); onRef(node)}}
+            mainContent={getMainHoverboxContent()}
             footerContent={hoverboxFooterContent}
           />
         </Toolbox>
