f3a1b43371c9f6d37237c69ec515e28e8444020f
Andrey Azov <andrey.azov@gmail.com>
2020-03-16 01:12:51 +0000

    Update entityViewerGeneViewReducer

--- src/ensembl/src/content/app/entity-viewer/state/gene-view/entityViewerGeneViewReducer.ts
+++ src/ensembl/src/content/app/entity-viewer/state/gene-view/entityViewerGeneViewReducer.ts
@@ -1,12 +1,11 @@
 import { ActionType, getType } from 'typesafe-actions';
+import merge from 'lodash/merge';
+import get from 'lodash/get';
 
 import {
   defaultEntityViewerGeneViewObjectState,
   initialEntityViewerGeneViewState,
-  EntityViewerGeneViewState,
-  EntityViewerGeneViewObjectState,
-  EntityViewerGeneFunctionState,
-  EntityViewerGeneRelationshipsState
+  EntityViewerGeneViewState
 } from './entityViewerGeneViewState';
 import * as actions from './entityViewerGeneViewActions';
 
@@ -16,88 +15,19 @@ export default function entityViewerGeneViewReducer(
 ) {
   switch (action.type) {
     case getType(actions.updateActiveGeneViewObjectState):
+      const { activeGenomeId, activeObjectId, fragment } = action.payload;
+      const oldStateFragment = get(
+        state,
+        `${activeGenomeId}.${activeObjectId}`,
+        defaultEntityViewerGeneViewObjectState
+      );
+      const updatedStateFragment = merge({}, oldStateFragment, fragment);
       return {
         ...state,
-        [action.payload.activeGenomeId]: entityViewerGeneViewObjectReducer(
-          state[action.payload.activeGenomeId],
-          action
-        )
-      };
-    case getType(actions.updateActiveGeneViewObjectGeneFunctionState):
-      return {
-        ...state,
-        [action.payload.activeGenomeId]: entityViewerGeneViewObjectReducer(
-          state[action.payload.activeGenomeId],
-          action
-        )
-      };
-    default:
-      return state;
-  }
-}
-
-function entityViewerGeneViewObjectReducer(
-  state: { [activeObjectId: string]: EntityViewerGeneViewObjectState } = {},
-  action: ActionType<typeof actions>
-) {
-  switch (action.type) {
-    case getType(actions.updateActiveGeneViewObjectState):
-      const oldStateFragment = state[action.payload.activeObjectId];
-      const newStateFragment = {
-        ...oldStateFragment,
-        ...action.payload.fragment
-      };
-      return {
-        ...state,
-        [action.payload.activeObjectId]: newStateFragment
-      };
-    case getType(actions.updateActiveGeneViewObjectGeneFunctionState):
-      return {
-        ...state,
-        [action.payload
-          .activeObjectId]: entityViewerGeneViewObjectGeneFunctionReducer(
-          state[action.payload.activeObjectId].geneFunction,
-          action
-        )
-      };
-    case getType(actions.updateActiveGeneViewObjectGeneRelationshipsState):
-      return {
-        ...state,
-        [action.payload
-          .activeObjectId]: entityViewerGeneViewObjectGeneRelationshipsReducer(
-          state[action.payload.activeObjectId].geneRelationships,
-          action
-        )
-      };
-    default:
-      return state;
-  }
-}
-
-function entityViewerGeneViewObjectGeneFunctionReducer(
-  state: EntityViewerGeneFunctionState = defaultEntityViewerGeneViewObjectState.geneFunction,
-  action: ActionType<typeof actions>
-) {
-  switch (action.type) {
-    case getType(actions.updateActiveGeneViewObjectGeneFunctionState):
-      return {
-        ...state,
-        ...action.payload.fragment
-      };
-    default:
-      return state;
-  }
-}
-
-function entityViewerGeneViewObjectGeneRelationshipsReducer(
-  state: EntityViewerGeneRelationshipsState = defaultEntityViewerGeneViewObjectState.geneRelationships,
-  action: ActionType<typeof actions>
-) {
-  switch (action.type) {
-    case getType(actions.updateActiveGeneViewObjectGeneRelationshipsState):
-      return {
-        ...state,
-        ...action.payload.fragment
+        [activeGenomeId]: {
+          ...state[activeGenomeId],
+          [activeObjectId]: updatedStateFragment
+        }
       };
     default:
       return state;
