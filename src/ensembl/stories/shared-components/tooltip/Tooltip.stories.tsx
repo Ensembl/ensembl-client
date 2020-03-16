d5b806e0974df573bc1af5a2afd9d5894b02d85c
Andrey Azov <andrey.azov@gmail.com>
2020-03-16 21:37:48 +0000

    Add stories for Tooltip

--- src/ensembl/stories/shared-components/tooltip/Tooltip.stories.tsx
+++ src/ensembl/stories/shared-components/tooltip/Tooltip.stories.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { storiesOf } from '@storybook/react';
+import classNames from 'classnames';
 
 import useRefWithRerender from 'src/shared/hooks/useRefWithRerender';
 
@@ -7,17 +8,121 @@ import Tooltip from 'src/shared/components/tooltip/Tooltip';
 
 import styles from './Tooltip.stories.scss';
 
+const canonicalTranscriptDefinition = `
+	The canonical transcript is used in the gene tree analysis in Ensembl
+  and does not necessarily reflect the most biologically relevant transcript of a gene.
+  For human, the canonical transcript for a gene is set according to the following hierarchy:
+  1. Longest CCDS translation with no stop codons.
+  2. If no (1), choose the longest Ensembl/Havana merged translation with no stop codons.
+  3. If no (2), choose the longest translation with no stop codons.
+  4. If no translation, choose the longest non-protein-coding transcript.
+`;
+
+const ContentSwitch = (props: {
+  isLong: boolean;
+  onChange: (isLong: boolean) => void;
+}) => {
+  const { isLong, onChange } = props;
+  const handleClick = () => onChange(!isLong);
+  const unselectedProps = {
+    onClick: handleClick,
+    className: classNames(
+      styles.contentSwitchItem,
+      styles.contentSwitchItemActive
+    )
+  };
+  const selectedProps = {
+    className: classNames(styles.contentSwitchItem)
+  };
+
+  return (
+    <div className={styles.contentSwitch}>
+      <span {...(isLong ? unselectedProps : selectedProps)}>Short content</span>
+      <span {...(isLong ? selectedProps : unselectedProps)}>Long content</span>
+    </div>
+  );
+};
+
 const DefaultTooltipStory = () => {
   const [isMousedOver, setIsMousedOver] = useState(false);
+  const [isLongContent, setIsLongContent] = useState(false);
   const elementRef = useRefWithRerender<HTMLDivElement>(null);
 
   return (
     <div className={styles.container}>
       <div className={styles.containerInner}>
         <p>
-          Hover over the button to see the tooltip. Notice the slight delay
-          between the moment you mouse over the button and the moment the
-          tooltip appears.
+          Mouse over the rectangle below to see the tooltip. Notice the slight
+          delay between the moment your cursor is over the rectangle and the
+          moment the tooltip appears.
+        </p>
+        <ContentSwitch isLong={isLongContent} onChange={setIsLongContent} />
+        <div
+          onMouseEnter={() => setIsMousedOver(true)}
+          onMouseLeave={() => setIsMousedOver(false)}
+          ref={elementRef}
+          className={styles.tooltipAnchor}
+        >
+          Mouse over me
+        </div>
+        {isMousedOver && elementRef.current && (
+          <Tooltip delay={300} anchor={elementRef.current}>
+            <div className={styles.tooltipContent}>
+              {isLongContent ? canonicalTranscriptDefinition : 'I am tooltip'}
+            </div>
+          </Tooltip>
+        )}
+      </div>
+    </div>
+  );
+};
+
+const OnScrollTooltipStory = () => {
+  const [isMousedOver, setIsMousedOver] = useState(false);
+  const elementRef = useRefWithRerender<HTMLDivElement>(null);
+
+  const hideTooltip = () => setIsMousedOver(false);
+
+  return (
+    <div className={classNames(styles.container, styles.containerTall)}>
+      <div className={styles.containerInner}>
+        <p>
+          Scroll the page and mouse over the rectangle below. The tooltip should
+          be positioned correctly despite scrolling. Continue scrolling — and
+          the tooltip will disappear when the cursor leaves the rectangle.
+        </p>
+        <div
+          onMouseEnter={() => setIsMousedOver(true)}
+          onMouseLeave={() => setIsMousedOver(false)}
+          ref={elementRef}
+          className={styles.tooltipAnchor}
+        >
+          Mouse over me
+        </div>
+        {isMousedOver && elementRef.current && (
+          <Tooltip
+            delay={300}
+            anchor={elementRef.current}
+            onClose={hideTooltip}
+          >
+            I am tooltip
+          </Tooltip>
+        )}
+      </div>
+    </div>
+  );
+};
+
+const OverflowHiddenStory = () => {
+  const [isMousedOver, setIsMousedOver] = useState(false);
+  const elementRef = useRefWithRerender<HTMLDivElement>(null);
+
+  return (
+    <div className={classNames(styles.container, styles.containerOverflow)}>
+      <div className={styles.containerInner}>
+        <p>
+          The container crops anything that extends beyond its borders, but
+          despite that the tooltip is perfectly visible.
         </p>
         <div
           onMouseEnter={() => setIsMousedOver(true)}
@@ -37,14 +142,13 @@ const DefaultTooltipStory = () => {
   );
 };
 
-const OnScrollTooltipStory = () => {
-  return <div>Hello?</div>;
-};
-
 storiesOf('Components|Shared Components/Tooltip', module)
   .add('default', () => {
     return <DefaultTooltipStory />;
   })
   .add('scrolling', () => {
     return <OnScrollTooltipStory />;
+  })
+  .add('in cropping container', () => {
+    return <OverflowHiddenStory />;
   });
