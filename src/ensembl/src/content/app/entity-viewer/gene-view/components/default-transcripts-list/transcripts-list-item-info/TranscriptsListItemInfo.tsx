30d0e446fb922fa7002cc739e1a7929b9b30c724
Imran Salam <imran@ebi.ac.uk>
2020-04-09 16:38:14 +0100

    check if firstcodingexonindex and lastcodingexonindexes exist when calculating amino acid length

--- src/ensembl/src/content/app/entity-viewer/gene-view/components/default-transcripts-list/transcripts-list-item-info/TranscriptsListItemInfo.tsx
+++ src/ensembl/src/content/app/entity-viewer/gene-view/components/default-transcripts-list/transcripts-list-item-info/TranscriptsListItemInfo.tsx
@@ -74,26 +74,28 @@ const ItemInfo = (props: ItemInfoProps) => {
       } = getFirstAndLastCodingExonIndexes();
       let cdsLength = 0;
 
-      // add coding length of the first coding exon
-      const { end: firstCodingExonEnd } = getFeatureCoordinates(
-        exons[firstCodingExonIndex]
-      );
-      cdsLength += firstCodingExonEnd - cds.start + 1;
+      if (firstCodingExonIndex !== -1 && lastCodingExonIndex !== -1) {
+        // add coding length of the first coding exon
+        const { end: firstCodingExonEnd } = getFeatureCoordinates(
+          exons[firstCodingExonIndex]
+        );
+        cdsLength += firstCodingExonEnd - cds.start + 1;
 
-      // add coding length of the last coding exon
-      const { start: lastCodingExonStart } = getFeatureCoordinates(
-        exons[lastCodingExonIndex]
-      );
-      cdsLength += cds.end - lastCodingExonStart + 1;
+        // add coding length of the last coding exon
+        const { start: lastCodingExonStart } = getFeatureCoordinates(
+          exons[lastCodingExonIndex]
+        );
+        cdsLength += cds.end - lastCodingExonStart + 1;
 
-      // add coding length of exons between first and last coding exons
-      for (
-        let index = firstCodingExonIndex + 1;
-        index <= lastCodingExonIndex - 1;
-        index += 1
-      ) {
-        const { start, end } = getFeatureCoordinates(exons[index]);
-        cdsLength += end - start + 1;
+        // add coding length of exons between first and last coding exons
+        for (
+          let index = firstCodingExonIndex + 1;
+          index <= lastCodingExonIndex - 1;
+          index += 1
+        ) {
+          const { start, end } = getFeatureCoordinates(exons[index]);
+          cdsLength += end - start + 1;
+        }
       }
 
       const aminoAcidLength = Math.floor(cdsLength / 3);
