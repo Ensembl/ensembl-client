a9537a3fef29ba0a104fa405911bdb1aa1189b78
Andrey Azov <andrey@ebi.ac.uk>
2020-05-07 11:28:33 +0100

    Fix tests in TrackPanelBookmarks

--- src/ensembl/tests/fixtures/ens-object.ts
+++ src/ensembl/tests/fixtures/ens-object.ts
@@ -1,21 +1,29 @@
 import faker from 'faker';
 
-import { EnsObject, EnsObjectType } from 'src/shared/state/ens-object/ensObjectTypes';
+import {
+  EnsObject,
+  EnsObjectType
+} from 'src/shared/state/ens-object/ensObjectTypes';
 import { Strand } from 'src/content/app/entity-viewer/types/strand';
 
-export const createEnsObject = (objectType?: EnsObjectType): EnsObject => ({
-  bio_type: faker.lorem.words(),
-  label: faker.lorem.word(),
-  object_id: faker.random.uuid(),
-  genome_id: faker.lorem.word(),
-  location: createLocation(),
-  type: objectType || 'gene',
-  stable_id: faker.lorem.word(),
-  versioned_stable_id: faker.lorem.word(),
-  strand: Strand.FORWARD,
-  description: faker.lorem.words(),
-  track: createTrackInfo()
-});
+export const createEnsObject = (objectType?: EnsObjectType): EnsObject => {
+  const genome_id = faker.lorem.word();
+  const type = objectType || 'gene';
+  const object_id = `${genome_id}:${objectType}:${faker.random.uuid()};`;
+  return {
+    bio_type: faker.lorem.words(),
+    label: faker.lorem.word(),
+    object_id,
+    genome_id,
+    location: createLocation(),
+    type,
+    stable_id: faker.lorem.word(),
+    versioned_stable_id: faker.lorem.word(),
+    strand: Strand.FORWARD,
+    description: faker.lorem.words(),
+    track: createTrackInfo()
+  };
+};
 
 const createLocation = () => {
   const startPosition = faker.random.number(10000);
