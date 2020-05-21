b7aa59f77035b302e36a48be841e523fd7097be1
Andrey Azov <andrey@ebi.ac.uk>
2020-05-21 17:54:37 +0100

    Add licence header to all typescript files in the stories directory

--- src/ensembl/stories/static-images/StaticImages.stories.tsx
+++ src/ensembl/stories/static-images/StaticImages.stories.tsx
@@ -1,3 +1,19 @@
+/**
+ * See the NOTICE file distributed with this work for additional information
+ * regarding copyright ownership.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 
@@ -21,7 +37,7 @@ function importAllImages(fileType: string) {
 
   const images: any = [];
 
-  imagePaths.forEach(({  }: string, key: number) => {
+  imagePaths.forEach(({}: string, key: number) => {
     const filePath =
       'static/img' + imagePaths[key].substring(1, imagePaths[key].length);
     images.push([filePath, imageComponents[key]]);
