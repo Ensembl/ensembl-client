8ffb16bdb66589816b2d67dbbf350954314ba978
Imran Salam <imran@ebi.ac.uk>
2020-05-29 18:41:47 +0100

    remove ebi icons package and use static svgs for protein features count

--- src/ensembl/src/content/app/entity-viewer/gene-view/components/protein-features-count/ProteinFeaturesCount.tsx
+++ src/ensembl/src/content/app/entity-viewer/gene-view/components/protein-features-count/ProteinFeaturesCount.tsx
@@ -1,3 +1,19 @@
+/**
+ * See the NOTICE file distributed with this work for additional information
+ * regarding copyright ownership.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import React, { useState, useEffect } from 'react';
 import classNames from 'classnames';
 
@@ -6,10 +22,10 @@ import {
   fetchProteinSummaryStats
 } from 'src/content/app/entity-viewer/shared/rest/rest-data-fetchers/proteinData';
 
-import structuresIcon from 'EBI-Icon-fonts/EBI-Conceptual/static/svg/structures.svg';
-import ligandsIcon from 'EBI-Icon-fonts/EBI-Conceptual/static/svg/chemical.svg';
-import interactionsIcon from 'EBI-Icon-fonts/EBI-Conceptual/static/svg/systems.svg';
-import annotationsIcon from 'EBI-Icon-fonts/EBI-Conceptual/static/svg/cross-domain.svg';
+import structuresIcon from 'static/img/entity-viewer/icon_protein_structures.svg';
+import ligandsIcon from 'static/img/entity-viewer/icon_protein_ligands.svg';
+import interactionsIcon from 'static/img/entity-viewer/icon_protein_interactions.svg';
+import annotationsIcon from 'static/img/entity-viewer/icon_protein_annotations.svg';
 
 import transcriptListStyles from 'src/content/app/entity-viewer/gene-view/components/default-transcripts-list/DefaultTranscriptsList.scss';
 import styles from './ProteinFeaturesCount.scss';
