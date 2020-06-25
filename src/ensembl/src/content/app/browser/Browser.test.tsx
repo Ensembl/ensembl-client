6a493b29119537d476648325975bb571c78eee67
Manoj <manojpandian@ebi.ac.uk>
2020-06-25 16:47:06 +0100

    Add missing mock function

--- src/ensembl/src/content/app/browser/Browser.test.tsx
+++ src/ensembl/src/content/app/browser/Browser.test.tsx
@@ -29,7 +29,8 @@ import BrowserNavBar from './browser-nav/BrowserNavBar';
 import { createChrLocationValues } from 'tests/fixtures/browser';
 
 jest.mock('./hooks/useBrowserRouting', () => () => ({
-  changeGenomeId: jest.fn()
+  changeGenomeId: jest.fn(),
+  setBrowserLocation: jest.fn()
 }));
 jest.mock('./browser-bar/BrowserBar', () => () => <div>BrowserBar</div>);
 jest.mock('./browser-image/BrowserImage', () => () => <div>BrowserImage</div>);
