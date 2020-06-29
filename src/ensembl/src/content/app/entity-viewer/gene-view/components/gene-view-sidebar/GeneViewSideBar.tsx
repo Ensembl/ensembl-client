03368a7ddc4330680c105bddd8569db8121585bb
Manoj <manojpandian@ebi.ac.uk>
2020-06-29 14:21:29 +0100

    Use web thoas endpoint

--- src/ensembl/src/content/app/entity-viewer/gene-view/components/gene-view-sidebar/GeneViewSideBar.tsx
+++ src/ensembl/src/content/app/entity-viewer/gene-view/components/gene-view-sidebar/GeneViewSideBar.tsx
@@ -32,7 +32,7 @@ type Props = {
 };
 
 const client = new ApolloClient({
-  uri: 'http://hx-rke-wp-webadmin-13-worker-1.caas.ebi.ac.uk:31497'
+  uri: 'http://web-thoas.review.ensembl.org/'
 });
 
 const GeneViewSidebar = (props: Props) => {
