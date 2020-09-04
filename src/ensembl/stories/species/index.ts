8c55abc6516adee341f6c39e2ddd9bd78d470575
Manoj Pandian Sakthivel <manojpandian@ebi.ac.uk>
2020-09-04 15:38:27 +0100

    Add generic species stats component (#342)
    Add generic stats component for the species page

--- src/ensembl/stories/species/index.ts
+++ src/ensembl/stories/species/index.ts
@@ -14,12 +14,4 @@
  * limitations under the License.
  */
 
-import 'src/styles/main.scss';
-
-import './design-primitives/index.ts';
-import './shared-components/index.ts';
-import './static-images/index.ts';
-import './genome-browser/index.ts';
-import './entity-viewer/index.ts';
-import './species/index.ts';
-import './hooks/index.ts';
+import './species-stats/SpeciesStats.stories';
---
+++ src/ensembl/stories/species/species-stats/SpeciesStats.stories.tsx
@@ -0,0 +1,65 @@
+/**
+ * See the NOTICE file distributed with this work for additional information
+ * regarding copyright ownership.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+
+import SpeciesStats, {
+  SpeciesStatsProps
+} from 'src/content/app/species/components/species-stats/SpeciesStats';
+
+import styles from './SpeciesStats.stories.scss';
+
+const renderSpeciesStats = (props: SpeciesStatsProps) => {
+  return <SpeciesStats {...props} />;
+};
+
+storiesOf('Components|Species/SpeciesStats', module).add('default', () => (
+  <div className={styles.wrapper}>
+    {renderSpeciesStats({
+      label: 'Coding genes',
+      primaryValue: '20,438',
+      secondaryValue: '671',
+      secondaryUnit: 'readthrough'
+    })}
+    {renderSpeciesStats({
+      preLabel: 'preLabel',
+      label: 'label',
+      primaryValue: 'primaryValue',
+      primaryUnit: 'primaryUnit',
+      secondaryValue: 'secondaryValue',
+      secondaryUnit: 'secondaryUnit',
+      link: <a href="/">Link</a>
+    })}
+    {renderSpeciesStats({
+      preLabel: 'No. genes in',
+      label: 'Biological process',
+      primaryValue: '7,343',
+      primaryUnit: 'xyz',
+      secondaryValue: '1892',
+      secondaryUnit: 'terms'
+    })}
+    {renderSpeciesStats({
+      label: 'Transcriptomic data',
+      primaryValue: '90%',
+      link: <a href="/">View</a>
+    })}
+    {renderSpeciesStats({
+      label: 'SNVs',
+      primaryValue: '91%'
+    })}
+  </div>
+));
