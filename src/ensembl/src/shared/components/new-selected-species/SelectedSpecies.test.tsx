ca09049592b9c776cddc296fefe806a8def3ab1c
Jyothish NT <joyjoy.nt@gmail.com>
2020-09-17 23:56:39 +0100

    New selected species lozenge component

---
+++ src/ensembl/src/shared/components/new-selected-species/SelectedSpecies.test.tsx
@@ -0,0 +1,73 @@
+/**
+ * See the NOTICE file distributed with this work for additional information
+ * regarding copyright ownership.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import React from 'react';
+import { mount } from 'enzyme';
+
+import SelectedSpecies, {
+  Props as SelectedSpeciesProps
+} from './SelectedSpecies';
+import { CommittedItem } from 'src/content/app/species-selector/types/species-search';
+
+const speciesData = {
+  genome_id: 'homo_sapiens38',
+  reference_genome_id: null,
+  common_name: 'Human',
+  scientific_name: 'Homo sapiens',
+  assembly_name: 'GRCh38',
+  isEnabled: true
+};
+
+const propsObj = {
+  species: speciesData as CommittedItem,
+  isActive: true,
+  onClick: jest.fn()
+};
+
+describe('<SelectedSpecies />', () => {
+  const renderSelectedSpecies = (props: SelectedSpeciesProps) =>
+    mount(<SelectedSpecies {...speciesData} {...props} />);
+
+  it('renders without error', () => {
+    expect(() => renderSelectedSpecies(propsObj)).not.toThrow();
+  });
+
+  describe('lozenge', () => {
+    it('is active and enabled', () => {
+      const wrapper = renderSelectedSpecies(propsObj);
+      expect(wrapper.children('div').hasClass('inUseActive')).toEqual(true);
+    });
+    it('is active and not enabled', () => {
+      propsObj.species.isEnabled = false;
+      const wrapper = renderSelectedSpecies(propsObj);
+      expect(wrapper.children('div').hasClass('notInUseActive')).toEqual(true);
+    });
+    it('is inactive and enabled', () => {
+      propsObj.isActive = false;
+      propsObj.species.isEnabled = true;
+      const wrapper = renderSelectedSpecies(propsObj);
+      expect(wrapper.children('div').hasClass('inUseInactive')).toEqual(true);
+    });
+    it('is inactive and disabled', () => {
+      propsObj.species.isEnabled = false;
+      propsObj.isActive = false;
+      const wrapper = renderSelectedSpecies(propsObj);
+      expect(wrapper.children('div').hasClass('notInUseInactive')).toEqual(
+        true
+      );
+    });
+  });
+});
---
+++ src/ensembl/stories/shared-components/selected-species/SelectedSpecies.stories.tsx
@@ -0,0 +1,65 @@
+/**
+ * See the NOTICE file distributed with this work for additional information
+ * regarding copyright ownership.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import React from 'react';
+
+import SelectedSpecies from 'src/shared/components/new-selected-species/SelectedSpecies';
+
+import speciesData from '../species-tabs-wrapper/speciesData';
+
+import styles from './SelectedSpecies.stories.scss';
+
+export default {
+  title: 'Components/Shared Components/New Selected Species',
+  argTypes: { onClick: { action: 'Clicked' } }
+};
+
+type StoryArgs = {
+  onClick: () => void;
+};
+
+export const SelectedSpeciesStory = (args: StoryArgs) => {
+  const enabledSpecies = speciesData[0];
+  const disabledSpecies = {
+    ...enabledSpecies,
+    isEnabled: false
+  };
+
+  return (
+    <div className={styles.wrapper}>
+      <SelectedSpecies
+        species={enabledSpecies}
+        onClick={args.onClick}
+      ></SelectedSpecies>
+      <SelectedSpecies
+        species={enabledSpecies}
+        isActive={true}
+        onClick={args.onClick}
+      ></SelectedSpecies>
+      <SelectedSpecies
+        species={disabledSpecies}
+        onClick={args.onClick}
+      ></SelectedSpecies>
+      <SelectedSpecies
+        species={disabledSpecies}
+        isActive={true}
+        onClick={args.onClick}
+      ></SelectedSpecies>
+    </div>
+  );
+};
+
+SelectedSpeciesStory.storyName = 'Lozenge';
