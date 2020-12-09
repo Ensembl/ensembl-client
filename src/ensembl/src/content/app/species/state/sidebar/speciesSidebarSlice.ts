b12392bee97e93b23e11d606ffd5c8b2687c36ad
Manoj Pandian Sakthivel <manojpandian@ebi.ac.uk>
2020-12-09 11:33:26 +0000

    Save sidebar open or closed state in session (#412)

--- src/ensembl/src/content/app/species/state/sidebar/speciesSidebarSlice.ts
+++ src/ensembl/src/content/app/species/state/sidebar/speciesSidebarSlice.ts
@@ -22,6 +22,9 @@ import {
 } from '@reduxjs/toolkit';
 
 import { getActiveGenomeId } from 'src/content/app/species/state/general/speciesGeneralSelectors';
+import { isSidebarOpen } from 'src/content/app/species/state/sidebar/speciesSidebarSelectors';
+import speciesStorageService from '../../services/species-storage-service';
+
 import { RootState } from 'src/store';
 
 import { sidebarData } from 'src/content/app/species/sample-data.ts';
@@ -58,7 +61,7 @@ export type SpeciesSidebarPayload = {
   strain: Strain | null;
 };
 
-type SpeciesPageSidebarState = {
+export type SpeciesPageSidebarState = {
   isOpen: boolean;
   species: {
     [genomeId: string]: {
@@ -68,7 +71,7 @@ type SpeciesPageSidebarState = {
 };
 
 const initialState: SpeciesPageSidebarState = {
-  isOpen: true,
+  isOpen: speciesStorageService.getSidebarState()?.isOpen ?? true,
   species: {}
 };
 
@@ -92,6 +95,24 @@ export const fetchSidebarPayload = (): ThunkAction<
       sidebarPayload
     })
   );
+};
+
+export const toggleSidebarAndSave = (): ThunkAction<
+  void,
+  any,
+  null,
+  Action<string>
+> => (dispatch, getState: () => RootState) => {
+  const state = getState();
+  const activeGenomeId = getActiveGenomeId(state);
+  if (!activeGenomeId) {
+    return;
+  }
+  const isSidebarCurrentlyOpen = isSidebarOpen(state);
+
+  speciesStorageService.updateSidebarState({ isOpen: !isSidebarCurrentlyOpen });
+
+  dispatch(speciesPageSidebarSlice.actions.toggleSidebar());
 };
 
 const speciesPageSidebarSlice = createSlice({
