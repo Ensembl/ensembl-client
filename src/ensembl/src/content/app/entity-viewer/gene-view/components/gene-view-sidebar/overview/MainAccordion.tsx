Ensembl/ensembl-client

commit de06e80c015c6876d531914de6292b747bc37e33

Retrieve gene data for gene overview from thoas (#422)

src/ensembl/src/content/app/entity-viewer/gene-view/components/gene-view-sidebar/overview/MainAccordion.tsx
@@ -32,6 +32,7 @@ import { ReactComponent as DownloadButton } from 'static/img/launchbar/custom-do
 import Checkbox from 'src/shared/components/checkbox/Checkbox';
 
 import { Status } from 'src/shared/types/status';
+
 import {
   EntityViewerSidebarPayload,
   EntityViewerSidebarUIState

src/ensembl/src/content/app/entity-viewer/gene-view/components/gene-view-sidebar/external-references/GeneExternalReferences.tsx
@@ -15,12 +15,10 @@
  */
 
 import React, { useState } from 'react';
-import { connect } from 'react-redux';
 import { useQuery, gql } from '@apollo/client';
 import { useParams } from 'react-router';
 import sortBy from 'lodash/sortBy';
 
-import { getEntityViewerSidebarPayload } from 'src/content/app/entity-viewer/state/sidebar/entityViewerSidebarSelectors';
 import { parseEnsObjectIdFromUrl } from 'src/shared/state/ens-object/ensObjectHelpers';
 import { defaultSort } from 'src/content/app/entity-viewer/shared/helpers/transcripts-sorter';
 
@@ -33,7 +31,6 @@ import {
 } from 'src/shared/components/accordion';
 import ExternalReference from 'src/shared/components/external-reference/ExternalReference';
 
-import { RootState } from 'src/store';
 import {
   ExternalReference as ExternalReferenceType,
   ExternalReferencesGroup
@@ -302,8 +299,4 @@ const renderXrefGroup = (
   );
 };
 
-const mapStateToProps = (state: RootState) => ({
-  sidebarPayload: getEntityViewerSidebarPayload(state)
-});
-
-export default connect(mapStateToProps)(GeneExternalReferences);
+export default GeneExternalReferences;

src/ensembl/src/content/app/entity-viewer/gene-view/components/gene-view-sidebar/overview/GeneOverview.test.tsx
@@ -1,0 +1,159 @@
+/**
+ * See the NOTICE file distributed with this work for additional information
+ * regarding copyright ownership.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import React from 'react';
+import { render } from '@testing-library/react';
+import { useParams } from 'react-router-dom';
+import { useQuery } from '@apollo/client';
+
+import GeneOverview from './GeneOverview';
+
+jest.mock('react-router-dom', () => ({
+  useParams: jest.fn()
+}));
+
+jest.mock('@apollo/client', () => ({
+  gql: jest.fn(),
+  useQuery: jest.fn()
+}));
+
+const genomeId = 'genome_id';
+const geneId = 'unversioned_gene_id';
+const geneName = 'gene_name';
+const geneSymbol = 'gene_symbol';
+const stableId = 'gene_stable_id';
+const alternativeSymbols = [
+  'gene_synonym_1',
+  'gene_synonym_2',
+  'gene_synonym_3'
+];
+
+const completeGeneData = {
+  name: geneName,
+  stable_id: stableId,
+  symbol: geneSymbol,
+  alternative_symbols: alternativeSymbols
+};
+
+describe('<GeneOverview />', () => {
+  beforeEach(() => {
+    jest.resetAllMocks();
+
+    (useParams as any).mockImplementation(() => ({
+      params: {
+        entityId: geneId,
+        genomeId
+      }
+    }));
+  });
+
+  describe('loading', () => {
+    beforeEach(() => {
+      (useQuery as any).mockImplementation(() => ({
+        data: null,
+        loading: true
+      }));
+    });
+
+    it('shows the loading indicator', () => {
+      const { container } = render(<GeneOverview />);
+      const geneOverviewElement = container.firstChild;
+
+      expect(geneOverviewElement?.textContent).toBe('Loading...');
+    });
+  });
+
+  describe('empty data', () => {
+    beforeEach(() => {
+      (useQuery as any).mockImplementation(() => ({
+        data: null,
+        loading: false
+      }));
+    });
+
+    it('shows the no data message', () => {
+      const { container } = render(<GeneOverview />);
+      const geneOverviewElement = container.firstChild;
+
+      expect(geneOverviewElement?.textContent).toBe('No data to display');
+    });
+  });
+
+  describe('full data', () => {
+    beforeEach(() => {
+      (useQuery as any).mockImplementation(() => ({
+        data: { gene: completeGeneData },
+        loading: false
+      }));
+    });
+
+    it('renders all data correctly', () => {
+      const { container } = render(<GeneOverview />);
+
+      const geneDetailsElement = container.querySelector('.geneDetails');
+      const geneNameElement = container.querySelector('.geneName');
+      const synonymsElement = container.querySelector('.synonyms');
+
+      expect(geneDetailsElement?.textContent).toMatch(geneSymbol);
+      expect(geneDetailsElement?.textContent).toMatch(stableId);
+      expect(geneNameElement?.textContent).toMatch(geneName);
+      expect(synonymsElement?.textContent).toMatch(
+        alternativeSymbols.join(', ')
+      );
+    });
+  });
+
+  describe('partial data', () => {
+    it('renders only stable id if gene symbol is not available', () => {
+      const geneData = { ...completeGeneData, symbol: null };
+      (useQuery as any).mockImplementation(() => ({
+        data: { gene: geneData },
+        loading: false
+      }));
+
+      const { container } = render(<GeneOverview />);
+
+      const geneDetailsElement = container.querySelector('.geneDetails');
+      expect(geneDetailsElement?.textContent).toBe(stableId);
+    });
+
+    it('does not render gene name section if gene name is not available', () => {
+      const geneData = { ...completeGeneData, name: null };
+      (useQuery as any).mockImplementation(() => ({
+        data: { gene: geneData },
+        loading: false
+      }));
+
+      const { container } = render(<GeneOverview />);
+
+      const geneNameElement = container.querySelector('.geneName');
+      expect(geneNameElement).toBeFalsy();
+    });
+
+    it('shows "no synonyms" message for gene without synonyms', () => {
+      const geneData = { ...completeGeneData, alternative_symbols: [] };
+      (useQuery as any).mockImplementation(() => ({
+        data: { gene: geneData },
+        loading: false
+      }));
+
+      const { container } = render(<GeneOverview />);
+
+      const synonymsElement = container.querySelector('.synonyms');
+      expect(synonymsElement?.textContent).toBe('No synonyms');
+    });
+  });
+});

src/ensembl/src/content/app/entity-viewer/gene-view/components/gene-view-sidebar/overview/GeneOverview.tsx
@@ -15,119 +15,78 @@
  */
 
 import React from 'react';
-import { connect } from 'react-redux';
+import { useParams } from 'react-router-dom';
+import { useQuery, gql } from '@apollo/client';
 
-import ExternalReference from 'src/shared/components/external-reference/ExternalReference';
-import MainAccordion from './MainAccordion';
+import { parseEnsObjectIdFromUrl } from 'src/shared/state/ens-object/ensObjectHelpers';
 
-import {
-  getEntityViewerSidebarPayload,
-  getEntityViewerSidebarUIState
-} from 'src/content/app/entity-viewer/state/sidebar/entityViewerSidebarSelectors';
-import { updateEntityUI } from 'src/content/app/entity-viewer/state/sidebar/entityViewerSidebarActions';
-
-import { RootState } from 'src/store';
-import {
-  EntityViewerSidebarPayload,
-  EntityViewerSidebarUIState
-} from 'src/content/app/entity-viewer/state/sidebar/entityViewerSidebarState';
+import { EntityViewerParams } from 'src/content/app/entity-viewer/EntityViewer';
+import { Gene as GeneFromGraphql } from 'src/content/app/entity-viewer/types/gene';
 
 import styles from './GeneOverview.scss';
 
-type Props = {
-  sidebarPayload: EntityViewerSidebarPayload | null;
-  sidebarUIState: EntityViewerSidebarUIState | null;
-  updateEntityUI: (uIstate: Partial<EntityViewerSidebarUIState>) => void;
-};
+export const GENE_OVERVIEW_QUERY = gql`
+  query Gene($genomeId: String!, $geneId: String!) {
+    gene(byId: { genome_id: $genomeId, stable_id: $geneId }) {
+      alternative_symbols
+      name
+      stable_id
+      symbol
+    }
+  }
+`;
 
-const GeneOverview = (props: Props) => {
-  if (!props.sidebarPayload) {
+type Gene = Required<
+  Pick<GeneFromGraphql, 'stable_id' | 'symbol' | 'name' | 'alternative_symbols'>
+>;
+
+const GeneOverview = () => {
+  const params: EntityViewerParams = useParams();
+  const { entityId, genomeId } = params;
+  const geneId = entityId ? parseEnsObjectIdFromUrl(entityId).objectId : null;
+
+  const { data, loading } = useQuery<{ gene: Gene }>(GENE_OVERVIEW_QUERY, {
+    variables: {
+      geneId,
+      genomeId
+    },
+    skip: !geneId
+  });
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (!data || !data.gene) {
     return <div>No data to display</div>;
   }
-  const { gene } = props.sidebarPayload;
+
+  const { gene } = data;
+
   return (
     <div className={styles.overviewContainer}>
       <div className={styles.geneDetails}>
-        <span className={styles.geneSymbol}>{gene.symbol}</span>
-        <span>{gene.id}</span>
+        {!!gene.symbol && (
+          <span className={styles.geneSymbol}>{gene.symbol}</span>
+        )}
+        <span>{gene.stable_id}</span>
       </div>
 
-      <div className={styles.sectionHead}>Gene name</div>
+      {!!gene.name && (
+        <>
+          <div className={styles.sectionHead}>Gene name</div>
+          <div className={styles.geneName}>{gene.name}</div>
+        </>
+      )}
 
-      <div className={styles.geneName}>
-        <ExternalReference
-          label={gene.metadata?.name?.description}
-          linkText={gene.metadata?.name?.source?.id || ''}
-          to={''}
-        />
+      <div className={styles.sectionHead}>Synonyms</div>
+      <div className={styles.synonyms}>
+        {gene.alternative_symbols.length
+          ? gene.alternative_symbols.join(', ')
+          : 'No synonyms'}
       </div>
-      {gene.synonyms && (
-        <div>
-          <div className={styles.sectionHead}>Synonyms</div>
-          <div className={styles.synonyms}>{gene.synonyms.join(', ')}</div>
-        </div>
-      )}
-      {gene.attributes && (
-        <div>
-          <div className={styles.sectionHead}>Additional attributes</div>
-          <div>
-            {gene.attributes.map((attribute, key) => (
-              <div key={key}> {attribute} </div>
-            ))}
-          </div>
-        </div>
-      )}
-
-      <div>
-        <MainAccordion {...props} />
-      </div>
-
-      {props.sidebarPayload.homeologues && (
-        <div className={styles.homeologues}>
-          <div className={styles.sectionHead}>Homeologues</div>
-          <div>
-            {props.sidebarPayload.homeologues.map((homeologue, key) => (
-              <div className={styles.standardLabelValue} key={key}>
-                <div className={styles.label}>{homeologue.type}</div>
-                <div className={styles.value}>
-                  <a href={''}>{homeologue.stable_id}</a>
-                </div>
-              </div>
-            ))}
-          </div>
-        </div>
-      )}
-      {props.sidebarPayload.other_assemblies && (
-        <div>
-          <div className={styles.sectionHead}>
-            Other assemblies with this gene
-          </div>
-          <div>
-            {props.sidebarPayload.other_assemblies.map((otherAssembly, key) => (
-              <div className={styles.otherAssembly} key={key}>
-                <div className={styles.speciesName}>
-                  {otherAssembly.species_name}
-                </div>
-                <div className={styles.geneName}>
-                  {otherAssembly.assembly_name}
-                </div>
-                <div className={styles.stableId}>{otherAssembly.stable_id}</div>
-              </div>
-            ))}
-          </div>
-        </div>
-      )}
     </div>
   );
 };
 
-const mapStateToProps = (state: RootState) => ({
-  sidebarPayload: getEntityViewerSidebarPayload(state),
-  sidebarUIState: getEntityViewerSidebarUIState(state)
-});
-
-const mapDispatchToProps = {
-  updateEntityUI
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(GeneOverview);
+export default GeneOverview;

src/ensembl/src/content/app/entity-viewer/state/general/entityViewerGeneralActions.ts
@@ -42,7 +42,7 @@ import { ensureSpeciesIsEnabled } from 'src/content/app/species-selector/state/s
 import { EntityViewerParams } from 'src/content/app/entity-viewer/EntityViewer';
 import { RootState } from 'src/store';
 import { fetchEnsObject } from 'src/shared/state/ens-object/ensObjectActions';
-import { fetchSidebarPayload } from 'src/content/app/entity-viewer/state/sidebar/entityViewerSidebarActions';
+
 export const setActiveGenomeId = createAction(
   'entity-viewer/set-active-genome-id'
 )<string>();
@@ -96,7 +96,6 @@ export const setDataFromUrl: ActionCreator<ThunkAction<
 
   if (entityId && entityId !== activeEntityId) {
     dispatch(updateEnsObject(entityId));
-    dispatch(fetchSidebarPayload());
   }
 };
 

src/ensembl/src/content/app/entity-viewer/state/sidebar/entityViewerSidebarActions.ts
@@ -26,15 +26,12 @@ import { isEntityViewerSidebarOpen } from 'src/content/app/entity-viewer/state/s
 
 import {
   EntityViewerSidebarGenomeState,
-  EntityViewerSidebarPayload,
   SidebarTabName,
   SidebarStatus,
   EntityViewerSidebarUIState
 } from './entityViewerSidebarState';
 import { Status } from 'src/shared/types/status';
 import { RootState } from 'src/store';
-
-import { entityViewerSidebarSampleData } from './sampleData';
 
 export const updateGenomeState = createAction(
   'entity-viewer-sidebar/update-genome-ui-state'
@@ -84,34 +81,6 @@ export const toggleSidebar: ActionCreator<ThunkAction<
 export const openSidebar = () => toggleSidebar(Status.OPEN);
 
 export const closeSidebar = () => toggleSidebar(Status.CLOSED);
-
-export const updateEntityState = createAction(
-  'entity-viewer-sidebar/update-entity-state'
-)<{
-  genomeId: string;
-  entityId: string;
-  fragment: Partial<EntityViewerSidebarPayload>;
-}>();
-
-export const fetchSidebarPayload: ActionCreator<ThunkAction<
-  void,
-  any,
-  null,
-  Action<string>
->> = () => (dispatch, getState: () => RootState) => {
-  const state = getState();
-  const genomeId = getEntityViewerActiveGenomeId(state);
-  const entityId = getEntityViewerActiveEnsObjectId(state);
-
-  if (!genomeId || !entityId) {
-    return;
-  }
-
-  const sidebarPayload =
-    entityViewerSidebarSampleData[genomeId]?.entities[entityId] || null;
-
-  dispatch(updateEntityState({ genomeId, entityId, fragment: sidebarPayload }));
-};
 
 export const updateEntityUIState = createAction(
   'entity-viewer-sidebar/update-entity-ui-state'

src/ensembl/src/content/app/entity-viewer/state/sidebar/entityViewerSidebarReducer.ts
@@ -54,19 +54,6 @@ export default function entityViewerSidebarReducer(
         [action.payload.genomeId]: updatedStateFragment
       };
     }
-    case getType(actions.updateEntityState): {
-      const oldStateFragment = get(state, `${action.payload.genomeId}`);
-      const updatedStateFragment = merge({}, oldStateFragment, {
-        entities: {
-          [action.payload.entityId]: { payload: action.payload.fragment }
-        }
-      });
-
-      return {
-        ...state,
-        [action.payload.genomeId]: updatedStateFragment
-      };
-    }
     case getType(actions.updateEntityUIState): {
       const oldStateFragment = get(state, `${action.payload.genomeId}`);
 

src/ensembl/src/content/app/entity-viewer/state/sidebar/entityViewerSidebarSelectors.ts
@@ -32,14 +32,6 @@ export const getEntityViewerGenomeState = (state: RootState) => {
     : null;
 };
 
-export const getEntityViewerSidebarPayload = (state: RootState) => {
-  const activeEntityId = getEntityViewerActiveEnsObjectId(state);
-  return activeEntityId
-    ? getEntityViewerGenomeState(state)?.entities[activeEntityId].payload ||
-        null
-    : null;
-};
-
 export const getEntityViewerSidebarUIState = (state: RootState) => {
   const activeEntityId = getEntityViewerActiveEnsObjectId(state);
   return activeEntityId

commit 1b3e2adab996879d4944d04929dbf89fdde1ff4c

Temporarily fix types for MainAccordion component in EntityViewer sidebar
(This component is not used anywhere anyway at the moment,
but its types were broken after commit de06e80c015c6876d531914de6292b747bc37e33)

src/ensembl/src/content/app/entity-viewer/gene-view/components/gene-view-sidebar/overview/MainAccordion.tsx
@@ -33,15 +33,15 @@ import Checkbox from 'src/shared/components/checkbox/Checkbox';
 
 import { Status } from 'src/shared/types/status';
 
-import {
-  EntityViewerSidebarPayload,
-  EntityViewerSidebarUIState
-} from 'src/content/app/entity-viewer/state/sidebar/entityViewerSidebarState';
+import { EntityViewerSidebarUIState } from 'src/content/app/entity-viewer/state/sidebar/entityViewerSidebarState';
 
 import styles from './GeneOverview.scss';
 
+// FIXME:
+// We started using real data from Thoas, and are not yet getting anything that can be rendered
+// in this accordion. When we get back to using this accordion, the type of its props will need updating
 type Props = {
-  sidebarPayload: EntityViewerSidebarPayload | null;
+  sidebarPayload: any | null; // FIXME: update the type with appropriate shape of data
   sidebarUIState: EntityViewerSidebarUIState | null;
   updateEntityUI: (uIstate: Partial<EntityViewerSidebarUIState>) => void;
 };
@@ -210,7 +210,10 @@ const MainAccordion = (props: Props) => {
             className={styles.entityViewerAccordionItemContent}
           >
             <div>
-              {sidebarPayload.other_data_sets?.map((entry, key) =>
+              {sidebarPayload.other_data_sets?.map((
+                entry: any /* FIXME! */,
+                key: number
+              ) =>
                 renderStandardLabelValue({
                   label: entry.type,
                   value: entry.value,