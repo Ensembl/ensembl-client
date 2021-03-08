d71c9f3f29c324f977e7969bcac77cf1246de41d
Andrey Azov <andrey@ebi.ac.uk>
2021-03-08 18:06:38 +0000

    Update LoadingButton
    - Show error
    - Replaced console log with actions in the LoadingButton story
    - Updated rxjs stream for a saner flow

--- src/ensembl/src/shared/components/loading-button/LoadingButton.tsx
+++ src/ensembl/src/shared/components/loading-button/LoadingButton.tsx
@@ -1,8 +1,28 @@
+/**
+ * See the NOTICE file distributed with this work for additional information
+ * regarding copyright ownership.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import React, { useState, useRef, ReactNode, useEffect } from 'react';
 import { of, from, merge, timer, combineLatest, Subscription } from 'rxjs';
-import { tap, mergeMap, delay, take } from 'rxjs/operators'
+import { tap, mergeMap, delay, map, take, catchError } from 'rxjs/operators';
 
 import { PrimaryButton } from 'src/shared/components/button/Button';
+import { CircleLoader } from 'src/shared/components/loader/Loader';
+
+import { ReactComponent as Checkmark } from './checkmark.svg';
+import { ReactComponent as Cross } from './cross.svg';
 
 import { LoadingState } from 'src/shared/types/loading-state.ts';
 
@@ -10,8 +30,8 @@ import styles from './LoadingButton.scss';
 
 type LoadingButtonProps = {
   onClick: () => Promise<unknown>;
-  onSuccess?: (x: unknown) => void;
-  onError?: () => void;
+  onSuccess?: (x?: unknown) => void;
+  onError?: (x?: unknown) => void;
   isDisabled?: boolean;
   className?: string;
   children: ReactNode;
@@ -19,59 +39,100 @@ type LoadingButtonProps = {
 
 type RequestHandlerParams = {
   loader: () => Promise<unknown>;
-  onSuccess?: (x: unknown) => void;
+  onSuccess?: (x?: unknown) => void;
+  onError?: (x?: unknown) => void;
 };
 
-const MINIMUM_SPINNER_TIME = 2000; // 2 seconds
-const SUCCESS_INDICATOR_TIME = 2000; // 2 seconds
+const MINIMUM_SPINNER_TIME = 1000; // 2 seconds
+const SUCCESS_INDICATOR_TIME = 1000; // 1 second
 
 const getLoadingState$ = (params: RequestHandlerParams) => {
   const loadingStart$ = of(LoadingState.LOADING);
   const request$ = combineLatest([
     timer(MINIMUM_SPINNER_TIME).pipe(take(1)),
-    from(params.loader())
+    from(params.loader()).pipe(
+      map((result) => ({
+        status: LoadingState.SUCCESS as const,
+        result
+      })),
+      catchError((error) =>
+        of({
+          status: LoadingState.ERROR as const,
+          error
+        })
+      )
+    )
   ]).pipe(
-    tap(([, result]) => params?.onSuccess?.(result)),
-    mergeMap(() => merge(of(LoadingState.SUCCESS), returnToIniital$)),
+    tap(([, result]) => {
+      if (result.status === LoadingState.SUCCESS) {
+        params?.onSuccess?.(result.result);
+      } else {
+        params?.onError?.(result.error);
+      }
+    }),
+    mergeMap(([, result]) => {
+      return merge(of(result.status), returnToIniital$);
+    })
   );
   const returnToIniital$ = of(LoadingState.NOT_REQUESTED).pipe(
     delay(SUCCESS_INDICATOR_TIME)
-  )
+  );
   const action$ = merge(loadingStart$, request$);
 
   return action$;
-}
-
+};
 
 const LoadingButton = (props: LoadingButtonProps) => {
-  const [ loadingState, setLoadingState ] = useState<LoadingState>(LoadingState.NOT_REQUESTED);
+  const [loadingState, setLoadingState] = useState<LoadingState>(
+    LoadingState.NOT_REQUESTED
+  );
   const subscriptionRef = useRef<Subscription | null>(null);
 
   const onClick = () => {
-    const loadingState$ = getLoadingState$({ loader: props.onClick, onSuccess: props.onSuccess });
+    const loadingState$ = getLoadingState$({
+      loader: props.onClick,
+      onSuccess: props.onSuccess,
+      onError: props.onError
+    });
     const subscription = loadingState$.subscribe(setLoadingState);
     subscriptionRef.current = subscription;
-  }
+  };
 
   useEffect(() => {
     return () => subscriptionRef.current?.unsubscribe();
   }, []);
 
-  const buttonClass = loadingState !== LoadingState.NOT_REQUESTED ? styles.invisible : undefined;
+  const buttonClass =
+    loadingState !== LoadingState.NOT_REQUESTED ? styles.invisible : undefined;
 
   return (
     <div className={styles.buttonWrapper}>
-      { loadingState === LoadingState.LOADING && <Loading /> }
-      { loadingState === LoadingState.SUCCESS && <Success /> }
+      {loadingState === LoadingState.LOADING && <Loading />}
+      {loadingState === LoadingState.SUCCESS && <Success />}
+      {loadingState === LoadingState.ERROR && <ErrorIndicator />}
       <PrimaryButton onClick={onClick} className={buttonClass}>
-        { props.children }
+        {props.children}
       </PrimaryButton>
     </div>
   );
 };
 
-const Loading = () => <div className={styles.loadingIndicator}>Loading...</div>
+const Loading = () => (
+  <div className={styles.loadingIndicator}>
+    <CircleLoader className={styles.spinner} />
+  </div>
+);
 
-const Success = () => <div className={styles.successIndicator}>Success!</div>
+const Success = () => (
+  <div className={styles.successIndicator}>
+    <Checkmark className={styles.checkmark} />
+  </div>
+);
+
+const ErrorIndicator = () => (
+  <div className={styles.errorIndicator}>
+    <Cross className={styles.cross} />
+  </div>
+);
 
 export default LoadingButton;
--- src/ensembl/stories/shared-components/loading-button/LoadingButton.stories.tsx
+++ src/ensembl/stories/shared-components/loading-button/LoadingButton.stories.tsx
@@ -1,23 +1,101 @@
-import React from 'react';
+/**
+ * See the NOTICE file distributed with this work for additional information
+ * regarding copyright ownership.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import React, { useState, ChangeEvent } from 'react';
 
 import LoadingButton from 'src/shared/components/loading-button/LoadingButton';
 
-export default {
-  title: 'Components/Shared Components/LoadingButton'
+import styles from './LoadingButton.stories.scss';
+
+type DefaultArgs = {
+  onSuccess: (...args: any) => void;
+  onError: (...args: any) => void;
 };
 
-const longTask = () => new Promise((resolve) => {
-  setTimeout(() => {
-    resolve('task completed')
-  }, 100);
-});
+const longTask = (timeout: number, shouldError: boolean) =>
+  new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldError) {
+        reject('Error!');
+      } else {
+        resolve('task completed');
+      }
+    }, timeout);
+  });
 
-export const LoadingButtonStory = () => (
-  <div >
-    <LoadingButton onClick={longTask} onSuccess={(x: unknown) => console.log('on success!', x)}>
-      Press me!
-    </LoadingButton>
-  </div>
-);
+export const LoadingButtonStory = (args: DefaultArgs) => {
+  const [responseDuration, setResponseDuration] = useState(200);
+  const [shouldError, setShouldError] = useState(false);
+
+  const updateResponseDuration = (e: ChangeEvent<HTMLInputElement>) => {
+    const value = parseInt(e.target.value);
+    setResponseDuration(value);
+  };
+
+  const updateResponseError = () => {
+    setShouldError(!shouldError);
+  };
+
+  return (
+    <div className={styles.wrapper}>
+      <LoadingButton
+        onClick={() => longTask(responseDuration, shouldError)}
+        onSuccess={(result: unknown) => args.onSuccess(result)}
+        onError={(err: unknown) => args.onError(err)}
+      >
+        Press me!
+      </LoadingButton>
+
+      <p className={styles.note}>
+        (note that regardless of the response duration, the spinner will be
+        shown for at least 1 second)
+      </p>
+
+      <div className={styles.controls}>
+        <div className={styles.responseDurationControl}>
+          Response duration:
+          <input
+            type="range"
+            min="50"
+            max="5000"
+            step="10"
+            value={responseDuration}
+            onChange={updateResponseDuration}
+          />
+          {responseDuration} ms
+        </div>
+        <div>
+          Responds with an error:
+          <input
+            type="checkbox"
+            checked={shouldError}
+            onChange={updateResponseError}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
 
 LoadingButtonStory.storyName = 'default';
+
+export default {
+  title: 'Components/Shared Components/LoadingButton',
+  argTypes: {
+    onSuccess: { action: 'success' },
+    onError: { action: 'error' }
+  }
+};
