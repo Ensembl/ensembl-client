0bf38aaeb83468738bed58f216ca593a025590ca
Imran Salam <32512909+imransl@users.noreply.github.com>
2021-03-11 11:01:01 +0000

    Remove message and retry button for PDBe error responses (#475)

--- src/ensembl/src/content/app/entity-viewer/gene-view/components/proteins-list/proteins-list-item-info/ProteinsListItemInfo.tsx
+++ src/ensembl/src/content/app/entity-viewer/gene-view/components/proteins-list/proteins-list-item-info/ProteinsListItemInfo.tsx
@@ -20,7 +20,6 @@ import set from 'lodash/fp/set';
 import { Pick2 } from 'ts-multipick';
 
 import { CircleLoader } from 'src/shared/components/loader/Loader';
-import { PrimaryButton } from 'src/shared/components/button/Button';
 import ProteinDomainImage from 'src/content/app/entity-viewer/gene-view/components/protein-domain-image/ProteinDomainImage';
 import ProteinImage from 'src/content/app/entity-viewer/gene-view/components/protein-image/ProteinImage';
 import ProteinFeaturesCount from 'src/content/app/entity-viewer/gene-view/components/protein-features-count/ProteinFeaturesCount';
@@ -177,6 +176,10 @@ const ProteinsListItemInfo = (props: Props) => {
     };
   }, [summaryStatsLoadingState, uniprotXref]);
 
+  const showLoadingIndicator =
+    domainsLoadingState === LoadingState.LOADING ||
+    summaryStatsLoadingState === LoadingState.LOADING;
+
   return (
     <div className={styles.proteinsListItemInfo}>
       {productWithProteinDomains && (
@@ -237,56 +240,15 @@ const ProteinsListItemInfo = (props: Props) => {
               </div>
             )}
         </>
-        <StatusContent
-          summaryLoadingState={summaryStatsLoadingState}
-          domainsLoadingState={domainsLoadingState}
-          setSummaryStatsLoadingState={setSummaryStatsLoadingState}
-          setDomainsLoadingState={setDomainsLoadingState}
-        />
-
+        {showLoadingIndicator && (
+          <div className={styles.statusContainer}>
+            <CircleLoader />
+          </div>
+        )}
         <div className={styles.keyline}></div>
       </div>
     </div>
   );
-};
-
-type StatusContentProps = {
-  summaryLoadingState: LoadingState;
-  domainsLoadingState: LoadingState;
-  setSummaryStatsLoadingState: (loadingState: LoadingState) => void;
-  setDomainsLoadingState: (loadingState: LoadingState) => void;
-};
-
-const StatusContent = (props: StatusContentProps) => {
-  if (
-    props.domainsLoadingState === LoadingState.LOADING ||
-    props.summaryLoadingState === LoadingState.LOADING
-  ) {
-    return (
-      <div className={styles.statusContainer}>
-        <CircleLoader />
-      </div>
-    );
-  }
-
-  const retryHandler = () => {
-    if (props.domainsLoadingState === LoadingState.ERROR) {
-      props.setDomainsLoadingState(LoadingState.LOADING);
-    }
-    if (props.summaryLoadingState === LoadingState.ERROR) {
-      props.setSummaryStatsLoadingState(LoadingState.LOADING);
-    }
-  };
-
-  return props.domainsLoadingState === LoadingState.ERROR ||
-    props.summaryLoadingState === LoadingState.ERROR ? (
-    <div className={styles.statusContainer}>
-      <span className={styles.errorMessage}>
-        Failed to get data from PDBe Knowledge Base.
-      </span>
-      <PrimaryButton onClick={retryHandler}>Try again</PrimaryButton>
-    </div>
-  ) : null;
 };
 
 type ProteinExternalReferenceProps = {
