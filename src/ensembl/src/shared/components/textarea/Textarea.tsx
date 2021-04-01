3a39d42cd87bf646f13cf24a3d88b3425a7e4c14
Andrey Azov <andrey@ebi.ac.uk>
2021-04-01 01:38:38 +0100

    Migrate tests for Textarea to react-testing-library

--- src/ensembl/src/shared/components/textarea/Textarea.tsx
+++ src/ensembl/src/shared/components/textarea/Textarea.tsx
@@ -21,7 +21,7 @@ import noop from 'lodash/noop';
 import styles from './Textarea.scss';
 
 type PropsForRespondingWithEvents = {
-  onChange: (e: React.SyntheticEvent<HTMLTextAreaElement>) => void;
+  onChange: (e: React.ChangeEvent<HTMLTextAreaElement>) => void;
   onFocus: (e: React.SyntheticEvent<HTMLTextAreaElement>) => void;
   onBlur: (e: React.SyntheticEvent<HTMLTextAreaElement>) => void;
   callbackWithEvent: true;
@@ -55,6 +55,7 @@ const Textarea = (props: Props) => {
       | React.ChangeEvent<HTMLTextAreaElement>
       | React.FocusEvent<HTMLTextAreaElement>
   ) => {
+    e.persist();
     const value = e.target.value;
 
     if (eventName === 'change') {
