361f8b59da706d46724f0f3eabe454d319d72c71
Andrey Azov <andrey@ebi.ac.uk>
2021-08-09 15:58:05 +0100

    Update contact us form (#544)
    - Add the Messages and Contact us elements (tabs) to the header of the communication panel
    - Add the disabled state to the Upload component
    - Prevent body scroll when the popup is open
    - Fix a bug in the Upload component (undefined passed to the callback during file cancellation)
    - Close the panel upon a press on the close button
    - Allow adding multiple files at once
    - Open links to external sites in different tabs
    - Remove white space from the fields when validating the form

---
+++ src/ensembl/src/shared/components/communication-framework/communicationPanelContext.ts
@@ -0,0 +1,33 @@
+/**
+ * See the NOTICE file distributed with this work for additional information
+ * regarding copyright ownership.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { createContext, RefObject } from 'react';
+
+type CommunicationPanelContextType = {
+  panelBody: RefObject<HTMLDivElement> | null;
+};
+
+const initialContext: CommunicationPanelContextType = {
+  panelBody: null
+};
+
+const CommunicationPanelContext =
+  createContext<CommunicationPanelContextType>(initialContext);
+
+export const { Provider: CommunicationPanelContextProvider } =
+  CommunicationPanelContext;
+
+export default CommunicationPanelContext;
