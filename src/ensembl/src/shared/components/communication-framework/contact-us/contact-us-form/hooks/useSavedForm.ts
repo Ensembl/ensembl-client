a877ed3709fc5606d5700776e5ef03897cafd29c
Andrey Azov <andrey@ebi.ac.uk>
2021-09-21 23:44:43 +0100

    Add more nuance to the persistence of the unsubmitted form

--- src/ensembl/src/shared/components/communication-framework/contact-us/contact-us-form/hooks/useSavedForm.ts
+++ src/ensembl/src/shared/components/communication-framework/contact-us/contact-us-form/hooks/useSavedForm.ts
@@ -76,13 +76,20 @@ const useSavedForm: UseSavedForm = (params) => {
     return isEmpty;
   };
 
-  const saveFormState = () => {
-    // only save the form if it is not empty
-    if (isEmptyForm(stateRef.current)) {
+  const hasSavedForm = async () => {
+    const savedForm = await indexedDB.get(STORE_NAME, formName);
+    return Boolean(savedForm);
+  };
+
+  const saveFormState = async () => {
+    // do not save the form if it is currently empty and has not been saved before
+    // (if it has been saved, then saving an empty form to overwrite the previously saved one is fine)
+    const hasPreviouslySavedForm = await hasSavedForm();
+    if (!hasPreviouslySavedForm && isEmptyForm(stateRef.current)) {
       return;
     }
     const formWithoutHugeFiles = withoutHugeFiles(stateRef.current);
-    indexedDB.set(STORE_NAME, params.formName, formWithoutHugeFiles);
+    indexedDB.set(STORE_NAME, formName, formWithoutHugeFiles);
   };
 
   const withoutHugeFiles = (state: Form) => {
