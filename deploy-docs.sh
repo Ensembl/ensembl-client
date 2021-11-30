2b7208385c7c2d3bb995f0cd9d2455d99dca1863
Manoj <manojpandian@ebi.ac.uk>
2021-11-30 11:42:05 +0000

    Update deploy docs

--- deploy-docs.sh
+++ deploy-docs.sh
@@ -3,13 +3,10 @@
 # prepare directories
 BUILD_DIR="built-docs"
 if [ -d "$BUILD_DIR" ]; then rm -rf $BUILD_DIR; fi
-mkdir -p "$BUILD_DIR" "$BUILD_DIR/storybook" "$BUILD_DIR/genome-browser"
+mkdir -p "$BUILD_DIR" "$BUILD_DIR/storybook"
 
 # build storybook in respective folder
 build-storybook -o "$BUILD_DIR/storybook"
-
-# copy pre-built docs for genome browser in respective folder
-cp -a ../assets/browser/app/doc/manual/site/. "$BUILD_DIR/genome-browser"
 
 # copy landing page to the root docs build folder
 cp static/html/docs-main.html "$BUILD_DIR/index.html"
