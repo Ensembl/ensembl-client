b0e142d3264ee853e4608e1a4e171433fc387008
Jyothish <joyjoy.nt@gmail.com>
2022-01-31 18:23:52 +0530

    Circular chromosome indicator (#672)

--- src/content/app/genome-browser/components/browser-location-indicator/BrowserLocationIndicator.tsx
+++ src/content/app/genome-browser/components/browser-location-indicator/BrowserLocationIndicator.tsx
@@ -23,6 +23,7 @@ import { getCommaSeparatedNumber } from 'src/shared/helpers/formatters/numberFor
 import { getBrowserActiveGenomeId } from 'src/content/app/genome-browser/state/browser-general/browserGeneralSelectors';
 import { getActualChrLocation } from 'src/content/app/genome-browser/state/browser-general/browserGeneralSelectors';
 import { toggleBrowserNav } from 'src/content/app/genome-browser/state/browser-nav/browserNavSlice';
+import { getGenomeKaryotype } from 'src/shared/state/genome/genomeSelectors';
 
 import styles from './BrowserLocationIndicator.scss';
 
@@ -33,6 +34,8 @@ type Props = {
 export const BrowserLocationIndicator = (props: Props) => {
   const actualChrLocation = useSelector(getActualChrLocation);
   const activeGenomeId = useSelector(getBrowserActiveGenomeId);
+  const genomeKaryotype = useSelector(getGenomeKaryotype);
+
   const dispatch = useDispatch();
 
   const [chrCode, chrStart, chrEnd] = actualChrLocation || [];
@@ -47,11 +50,21 @@ export const BrowserLocationIndicator = (props: Props) => {
     ? {}
     : { onClick: () => dispatch(toggleBrowserNav({ activeGenomeId })) };
 
+  const activeChromosome =
+    genomeKaryotype &&
+    genomeKaryotype.find((karyotype) => {
+      return karyotype.name === chrCode;
+    });
+
   return (
     <div className={className}>
       <div className={styles.chrLabel}>Chromosome</div>
       <div className={styles.chrLocationView} {...onClickProps}>
-        <div className={styles.chrCode}>{chrCode}</div>
+        {activeChromosome?.is_circular ? (
+          <CircularChromosomeIndicator />
+        ) : (
+          <div className={styles.chrCode}>{chrCode}</div>
+        )}
         <div className={styles.chrRegion}>
           <span>{getCommaSeparatedNumber(chrStart as number)}</span>
           <span className={styles.chrSeparator}>-</span>
@@ -62,4 +75,7 @@ export const BrowserLocationIndicator = (props: Props) => {
   );
 };
 
+const CircularChromosomeIndicator = () => {
+  return <div className={styles.circularIndicator}></div>;
+};
 export default BrowserLocationIndicator;
