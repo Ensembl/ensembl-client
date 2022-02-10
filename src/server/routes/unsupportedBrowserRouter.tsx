0502909553e8a7abe9c9a3a2703f202ba4358699
Andrey Azov <andrey@ebi.ac.uk>
2022-02-10 15:25:28 +0000

    Fix the injection of the script

--- src/server/routes/unsupportedBrowserRouter.tsx
+++ src/server/routes/unsupportedBrowserRouter.tsx
@@ -49,10 +49,7 @@ const unsupportedBrowserRouter = (_: Request, res: Response) => {
       <title>Unsupported browser</title>
       <meta name="description" content="Your browser is not supported"></meta>
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      ${
-        '${process.env.REPORT_ANALYTICS}'.toLowerCase() === 'true' &&
-        getAnalyticsScript()
-      }
+      ${getAnalyticsScript()}
       ${extractor.getLinkTags()}
       ${extractor.getStyleTags()}
     </head>
@@ -68,13 +65,16 @@ const unsupportedBrowserRouter = (_: Request, res: Response) => {
 };
 
 const getAnalyticsScript = () =>
-  `<script>
+  process.env.REPORT_ANALYTICS?.toLowerCase() === 'true' &&
+  process.env.GOOGLE_ANALYTICS_KEY
+    ? `<script>
     (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
     (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
     m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
     })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
     ga('create', '${process.env.GOOGLE_ANALYTICS_KEY}', 'auto');
     ga('send', 'pageview');
-  </script>`;
+  </script>`
+    : '';
 
 export default unsupportedBrowserRouter;
