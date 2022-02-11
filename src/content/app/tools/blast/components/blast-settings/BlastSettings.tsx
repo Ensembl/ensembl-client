c21a3afdd927ed170af3294a2f9f4bf3b1dcb242
Andrey Azov <andrey@ebi.ac.uk>
2022-02-11 12:56:24 +0000

    Add Blast settings (#684)
    - Add a config for the Blast parameters (hard-coded for now; will be fetched from the backend in the future)
    - Add a React component to display a list of available Blast parameters
       The availability of some of the parameters depends on which other parameters have been chosen
    - Fix a test of the Upload component to properly wait for the expected value

---
+++ src/content/app/tools/blast/components/blast-settings/BlastSettings.tsx
@@ -0,0 +1,354 @@
+/**
+ * See the NOTICE file distributed with this work for additional information
+ * regarding copyright ownership.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import React, { FormEvent, useEffect, useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
+import ShowHide from 'src/shared/components/show-hide/ShowHide';
+import Checkbox from 'src/shared/components/checkbox/Checkbox';
+import RadioGroup from 'src/shared/components/radio-group/RadioGroup';
+
+import untypedBlastSettingsConfig from './blastSettingsConfig.json';
+
+import {
+  getSelectedSequenceType,
+  getSelectedBlastProgram,
+  getSelectedSearchSensitivity,
+  getBlastSearchParameters
+} from 'src/content/app/tools/blast/state/blast-form/blastFormSelectors';
+import {
+  setSequenceType,
+  setBlastDatabase,
+  setBlastProgram,
+  changeSensitivityPresets,
+  setBlastParameter
+} from 'src/content/app/tools/blast/state/blast-form/blastFormSlice';
+
+import type {
+  SequenceType,
+  BlastProgram,
+  BlastSelectSetting,
+  BlastBooleanSetting,
+  BlastParameterName,
+  BlastSettingsConfig
+} from 'src/content/app/tools/blast/types/blastSettings';
+
+import styles from './BlastSettings.scss';
+
+const blastSettingsConfig = untypedBlastSettingsConfig as BlastSettingsConfig;
+
+const getParameterData = (name: BlastParameterName) => {
+  return blastSettingsConfig.parameters[name];
+};
+
+const getPresetsList = () => {
+  const { presets } = blastSettingsConfig;
+  const { label, options } = presets;
+  return { label, options };
+};
+
+const getDatabaseSequenceType = (database: string) => {
+  return blastSettingsConfig.database_sequence_types[database];
+};
+
+const getAvailableBlastPrograms = (
+  sequenceType: string,
+  databaseSequenceType: string
+) => {
+  const availablePrograms = blastSettingsConfig.programs_configurator.find(
+    (option) =>
+      option.sequence_type === sequenceType &&
+      option.database_type === databaseSequenceType
+  )?.programs as string[];
+  const blastProgramSetting = {
+    ...blastSettingsConfig.parameters.program,
+    options: (
+      blastSettingsConfig.parameters.program as BlastSelectSetting
+    ).options.filter(({ value }) => availablePrograms.includes(value))
+  };
+  return blastProgramSetting;
+};
+
+const BlastSettings = () => {
+  const [parametersExpanded, setParametersExpanded] = useState(false);
+  const sequenceType = useSelector(getSelectedSequenceType);
+  const blastProgram = useSelector(getSelectedBlastProgram);
+  const searchSensitivity = useSelector(getSelectedSearchSensitivity);
+  const blastParameters = useSelector(getBlastSearchParameters);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (!blastParameters.database) {
+      const defaultDatabase = blastSettingsConfig.defaults.database;
+      onDatabaseChange(defaultDatabase);
+    }
+  }, []);
+
+  const onSequenceTypeChange = (sequenceType: string) => {
+    dispatch(
+      setSequenceType({
+        sequenceType: sequenceType as SequenceType,
+        config: blastSettingsConfig
+      })
+    );
+  };
+
+  const onDatabaseChange = (database: string) => {
+    dispatch(
+      setBlastDatabase({
+        database,
+        config: blastSettingsConfig
+      })
+    );
+  };
+
+  const onBlastProgramChange = (program: string) => {
+    dispatch(
+      setBlastProgram({
+        program: program as BlastProgram,
+        config: blastSettingsConfig
+      })
+    );
+  };
+
+  const onSearchSensitivityChange = (presetName: string) => {
+    dispatch(
+      changeSensitivityPresets({
+        presetName,
+        config: blastSettingsConfig
+      })
+    );
+  };
+
+  const onBlastParameterChange = (
+    parameterName: string,
+    parameterValue: string
+  ) => {
+    dispatch(
+      setBlastParameter({
+        parameterName: parameterName as BlastParameterName,
+        parameterValue
+      })
+    );
+  };
+
+  const onParametersToggle = () => {
+    setParametersExpanded(!parametersExpanded);
+  };
+
+  if (Object.keys(blastParameters).length === 0) {
+    return null;
+  }
+
+  const databaseSequenceType = getDatabaseSequenceType(
+    blastParameters.database as string
+  );
+  const availableBlastPrograms = getAvailableBlastPrograms(
+    sequenceType,
+    databaseSequenceType
+  );
+
+  return (
+    <>
+      <div className={styles.topLevel}>
+        Settings
+        <div className={styles.mainSettings}>
+          {buildSelect({
+            ...(getParameterData('database') as BlastSelectSetting),
+            selectedOption: blastParameters.database as string,
+            onChange: onDatabaseChange
+          })}
+          {buildSelect({
+            ...(availableBlastPrograms as BlastSelectSetting),
+            selectedOption: blastProgram,
+            onChange: onBlastProgramChange
+          })}
+          {buildSelect({
+            ...(getPresetsList() as BlastSelectSetting),
+            selectedOption: searchSensitivity,
+            onChange: onSearchSensitivityChange
+          })}
+        </div>
+        <div>
+          <ShowHide
+            label="Parameters"
+            isExpanded={parametersExpanded}
+            onClick={onParametersToggle}
+          />
+        </div>
+        <SequenceSwitcher
+          sequenceType={sequenceType}
+          onChange={onSequenceTypeChange}
+        />
+      </div>
+      {parametersExpanded && (
+        <div className={styles.bottomLevel}>
+          {buildSelect({
+            ...(getParameterData('alignments') as BlastSelectSetting),
+            selectedOption: blastParameters.alignments as string,
+            onChange: (value: string) =>
+              onBlastParameterChange('alignments', value)
+          })}
+          {buildSelect({
+            ...(getParameterData('scores') as BlastSelectSetting),
+            selectedOption: blastParameters.scores as string,
+            onChange: (value: string) => onBlastParameterChange('scores', value)
+          })}
+          {buildSelect({
+            ...(getParameterData('hsps') as BlastSelectSetting),
+            selectedOption: blastParameters.scores as string,
+            onChange: (value: string) => onBlastParameterChange('hsps', value)
+          })}
+          {buildSelect({
+            ...(getParameterData('dropoff') as BlastSelectSetting),
+            selectedOption: blastParameters.scores as string,
+            onChange: (value: string) =>
+              onBlastParameterChange('dropoff', value)
+          })}
+          {buildCheckbox({
+            ...(getParameterData('gapalign') as BlastBooleanSetting),
+            selectedOption: blastParameters.gapalign as string,
+            onChange: (value: string) =>
+              onBlastParameterChange('gapalign', value)
+          })}
+          {buildCheckbox({
+            ...(getParameterData('filter') as BlastBooleanSetting),
+            selectedOption: blastParameters.filter as string,
+            onChange: (value: string) => onBlastParameterChange('filter', value)
+          })}
+          {buildSelect({
+            ...(getParameterData('compstats') as BlastSelectSetting),
+            selectedOption: blastParameters.compstats as string,
+            onChange: (value: string) =>
+              onBlastParameterChange('compstats', value)
+          })}
+          {buildSelect({
+            ...(getParameterData('exp') as BlastSelectSetting),
+            selectedOption: blastParameters.exp as string,
+            onChange: (value: string) => onBlastParameterChange('exp', value)
+          })}
+          {databaseSequenceType === 'dna' &&
+            buildSelect({
+              ...(getParameterData('match_scores') as BlastSelectSetting),
+              selectedOption: blastParameters.match_scores as string,
+              onChange: (value: string) =>
+                onBlastParameterChange('match_scores', value)
+            })}
+          {buildSelect({
+            ...(getParameterData('wordsize') as BlastSelectSetting),
+            selectedOption: blastParameters.match_scores as string,
+            onChange: (value: string) =>
+              onBlastParameterChange('wordsize', value)
+          })}
+          {buildSelect({
+            ...(getParameterData('gapopen') as BlastSelectSetting),
+            selectedOption: blastParameters.gapopen as string,
+            onChange: (value: string) =>
+              onBlastParameterChange('gapopen', value)
+          })}
+          {buildSelect({
+            ...(getParameterData('gapext') as BlastSelectSetting),
+            selectedOption: blastParameters.gapopen as string,
+            onChange: (value: string) => onBlastParameterChange('gapext', value)
+          })}
+          {databaseSequenceType === 'protein' &&
+            buildSelect({
+              ...(getParameterData('matrix') as BlastSelectSetting),
+              selectedOption: blastParameters.gapopen as string,
+              onChange: (value: string) =>
+                onBlastParameterChange('matrix', value)
+            })}
+        </div>
+      )}
+    </>
+  );
+};
+
+const SequenceSwitcher = (props: {
+  sequenceType: string;
+  onChange: (sequenceType: string) => void;
+}) => {
+  const options = [
+    {
+      label: 'Nucleotide',
+      value: 'dna'
+    },
+    {
+      label: 'Protein',
+      value: 'protein'
+    }
+  ];
+  return (
+    <RadioGroup
+      options={options}
+      selectedOption={props.sequenceType}
+      onChange={(val) => props.onChange(val as string)}
+    />
+  );
+};
+
+// to be replaced with a simple select component
+const buildSelect = (setting: {
+  options: { label: string; value: string }[];
+  label: string;
+  selectedOption: string;
+  onChange: (value: string) => void;
+}) => {
+  const options = setting.options.map((option) => (
+    <option key={option.value} value={option.value}>
+      {option.label}
+    </option>
+  ));
+
+  const onChange = (e: FormEvent<HTMLSelectElement>) => {
+    const value = e.currentTarget.value;
+    setting.onChange(value);
+  };
+
+  return (
+    <div className={styles.select}>
+      <label>
+        <span>{setting.label}</span>
+        <select value={setting.selectedOption} onInput={onChange}>
+          {options}
+        </select>
+      </label>
+    </div>
+  );
+};
+
+const buildCheckbox = (params: {
+  label: string;
+  options: {
+    true: string;
+    false: string;
+  };
+  selectedOption: string;
+  onChange: (value: string) => void;
+}) => {
+  const { label, options, selectedOption, onChange } = params;
+  const isSelected = options.true === selectedOption;
+
+  const onCheckboxChange = (isChecked: boolean) => {
+    onChange(options[`${isChecked}`]);
+  };
+
+  return (
+    <Checkbox label={label} checked={isSelected} onChange={onCheckboxChange} />
+  );
+};
+
+export default BlastSettings;
--- src/content/app/tools/blast/state/blast-form/blastFormSlice.ts
+++ src/content/app/tools/blast/state/blast-form/blastFormSlice.ts
@@ -18,19 +18,72 @@ import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import cloneDeep from 'lodash/cloneDeep';
 
 import type { ParsedInputSequence } from 'src/content/app/tools/blast/types/parsedInputSequence';
+import type {
+  BlastParameterName,
+  BlastSettingsConfig,
+  BlastProgram,
+  SequenceType
+} from 'src/content/app/tools/blast/types/blastSettings';
+
+type BlastFormSettings = {
+  sequenceType: SequenceType;
+  sequenceSelectionMode: 'automatic' | 'manual';
+  program: BlastProgram;
+  preset: string;
+  parameters: Partial<Record<BlastParameterName, string>>;
+};
 
 export type BlastFormState = {
   step: 'sequences' | 'species'; // will only be relevant on smaller screens
   sequences: ParsedInputSequence[];
   shouldAppendEmptyInput: boolean;
   selectedSpecies: string[];
+  settings: BlastFormSettings;
+};
+
+const initialBlastFormSettings: BlastFormSettings = {
+  sequenceType: 'dna',
+  sequenceSelectionMode: 'automatic',
+  program: 'blastn',
+  preset: 'normal',
+  parameters: {}
 };
 
 export const initialState: BlastFormState = {
   step: 'sequences',
   sequences: [],
   shouldAppendEmptyInput: true,
-  selectedSpecies: []
+  selectedSpecies: [],
+  settings: initialBlastFormSettings
+};
+
+const getProgamName = ({
+  sequenceType,
+  databaseSequenceType,
+  config
+}: {
+  sequenceType: SequenceType;
+  databaseSequenceType: SequenceType;
+  config: BlastSettingsConfig;
+}) => {
+  const programs = config.programs_configurator.find(
+    ({ sequence_type, database_type }) => {
+      return (
+        sequence_type === sequenceType && database_type === databaseSequenceType
+      );
+    }
+  )?.programs as BlastProgram[];
+  return programs[0];
+};
+
+const getDatabaseSequenceType = ({
+  database,
+  config
+}: {
+  database: string;
+  config: BlastSettingsConfig;
+}) => {
+  return config.database_sequence_types[database];
 };
 
 const blastFormSlice = createSlice({
@@ -71,7 +124,105 @@ const blastFormSlice = createSlice({
     switchToSpeciesStep(state) {
       state.step = 'species';
     },
+    setSequenceType(
+      state,
+      action: PayloadAction<{
+        sequenceType: SequenceType;
+        isAutomatic?: boolean;
+        config: BlastSettingsConfig;
+      }>
+    ) {
+      const { sequenceType, config, isAutomatic } = action.payload;
+      const selectedDatabase =
+        state.settings.parameters.database ?? config.defaults.database;
+      const databaseSequenceType = getDatabaseSequenceType({
+        database: selectedDatabase,
+        config
+      });
+      const programName = getProgamName({
+        sequenceType,
+        databaseSequenceType,
+        config
+      });
+      const presetName = initialBlastFormSettings.preset;
+      const parameters = config.presets.settings[programName][presetName];
+      state.settings.sequenceType = sequenceType;
+      state.settings.sequenceSelectionMode = isAutomatic
+        ? 'automatic'
+        : 'manual';
+      state.settings.program = programName;
+      state.settings.preset = presetName;
+      state.settings.parameters = { ...parameters, database: selectedDatabase };
+    },
+    setBlastDatabase(
+      state,
+      action: PayloadAction<{
+        database: string;
+        config: BlastSettingsConfig;
+      }>
+    ) {
+      const { database, config } = action.payload;
+      const sequenceType = state.settings.sequenceType;
+      const databaseSequenceType = getDatabaseSequenceType({
+        database,
+        config
+      });
+      const programName = getProgamName({
+        sequenceType,
+        databaseSequenceType,
+        config
+      });
+      const presetName = initialBlastFormSettings.preset;
+      const parameters = config.presets.settings[programName][presetName];
+      state.settings.program = programName;
+      state.settings.preset = presetName;
+      state.settings.parameters = { ...parameters, database };
+    },
+    setBlastProgram(
+      state,
+      action: PayloadAction<{
+        program: BlastProgram;
+        config: BlastSettingsConfig;
+      }>
+    ) {
+      const { program, config } = action.payload;
+      const presetName = initialBlastFormSettings.preset;
+      const parameters = config.presets.settings[program][presetName];
+      state.settings.program = program;
+      state.settings.preset = presetName;
+      state.settings.parameters = {
+        ...parameters,
+        database: state.settings.parameters.database
+      };
+    },
+    changeSensitivityPresets(
+      state,
+      action: PayloadAction<{
+        presetName: string;
+        config: BlastSettingsConfig;
+      }>
+    ) {
+      const { presetName, config } = action.payload;
+      const program = state.settings.program;
+      const parameters = config.presets.settings[program][presetName];
+      state.settings.preset = presetName;
+      state.settings.parameters = {
+        ...parameters,
+        database: state.settings.parameters.database
+      };
+    },
+    setBlastParameter(
+      state,
+      action: PayloadAction<{
+        parameterName: BlastParameterName;
+        parameterValue: string;
+      }>
+    ) {
+      const { parameterName, parameterValue } = action.payload;
+      state.settings.parameters[parameterName] = parameterValue;
+    },
     clearForm() {
+      // TODO: apply default settings to the form
       return cloneDeep(initialState);
     }
   }
@@ -83,6 +234,11 @@ export const {
   switchToSpeciesStep,
   switchToSequencesStep,
   updateSelectedSpecies,
-  clearSelectedSpecies
+  clearSelectedSpecies,
+  setSequenceType,
+  setBlastDatabase,
+  setBlastProgram,
+  changeSensitivityPresets,
+  setBlastParameter
 } = blastFormSlice.actions;
 export default blastFormSlice.reducer;
