2e3151e3cfe7922594635f83644bb18f7cf9f37b
Andrey Azov <andrey@ebi.ac.uk>
2022-05-03 23:03:09 +0100

    Add the ButtonLink component

---
+++ src/shared/components/button-link/ButtonLink.tsx
@@ -0,0 +1,51 @@
+/**
+ * See the NOTICE file distributed with this work for additional information
+ * regarding copyright ownership.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import React from 'react';
+import { NavLink, NavLinkProps } from 'react-router-dom';
+import classNames from 'classnames';
+
+import styles from './ButtonLink.scss';
+
+/**
+ * This is a link that looks like a button.
+ * Using React-Router's Link component
+ */
+
+type Props = NavLinkProps;
+
+const ButtonLink = (props: Props) => {
+  const { className, ...otherProps } = props;
+
+  const inactiveLinkClasses = classNames(styles.buttonLink, className);
+
+  const activeLinkClasses = classNames(
+    styles.buttonLink,
+    styles.buttonLinkActive,
+    className
+  );
+
+  return (
+    <NavLink
+      {...otherProps}
+      className={({ isActive }) =>
+        isActive ? activeLinkClasses : inactiveLinkClasses
+      }
+    />
+  );
+};
+
+export default ButtonLink;
---
+++ stories/shared-components/button-link/ButtonLink.stories.tsx
@@ -0,0 +1,37 @@
+/**
+ * See the NOTICE file distributed with this work for additional information
+ * regarding copyright ownership.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import React from 'react';
+import { MemoryRouter } from 'react-router';
+
+import ButtonLink from 'src/shared/components/button-link/ButtonLink';
+
+import styles from './ButtonLink.stories.scss';
+
+export default {
+  title: 'Components/Shared Components/ButtonLink'
+};
+
+export const ButtonLinkStory = () => (
+  <MemoryRouter initialEntries={['/foo']}>
+    <div className={styles.wrapper}>
+      <ButtonLink to="/foo">I am a link</ButtonLink>
+      <ButtonLink to="/">I am a link</ButtonLink>
+    </div>
+  </MemoryRouter>
+);
+
+ButtonLinkStory.storyName = 'default';
