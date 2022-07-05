63821a1242223c8f8383ba339ae6f21c77891ff0
Manoj Pandian Sakthivel <msmanojpandian@gmail.com>
2022-07-05 15:23:43 +0100

    Put back UA

--- src/services/google-analytics/loadGoogleAnalytics.ts
+++ src/services/google-analytics/loadGoogleAnalytics.ts
@@ -60,7 +60,9 @@ const createGAShim = (trackerId: string) => {
 
   // The statement below automatically sends Pageview
   // TODO: Check if we need to disable it by passing: `{send_page_view: false}` as the third arg
-  window.gtag('config', trackerId);
+  window.gtag('config', 'UA-58710484-17');
+
+  window.gtag('config', trackerId, { debug_mode: true });
 };
 
 const loadScript = (trackerId: string) => {
