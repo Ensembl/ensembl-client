3d61dc61eee9e845e05113414fdfcb1adff64037
Ridwan Amode <ridwan@ebi.ac.uk>
2022-07-06 09:54:53 +0100

    update blast error message

--- src/content/app/tools/blast/components/blast-input-sequences/BlastInputSequence.tsx
+++ src/content/app/tools/blast/components/blast-input-sequences/BlastInputSequence.tsx
@@ -55,7 +55,7 @@ type Props = {
 };
 
 const errorTooltipDescription =
-  'Please check that all your sequences are nucleotide or protein, and that they do not contain any invalid characters';
+  'Please check the uploaded sequences are either all nucleotide or all protein, remove any invalid characters and ensure all sequences are over 5 bases long.';
 
 const BlastInputSequence = (props: Props) => {
   const {
