c5a95cddd0e3bd9c2ac96922258b0df433920ba2
Imran Salam <imran@ebi.ac.uk>
2022-07-20 12:05:58 +0100

    fix wrong updateObjectTrackStates call

--- src/content/app/genome-browser/components/track-panel/components/track-panel-list/track-panel-items/TrackPanelGene.tsx
+++ src/content/app/genome-browser/components/track-panel/components/track-panel-list/track-panel-items/TrackPanelGene.tsx
@@ -106,7 +106,7 @@ const TrackPanelGene = (props: TrackPanelGeneProps) => {
     dispatch(
       updateObjectTrackStates({
         status: transcriptIds.length ? Status.SELECTED : Status.UNSELECTED,
-        transcripts: transcriptIds
+        transcriptIds
       })
     );
   };
