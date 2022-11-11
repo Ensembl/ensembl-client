5c7fff36627d0c68512189bd02cdae3185224b56
Jyothish <joyjoy.nt@gmail.com>
2022-11-11 09:43:03 +0000

    Show help text for BLAST params labels in a tooltip (#875)

--- src/content/app/tools/blast/components/blast-settings/BlastSettings.tsx
+++ src/content/app/tools/blast/components/blast-settings/BlastSettings.tsx
@@ -19,6 +19,7 @@ import classNames from 'classnames';
 
 import { useAppSelector } from 'src/store';
 
+import useHover from 'src/shared/hooks/useHover';
 import useBlastForm from 'src/content/app/tools/blast/hooks/useBlastForm';
 
 import ShowHide from 'src/shared/components/show-hide/ShowHide';
@@ -27,6 +28,7 @@ import SimpleSelect from 'src/shared/components/simple-select/SimpleSelect';
 import ShadedInput from 'src/shared/components/input/ShadedInput';
 import BlastJobSubmit from 'src/content/app/tools/blast/components/blast-job-submit/BlastJobSubmit';
 import BlastJobListsNavigation from '../blast-job-lists-navigation/BlastJobListsNavigation';
+import Tooltip from 'src/shared/components/tooltip/Tooltip';
 
 import {
   getSelectedSequenceType,
@@ -149,25 +151,28 @@ const BlastSettings = ({ config }: Props) => {
           <h1 className={styles.title}>Blast</h1>
           <div className={styles.runJob}>Run a job</div>
           <div>
-            {buildSelect({
-              ...(config.parameters['database'] as BlastSelectSetting),
-              selectedOption: blastParameters.database as string,
-              onChange: onDatabaseChange
-            })}
+            <BlastSelect
+              options={config.parameters.database.options as Option[]}
+              label={config.parameters.database.label}
+              selectedOption={blastParameters.database as string}
+              onChange={onDatabaseChange}
+            />
           </div>
           <div>
-            {buildSelect({
-              ...(availableBlastPrograms as BlastSelectSetting),
-              selectedOption: blastProgram,
-              onChange: onBlastProgramChange
-            })}
+            <BlastSelect
+              options={availableBlastPrograms.options}
+              label={availableBlastPrograms.label}
+              selectedOption={blastProgram}
+              onChange={onBlastProgramChange}
+            />
           </div>
           <div>
-            {buildSelect({
-              ...(getPresetsList(config) as BlastSelectSetting),
-              selectedOption: searchSensitivity,
-              onChange: onSearchSensitivityChange
-            })}
+            <BlastSelect
+              options={getPresetsList(config).options}
+              label={getPresetsList(config).label}
+              selectedOption={searchSensitivity}
+              onChange={onSearchSensitivityChange}
+            />
           </div>
           <div>
             <ShowHide
@@ -186,88 +191,126 @@ const BlastSettings = ({ config }: Props) => {
       {parametersExpanded && (
         <div className={styles.bottomLevel}>
           <div className={styles.parametersColumn}>
-            {buildSelect({
-              ...(config.parameters['alignments'] as BlastSelectSetting),
-              selectedOption: blastParameters.alignments as string,
-              onChange: (value: string) =>
+            <BlastSelect
+              options={config.parameters.alignments.options as Option[]}
+              label={config.parameters.alignments.label}
+              description={config.parameters.alignments.description}
+              selectedOption={blastParameters.alignments as string}
+              onChange={(value: string) =>
                 onBlastParameterChange('alignments', value)
-            })}
-            {buildSelect({
-              ...(config.parameters['scores'] as BlastSelectSetting),
-              selectedOption: blastParameters.scores as string,
-              onChange: (value: string) =>
+              }
+            />
+
+            <BlastSelect
+              options={config.parameters.scores.options as Option[]}
+              label={config.parameters.scores.label}
+              description={config.parameters.scores.description}
+              selectedOption={blastParameters.scores as string}
+              onChange={(value: string) =>
                 onBlastParameterChange('scores', value)
-            })}
+              }
+            />
           </div>
           <div className={styles.parametersColumn}>
-            {buildSelect({
-              ...(config.parameters['exp'] as BlastSelectSetting),
-              selectedOption: blastParameters.exp as string,
-              onChange: (value: string) => onBlastParameterChange('exp', value)
-            })}
-            {buildSelect({
-              ...(config.parameters['compstats'] as BlastSelectSetting),
-              selectedOption: blastParameters.compstats as string,
-              onChange: (value: string) =>
+            <BlastSelect
+              options={config.parameters.exp.options as Option[]}
+              label={config.parameters.exp.label}
+              description={config.parameters.exp.description}
+              selectedOption={blastParameters.exp as string}
+              onChange={(value: string) => onBlastParameterChange('exp', value)}
+            />
+
+            <BlastSelect
+              options={config.parameters.compstats.options as Option[]}
+              label={config.parameters.compstats.label}
+              description={config.parameters.compstats.description}
+              selectedOption={blastParameters.compstats as string}
+              onChange={(value: string) =>
                 onBlastParameterChange('compstats', value)
-            })}
+              }
+            />
           </div>
           <div className={styles.parametersColumn}>
-            {buildSelect({
-              ...(config.parameters['hsps'] as BlastSelectSetting),
-              selectedOption: blastParameters.hsps as string,
-              onChange: (value: string) => onBlastParameterChange('hsps', value)
-            })}
-            {buildSelect({
-              ...(config.parameters['dropoff'] as BlastSelectSetting),
-              selectedOption: blastParameters.dropoff as string,
-              onChange: (value: string) =>
+            <BlastSelect
+              options={config.parameters.hsps.options as Option[]}
+              label={config.parameters.hsps.label}
+              description={config.parameters.hsps.description}
+              selectedOption={blastParameters.hsps as string}
+              onChange={(value: string) =>
+                onBlastParameterChange('hsps', value)
+              }
+            />
+
+            <BlastSelect
+              options={config.parameters.dropoff.options as Option[]}
+              label={config.parameters.dropoff.label}
+              description={config.parameters.dropoff.description}
+              selectedOption={blastParameters.dropoff as string}
+              onChange={(value: string) =>
                 onBlastParameterChange('dropoff', value)
-            })}
+              }
+            />
           </div>
           <div className={styles.parametersColumn}>
-            {buildSelect({
-              ...(config.parameters['gapopen'] as BlastSelectSetting),
-              selectedOption: blastParameters.gapopen as string,
-              onChange: (value: string) =>
+            <BlastSelect
+              options={config.parameters.gapopen.options as Option[]}
+              label={config.parameters.gapopen.label}
+              description={config.parameters.gapopen.description}
+              selectedOption={blastParameters.gapopen as string}
+              onChange={(value: string) =>
                 onBlastParameterChange('gapopen', value)
-            })}
-            {buildSelect({
-              ...(config.parameters['gapext'] as BlastSelectSetting),
-              selectedOption: blastParameters.gapext as string,
-              onChange: (value: string) =>
+              }
+            />
+
+            <BlastSelect
+              options={config.parameters.gapext.options as Option[]}
+              label={config.parameters.gapext.label}
+              description={config.parameters.gapext.description}
+              selectedOption={blastParameters.gapext as string}
+              onChange={(value: string) =>
                 onBlastParameterChange('gapext', value)
-            })}
+              }
+            />
           </div>
 
           <div className={styles.parametersColumn}>
-            {buildSelect({
-              options: config.programs_parameters_override.wordsize[
-                blastProgram
-              ]
-                ? config.programs_parameters_override.wordsize[blastProgram]
-                    .options
-                : (config.parameters.wordsize.options as Option[]),
-              label: config.parameters.wordsize.label,
-              selectedOption: blastParameters.wordsize as string,
-              onChange: (value: string) =>
+            <BlastSelect
+              options={
+                config.programs_parameters_override.wordsize[blastProgram]
+                  ? config.programs_parameters_override.wordsize[blastProgram]
+                      .options
+                  : (config.parameters.wordsize.options as Option[])
+              }
+              label={config.parameters.wordsize.label}
+              description={config.parameters.wordsize.description}
+              selectedOption={blastParameters.wordsize as string}
+              onChange={(value: string) =>
                 onBlastParameterChange('wordsize', value)
-            })}
-            {databaseSequenceType === 'dna' &&
-              buildSelect({
-                ...(config.parameters['match_scores'] as BlastSelectSetting),
-                selectedOption: blastParameters.match_scores as string,
-                onChange: (value: string) =>
+              }
+            />
+            {databaseSequenceType === 'dna' && (
+              <BlastSelect
+                options={config.parameters.match_scores.options as Option[]}
+                label={config.parameters.match_scores.label}
+                description={config.parameters.match_scores.description}
+                selectedOption={blastParameters.match_scores as string}
+                onChange={(value: string) =>
                   onBlastParameterChange('match_scores', value)
-              })}
+                }
+              />
+            )}
             <div className={styles.matrixSetting}>
-              {databaseSequenceType === 'protein' &&
-                buildSelect({
-                  ...(config.parameters['matrix'] as BlastSelectSetting),
-                  selectedOption: blastParameters.matrix as string,
-                  onChange: (value: string) =>
+              {databaseSequenceType === 'protein' && (
+                <BlastSelect
+                  options={config.parameters.matrix.options as Option[]}
+                  label={config.parameters.matrix.label}
+                  description={config.parameters.matrix.description}
+                  selectedOption={blastParameters.matrix as string}
+                  onChange={(value: string) =>
                     onBlastParameterChange('matrix', value)
-                })}
+                  }
+                />
+              )}
             </div>
           </div>
 
@@ -278,13 +321,13 @@ const BlastSettings = ({ config }: Props) => {
             )}
           >
             {buildCheckbox({
-              ...(config.parameters['gapalign'] as BlastBooleanSetting),
+              ...(config.parameters.gapalign as BlastBooleanSetting),
               selectedOption: blastParameters.gapalign as string,
               onChange: (value: string) =>
                 onBlastParameterChange('gapalign', value)
             })}
             {buildCheckbox({
-              ...(config.parameters['filter'] as BlastBooleanSetting),
+              ...(config.parameters.filter as BlastBooleanSetting),
               selectedOption: blastParameters.filter as string,
               onChange: (value: string) =>
                 onBlastParameterChange('filter', value)
@@ -319,28 +362,37 @@ const BlastSubmissionName = () => {
   );
 };
 
-// to be replaced with a simple select component
-const buildSelect = (setting: {
+type BlastSelectProps = {
   options: Option[];
   label: string;
+  description?: string;
   selectedOption: string;
   onChange: (value: string) => void;
-}) => {
+};
+
+const BlastSelect = (setting: BlastSelectProps) => {
   const onChange = (e: FormEvent<HTMLSelectElement>) => {
     const value = e.currentTarget.value;
     setting.onChange(value);
   };
 
+  const [hoverRef, isHovered] = useHover<HTMLSpanElement>();
+
   return (
     <div className={styles.select}>
       <label>
-        <span>{setting.label}</span>
+        <span ref={hoverRef}>{setting.label}</span>
         <SimpleSelect
           value={setting.selectedOption}
           onInput={onChange}
           options={setting.options}
         />
       </label>
+      {isHovered && setting.description && (
+        <Tooltip anchor={hoverRef.current} autoAdjust={true}>
+          {setting.description}
+        </Tooltip>
+      )}
     </div>
   );
 };
