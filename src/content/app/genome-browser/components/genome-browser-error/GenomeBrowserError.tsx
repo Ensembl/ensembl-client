0926a060408e1e859d611900dd8756c81c61dc19
Andrey Azov <andrey@ebi.ac.uk>
2022-11-30 12:40:39 +0000

    Update genome browser to 0.5.2 (#882)
    - New vertical scrolling of the genome browser
    - Handling of out-of-date error messages from the genome browser

---
+++ src/content/app/genome-browser/components/genome-browser-error/GenomeBrowserError.tsx
@@ -0,0 +1,71 @@
+/**
+ * See the NOTICE file distributed with this work for additional information
+ * regarding copyright ownership.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import React from 'react';
+
+import AlertButton from 'src/shared/components/alert-button/AlertButton';
+import { PrimaryButton } from 'src/shared/components/button/Button';
+
+import {
+  GenomeBrowserErrorType,
+  type GenomeBrowserError as GenomeBrowserErrorObj
+} from '@ensembl/ensembl-genome-browser';
+
+import styles from './GenomeBrowserError.scss';
+
+type Props = {
+  error: GenomeBrowserErrorObj;
+};
+
+const GenomeBrowserError = (props: Props) => {
+  const { error } = props;
+
+  if (error.type === GenomeBrowserErrorType.BAD_VERSION) {
+    return <GenomeBrowserVersionError />;
+  } else {
+    return <GenericGenomeBrowserError />;
+  }
+};
+
+const GenomeBrowserVersionError = () => {
+  return (
+    <div className={styles.container}>
+      <AlertButton />
+      <div>
+        <p className={styles.errorText}>We have updated the genome browser</p>
+        <p>Please reload this page to continue</p>
+      </div>
+      <PrimaryButton onClick={reloadPage}>Reload</PrimaryButton>
+    </div>
+  );
+};
+
+const GenericGenomeBrowserError = () => {
+  return (
+    <div className={styles.container}>
+      <AlertButton />
+      <div>
+        <p className={styles.errorText}>Genome browser has crashed</p>
+        <p>Please try reloading the page</p>
+      </div>
+      <PrimaryButton onClick={reloadPage}>Reload</PrimaryButton>
+    </div>
+  );
+};
+
+const reloadPage = () => window.location.reload();
+
+export default GenomeBrowserError;
