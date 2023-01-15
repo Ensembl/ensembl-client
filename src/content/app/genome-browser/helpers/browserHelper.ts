da7231850dda481fad2e49a7673fdd119f56aa7f
Andrey Azov <andrey@ebi.ac.uk>
2023-01-15 00:47:55 +0000

    A location is only allowed to have a start and an end coordinate

--- src/content/app/genome-browser/helpers/browserHelper.ts
+++ src/content/app/genome-browser/helpers/browserHelper.ts
@@ -31,21 +31,37 @@ type GenomeBrowserFocusIdConstituents = {
   objectId: string;
 };
 
+// A valid location string has a format of "regionName:start-end"
 export function getChrLocationFromStr(chrLocationStr: string): ChrLocation {
-  const [chrCode, chrRegion] = chrLocationStr.split(':');
-  const [startBp, endBp] = chrRegion.split('-');
+  const [regionName, coordinates] = chrLocationStr.split(':');
+  const splitCoordinates = coordinates.split('-');
+
+  if (splitCoordinates.length !== 2) {
+    throw new Error(
+      'There can only be a start and an end coordinate in a location string'
+    );
+  }
+
+  const [startBp, endBp] = coordinates.split('-');
 
   // We should only allow digits in start and end coordinates of location strings
   // Anything else (commas, white spaces, whatever) will be validated by the backend
-  const coordinateTestRegex = /[^\d]/;
+  const invalidCoordinateRegex = /[^\d]/;
 
-  if (startBp.match(coordinateTestRegex) || endBp.match(coordinateTestRegex)) {
+  if (
+    startBp.match(invalidCoordinateRegex) ||
+    endBp.match(invalidCoordinateRegex)
+  ) {
     throw new Error(
       'Start and end coordinates of a region must consist of digits only'
     );
   }
 
-  return [chrCode, parseInt(startBp, 10), parseInt(endBp, 10)] as ChrLocation;
+  return [
+    regionName,
+    parseInt(startBp, 10),
+    parseInt(endBp, 10)
+  ] as ChrLocation;
 }
 
 export function getChrLocationStr(
