4cf0d5e298e6cb150313a77fcfed36cb2c6e5309
Andrey Azov <andrey@ebi.ac.uk>
2023-03-02 14:46:11 +0000

    Add zmenu for focus variant track (#944)

---
+++ src/content/app/genome-browser/components/zmenu/zmenus/GeneAndOneTranscriptZmenu.tsx
@@ -0,0 +1,98 @@
+/**
+ * See the NOTICE file distributed with this work for additional information
+ * regarding copyright ownership.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import React, { useEffect } from 'react';
+
+import { useAppDispatch } from 'src/store';
+
+import { changeHighlightedTrackId } from 'src/content/app/genome-browser/state/track-panel/trackPanelSlice';
+
+import { ToolboxExpandableContent } from 'src/shared/components/toolbox';
+import ZmenuContent from '../ZmenuContent';
+import ZmenuInstantDownload from '../ZmenuInstantDownload';
+
+import {
+  type ZmenuContentTranscript,
+  type ZmenuContentGene,
+  type ZmenuCreatePayload
+} from '@ensembl/ensembl-genome-browser';
+
+import styles from '../Zmenu.scss';
+
+type Props = {
+  payload: ZmenuCreatePayload;
+  onDestroy: () => void;
+};
+
+const GeneAndOneTranscriptZmenu = (props: Props) => {
+  const { content } = props.payload;
+  const dispatch = useAppDispatch();
+
+  let featureId = '',
+    transcriptId = '',
+    gene: ZmenuContentGene | undefined;
+
+  const features: (ZmenuContentTranscript | ZmenuContentGene)[] = [];
+
+  const transcript = content.find(
+    (feature) => feature.metadata.type === 'transcript'
+  ) as ZmenuContentTranscript;
+
+  if (transcript) {
+    features.push(transcript);
+
+    gene = content.find(
+      (feature) =>
+        feature.metadata.type === 'gene' &&
+        feature.metadata.versioned_id === transcript?.metadata.gene_id
+    ) as ZmenuContentGene;
+
+    if (gene) {
+      features.push(gene);
+    }
+
+    transcriptId = transcript.metadata.versioned_id;
+    featureId = `gene:${gene.metadata.unversioned_id}`;
+  }
+
+  useEffect(() => {
+    gene && dispatch(changeHighlightedTrackId(gene.metadata.track));
+  }, []);
+
+  const mainContent = (
+    <ZmenuContent
+      features={features}
+      featureId={featureId}
+      destroyZmenu={props.onDestroy}
+    />
+  );
+
+  const footerContent = (
+    <div className={styles.zmenuFooterContent}>
+      <ZmenuInstantDownload id={transcriptId} />
+    </div>
+  );
+
+  return (
+    <ToolboxExpandableContent
+      mainContent={mainContent}
+      footerContent={footerContent}
+      className={styles.toolBox}
+    />
+  );
+};
+
+export default GeneAndOneTranscriptZmenu;
