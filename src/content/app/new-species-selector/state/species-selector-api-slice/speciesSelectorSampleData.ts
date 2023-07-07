62d82173798cae3bda4182cfb789f7534d537690
Andrey Azov <andrey@ebi.ac.uk>
2023-07-07 23:42:05 +0100

    Add a list of popular species to the new species selector screen (#1001)

--- src/content/app/new-species-selector/state/species-selector-api-slice/speciesSelectorSampleData.ts
+++ src/content/app/new-species-selector/state/species-selector-api-slice/speciesSelectorSampleData.ts
@@ -25,6 +25,333 @@ export const popularSpecies: PopularSpecies[] = [
       'https://staging-2020.ensembl.org/static/genome_images/homo_sapiens_GCA_000001405_14.svg', // TODO: change this to updated human image
     members_count: 2,
     is_selected: false
+  },
+  {
+    id: 2,
+    name: 'Mouse',
+    image:
+      'https://staging-2020.ensembl.org/static/genome_images/mus_musculus.svg',
+    members_count: 16,
+    is_selected: false
+  },
+  {
+    id: 3,
+    name: 'Zebrafish',
+    image:
+      'https://staging-2020.ensembl.org/static/genome_images/danio_rerio.svg',
+    members_count: 1,
+    is_selected: false
+  },
+  {
+    id: 4,
+    name: 'Wheat',
+    image:
+      'https://staging-2020.ensembl.org/static/genome_images/triticum_aestivum.svg',
+    members_count: 1,
+    is_selected: false
+  },
+  {
+    id: 5,
+    name: 'Rice',
+    image:
+      'https://staging-2020.ensembl.org/static/genome_images/oryza_sativa.svg',
+    members_count: 1,
+    is_selected: false
+  },
+  {
+    id: 6,
+    name: 'Thale cress',
+    image:
+      'https://staging-2020.ensembl.org/static/genome_images/arabidopsis_thaliana.svg',
+    members_count: 1,
+    is_selected: false
+  },
+  {
+    id: 7,
+    name: 'Cattle',
+    image:
+      'https://staging-2020.ensembl.org/static/genome_images/bos_taurus.svg',
+    members_count: 1,
+    is_selected: false
+  },
+  {
+    id: 8,
+    name: 'Rat',
+    image:
+      'https://staging-2020.ensembl.org/static/genome_images/rattus_norvegicus.svg',
+    members_count: 1,
+    is_selected: false
+  },
+  {
+    id: 9,
+    name: 'Pig',
+    image:
+      'https://staging-2020.ensembl.org/static/genome_images/sus_scrofa.svg',
+    members_count: 1,
+    is_selected: false
+  },
+  {
+    id: 10,
+    name: 'Maize',
+    image: 'https://staging-2020.ensembl.org/static/genome_images/zea_mays.svg',
+    members_count: 1,
+    is_selected: false
+  },
+  {
+    id: 11,
+    name: 'Chicken',
+    image:
+      'https://staging-2020.ensembl.org/static/genome_images/gallus_gallus.svg',
+    members_count: 1,
+    is_selected: false
+  },
+  {
+    id: 12,
+    name: 'Dog',
+    image:
+      'https://staging-2020.ensembl.org/static/genome_images/canis_familiaris.svg',
+    members_count: 1,
+    is_selected: false
+  },
+  {
+    id: 13,
+    name: 'Barley',
+    image:
+      'https://staging-2020.ensembl.org/static/genome_images/hordeum_vulgare.svg',
+    members_count: 1,
+    is_selected: false
+  },
+  {
+    id: 14,
+    name: 'Drosophila melanogaster',
+    image:
+      'https://staging-2020.ensembl.org/static/genome_images/drosophila_melanogaster.svg',
+    members_count: 1,
+    is_selected: false
+  },
+  {
+    id: 15,
+    name: 'Japanese rice fish',
+    image:
+      'https://staging-2020.ensembl.org/static/genome_images/oryzias_latipes.svg',
+    members_count: 1,
+    is_selected: false
+  },
+  {
+    id: 16,
+    name: 'Sheep',
+    image:
+      'https://staging-2020.ensembl.org/static/genome_images/ovis_aries.svg',
+    members_count: 1,
+    is_selected: false
+  },
+  {
+    id: 17,
+    name: 'Grape',
+    image:
+      'https://staging-2020.ensembl.org/static/genome_images/vitis_vinifera.svg',
+    members_count: 1,
+    is_selected: false
+  },
+  {
+    id: 18,
+    name: 'Tomato',
+    image:
+      'https://staging-2020.ensembl.org/static/genome_images/solanum_lycopersicum.svg',
+    members_count: 1,
+    is_selected: false
+  },
+  {
+    id: 19,
+    name: 'Horse',
+    image:
+      'https://staging-2020.ensembl.org/static/genome_images/equus_caballus.svg',
+    members_count: 1,
+    is_selected: false
+  },
+  {
+    id: 20,
+    name: 'Rapeseed',
+    image:
+      'https://staging-2020.ensembl.org/static/genome_images/brassica_napus.svg',
+    members_count: 1,
+    is_selected: false
+  },
+  {
+    id: 21,
+    name: 'Potato',
+    image:
+      'https://staging-2020.ensembl.org/static/genome_images/solanum_tuberosum.svg',
+    members_count: 1,
+    is_selected: false
+  },
+  {
+    id: 22,
+    name: 'Rabbit',
+    image:
+      'https://staging-2020.ensembl.org/static/genome_images/oryctolagus_cuniculus.svg',
+    members_count: 1,
+    is_selected: false
+  },
+  {
+    id: 23,
+    name: 'Saccharomyces cerevisiae',
+    image:
+      'https://staging-2020.ensembl.org/static/genome_images/saccharomyces_cerevisiae.svg',
+    members_count: 1,
+    is_selected: false
+  },
+  {
+    id: 24,
+    name: 'C. elegans',
+    image:
+      'https://staging-2020.ensembl.org/static/genome_images/caenorhabditis_elegans.svg',
+    members_count: 1,
+    is_selected: false
+  },
+  {
+    id: 25,
+    name: 'Cat',
+    image:
+      'https://staging-2020.ensembl.org/static/genome_images/felis_catus.svg',
+    members_count: 1,
+    is_selected: false
+  },
+  {
+    id: 26,
+    name: 'Macaque',
+    image:
+      'https://staging-2020.ensembl.org/static/genome_images/macaca_mulatta.svg',
+    members_count: 1,
+    is_selected: false
+  },
+  {
+    id: 27,
+    name: 'Mexican tetra',
+    image:
+      'https://staging-2020.ensembl.org/static/genome_images/astyanax_mexicanus.svg',
+    members_count: 1,
+    is_selected: false
+  },
+  {
+    id: 28,
+    name: 'Chimpanzee',
+    image:
+      'https://staging-2020.ensembl.org/static/genome_images/pan_troglodytes.svg',
+    members_count: 1,
+    is_selected: false
+  },
+  {
+    id: 29,
+    name: 'Nile tilapia',
+    image:
+      'https://staging-2020.ensembl.org/static/genome_images/oreochromis_niloticus.svg',
+    members_count: 1,
+    is_selected: false
+  },
+  {
+    id: 30,
+    name: 'Goat',
+    image:
+      'https://staging-2020.ensembl.org/static/genome_images/capra_hircus.svg',
+    members_count: 1,
+    is_selected: false
+  },
+  {
+    id: 31,
+    name: 'Brassica oleracea',
+    image:
+      'https://staging-2020.ensembl.org/static/genome_images/brassica_oleracea.svg',
+    members_count: 1,
+    is_selected: false
+  },
+  {
+    id: 32,
+    name: 'Tropical clawed frog',
+    image:
+      'https://staging-2020.ensembl.org/static/genome_images/xenopus_tropicalis.svg',
+    members_count: 1,
+    is_selected: false
+  },
+  {
+    id: 33,
+    name: 'Soybean',
+    image:
+      'https://staging-2020.ensembl.org/static/genome_images/glycine_max.svg',
+    members_count: 1,
+    is_selected: false
+  },
+  {
+    id: 34,
+    name: 'Chinese hamster',
+    image:
+      'https://staging-2020.ensembl.org/static/genome_images/cricetulus_griseus.svg',
+    members_count: 1,
+    is_selected: false
+  },
+  {
+    id: 35,
+    name: 'Medicago truncatula',
+    image:
+      'https://staging-2020.ensembl.org/static/genome_images/medicago_truncatula.svg',
+    members_count: 1,
+    is_selected: false
+  },
+  {
+    id: 36,
+    name: 'Brassica rapa',
+    image:
+      'https://staging-2020.ensembl.org/static/genome_images/brassica_rapa.svg',
+    members_count: 1,
+    is_selected: false
+  },
+  {
+    id: 37,
+    name: 'Sorghum bicolor',
+    image:
+      'https://staging-2020.ensembl.org/static/genome_images/sorghum_bicolor.svg',
+    members_count: 1,
+    is_selected: false
+  },
+  {
+    id: 38,
+    name: 'Atlantic salmon',
+    image:
+      'https://staging-2020.ensembl.org/static/genome_images/salmo_salar.svg',
+    members_count: 1,
+    is_selected: false
+  },
+  {
+    id: 39,
+    name: 'Aegilops tauschii',
+    image:
+      'https://staging-2020.ensembl.org/static/genome_images/aegilops_tauschii.svg',
+    members_count: 1,
+    is_selected: false
+  },
+  {
+    id: 40,
+    name: 'E. coli',
+    image:
+      'https://staging-2020.ensembl.org/static/genome_images/escherichia_coli.svg',
+    members_count: 1,
+    is_selected: false
+  },
+  {
+    id: 41,
+    name: 'Plasmodium falciparum',
+    image:
+      'https://staging-2020.ensembl.org/static/genome_images/plasmodium_falciparum.svg',
+    members_count: 1,
+    is_selected: false
+  },
+  {
+    id: 42,
+    name: 'Durum wheat',
+    image:
+      'https://staging-2020.ensembl.org/static/genome_images/triticum_aestivum.svg',
+    members_count: 1,
+    is_selected: false
   }
 ];
 
