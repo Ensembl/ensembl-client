c0fc1c81f5d77f7cd524d36b7bd43f0c18b0d1cf
Andrey Azov <andrey@ebi.ac.uk>
2023-08-13 15:45:23 +0100

    Add ProportionIndicator component and its versions (#1012)
    The added components are, in effect, a linear and a circular progress indicators;
    but I did not want to call them ProgressIndicator, because that could be confusing
    (e.g. loading progress would be something different)
    
    The eventual decision was to name the added components thus:
    - LinearProportionIndicator — just the bar diagram
    - LinearPercentageIndicator — bar diagram with label; the input is between 0 and 100
    - LinearFractionIndicator — bar diagram with label; the input is between 0 and 1
    - CircularProportionIndicator — just the circle diagram
    - CircularPercentageIndicator– circle diagram with label; the input is between 0 and 100
    - CircularFractionIndicator – circle diagram with label; the input is between 0 and 1

---
+++ stories/shared-components/fraction-indicator/ProportionIndicator.stories.tsx
@@ -0,0 +1,136 @@
+/**
+ * See the NOTICE file distributed with this work for additional information
+ * regarding copyright ownership.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import React, { useState, type FormEvent } from 'react';
+
+import {
+  CircularPercentageIndicator,
+  CircularFractionIndicator
+} from 'src/shared/components/fraction-indicator/CircularProportionIndicator';
+import {
+  LinearPercentageIndicator,
+  LinearFractionIndicator
+} from 'src/shared/components/fraction-indicator/LinearProportionIndicator';
+
+import styles from './ProportionIndicator.stories.scss';
+
+const percentageControlDefaults = {
+  min: 0,
+  max: 100,
+  step: 0.5
+};
+
+const fractionControlDefaults = {
+  min: 0,
+  max: 1,
+  step: 0.01
+};
+
+const renderControls = (
+  props: {
+    value: number;
+    setValue: (val: number) => void;
+  } & typeof percentageControlDefaults
+) => {
+  const { value, setValue, min, max, step } = props;
+
+  const onChange = (event: FormEvent<HTMLInputElement>) => {
+    const newValue = parseFloat(event.currentTarget.value);
+    setValue(newValue);
+  };
+
+  return (
+    <div className={styles.controls}>
+      <label>Change value</label>
+      <input
+        type="range"
+        min={min}
+        max={max}
+        step={step}
+        value={value}
+        onChange={onChange}
+      />
+    </div>
+  );
+};
+
+const CircularPercentageIndicatorStory = () => {
+  const [value, setValue] = useState(20);
+
+  return (
+    <div>
+      <CircularPercentageIndicator value={value} />
+      {renderControls({ value, setValue, ...percentageControlDefaults })}
+    </div>
+  );
+};
+
+const CircularFractionIndicatorStory = () => {
+  const [value, setValue] = useState(0.3);
+
+  return (
+    <div>
+      <CircularFractionIndicator value={value} />
+      {renderControls({ value, setValue, ...fractionControlDefaults })}
+    </div>
+  );
+};
+
+const LinearPercentageIndicatorStory = () => {
+  const [value, setValue] = useState(20);
+
+  return (
+    <div>
+      <LinearPercentageIndicator value={value} />
+      {renderControls({ value, setValue, ...percentageControlDefaults })}
+    </div>
+  );
+};
+
+const LinearFractionIndicatorStory = () => {
+  const [value, setValue] = useState(0.3);
+
+  return (
+    <div>
+      <LinearFractionIndicator value={value} />
+      {renderControls({ value, setValue, ...fractionControlDefaults })}
+    </div>
+  );
+};
+
+export const ExportedCircularPercentageIndicatorStory = {
+  name: 'CircularPercentageIndicator',
+  render: () => <CircularPercentageIndicatorStory />
+};
+
+export const ExportedCircularFractionIndicatorStory = {
+  name: 'CirculaFractionIndicator',
+  render: () => <CircularFractionIndicatorStory />
+};
+
+export const ExportedLinearPercentageIndicatorStory = {
+  name: 'LinearPercentageIndicator',
+  render: () => <LinearPercentageIndicatorStory />
+};
+
+export const ExportedLinearFractionIndicatorStory = {
+  name: 'LinearFractionIndicator',
+  render: () => <LinearFractionIndicatorStory />
+};
+
+export default {
+  title: 'Components/Shared Components/ProportionIndicator'
+};
