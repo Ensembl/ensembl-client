efdac80e0a8c8ad0ef4eaa924a4b4daa99370698
Andrey Azov <andrey@ebi.ac.uk>
2023-09-05 14:30:56 +0100

    Undo changes to the middleware

--- src/server/middleware/proxyMiddleware.ts
+++ src/server/middleware/proxyMiddleware.ts
@@ -55,25 +55,15 @@ const serverConfig = getConfigForServer();
 */
 
 const createApiProxyMiddleware = () => {
-  const apiProxyMiddleware = createHttpProxyMiddleware(
-    ['/api/**', '!/api/tracks/**'],
-    {
-      target: 'https://staging-2020.ensembl.org',
-      changeOrigin: true,
-      secure: false
-    }
-  );
-
-  const browserProxyMiddleware = createHttpProxyMiddleware('/api/tracks/**', {
-    target: 'http://update.review.ensembl.org',
-    // pathRewrite: {
-    //   '^/api/browser': '/api' // rewrite path
-    // },
+  const apiProxyMiddleware = createHttpProxyMiddleware('/api', {
+    target: 'https://staging-2020.ensembl.org',
     changeOrigin: true,
     secure: false
   });
 
-  return [apiProxyMiddleware, browserProxyMiddleware];
+  // returning an array so that the specific proxies can be easily modified in local development
+  // (see example in the comment block above)
+  return [apiProxyMiddleware];
 };
 
 const createStaticAssetsMiddleware = () => {
