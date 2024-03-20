6a352f6ea35b6d6f1e2b154be9441e550feeed96
Andrey Azov <andrey@ebi.ac.uk>
2024-03-20 14:31:26 +0000

    Remove addon-essentials

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,7 +3,7 @@ import webpackConfig from './webpack';
 export default {
   stories: ['../stories/**/*.stories.tsx'],
   webpackFinal: config => webpackConfig(config),
-  addons: ['@storybook/addon-essentials', '@storybook/addon-webpack5-compiler-babel'],
+  addons: ['@storybook/addon-webpack5-compiler-babel'],
   framework: {
     name: '@storybook/react-webpack5',
     options: {}
