31762dee9dd7fd1e262cc026b996a1becfe48d7c
Andrey Azov <andrey@ebi.ac.uk>
2024-04-15 09:29:19 +0100

    Update storybook

--- stories/shared-components/selected-species/SelectedSpecies.stories.tsx
+++ stories/shared-components/selected-species/SelectedSpecies.stories.tsx
@@ -19,19 +19,15 @@ import React from 'react';
 import SpeciesLozenge from 'src/shared/components/selected-species/SpeciesLozenge';
 
 import speciesData from './speciesData';
+import { humanGenome } from 'src/shared/components/selected-species/fixtures/speciesTestData';
 
 import styles from './SelectedSpecies.stories.module.css';
 
 export default {
-  title: 'Components/Shared Components/Selected Species',
-  argTypes: { onClick: { action: 'Clicked' } }
+  title: 'Components/Shared Components/SpeciesLozenge'
 };
 
-type StoryArgs = {
-  onClick: () => void;
-};
-
-export const SelectedSpeciesStory = (args: StoryArgs) => {
+const SpeciesLozengeThemes = () => {
   const species = speciesData[0];
   const displayOption = 'common-name_assembly-name';
   return (
@@ -82,17 +78,130 @@ export const SelectedSpeciesStory = (args: StoryArgs) => {
           <span>red theme</span>
         </div>
       </div>
-      <p>A clickable lozenge will change the cursor to hover, like so:</p>
+      <p>
+        If a lozenge is disabled, the cursor hovering over it will not change
+        its shape:
+      </p>
       <div>
         <SpeciesLozenge
           speciesNameDisplayOption={displayOption}
-          theme="blue"
+          theme="grey"
           species={species}
-          onClick={args.onClick}
+          disabled={true}
         />
       </div>
     </>
   );
 };
 
-SelectedSpeciesStory.storyName = 'Themes';
+const SpeciesLozengeContentFormatting = () => {
+  return (
+    <>
+      <div className={styles.wrapper}>
+        <p>The different display options of species lozenge</p>
+
+        <p>1. Common name and assembly name</p>
+
+        <div className={styles.innerWrapper}>
+          <SpeciesLozenge
+            speciesNameDisplayOption="common-name_assembly-name"
+            theme="blue"
+            species={humanGenome}
+          />
+          <span>For species with a common name</span>
+        </div>
+
+        <div className={styles.innerWrapper}>
+          <SpeciesLozenge
+            speciesNameDisplayOption="common-name_assembly-name"
+            theme="blue"
+            species={{ ...humanGenome, common_name: null }}
+          />
+          <span>
+            For species without a common name (falls back to scientific name)
+          </span>
+        </div>
+
+        <p>2. Scientific name and assembly name</p>
+
+        <div className={styles.innerWrapper}>
+          <SpeciesLozenge
+            speciesNameDisplayOption="scientific-name_assembly-name"
+            theme="blue"
+            species={humanGenome}
+          />
+          <span>
+            A genome always has a species scientific name and an assembly name
+          </span>
+        </div>
+
+        <p>3. Common name, type, and assembly name</p>
+
+        <div className={styles.innerWrapper}>
+          <SpeciesLozenge
+            speciesNameDisplayOption="common-name_type_assembly-name"
+            theme="blue"
+            species={humanGenome}
+          />
+          <span>
+            For genome that both has a type, and is a reference assembly
+          </span>
+        </div>
+
+        <div className={styles.innerWrapper}>
+          <SpeciesLozenge
+            speciesNameDisplayOption="common-name_type_assembly-name"
+            theme="blue"
+            species={{ ...humanGenome, is_reference: false }}
+          />
+          <span>
+            For genome that has a type, but is not a reference assembly
+          </span>
+        </div>
+
+        <div className={styles.innerWrapper}>
+          <SpeciesLozenge
+            speciesNameDisplayOption="common-name_type_assembly-name"
+            theme="blue"
+            species={{ ...humanGenome, type: null }}
+          />
+          <span>
+            For genome that does not have a type, but is a reference assembly
+          </span>
+        </div>
+
+        <p>4. Scientific name, type, and assembly name</p>
+
+        <div className={styles.innerWrapper}>
+          <SpeciesLozenge
+            speciesNameDisplayOption="scientific-name_type_assembly-name"
+            theme="blue"
+            species={humanGenome}
+          />
+          <span>For a genome that has a type and is a reference assembly</span>
+        </div>
+
+        <p>5. Only assembly accession id</p>
+
+        <div className={styles.innerWrapper}>
+          <SpeciesLozenge
+            speciesNameDisplayOption="assembly-accession-id"
+            theme="blue"
+            species={humanGenome}
+          />
+          <span>A genome always has an accession id</span>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export const SpeciesLozengeThemesStory = {
+  name: 'Themes',
+  render: () => <SpeciesLozengeThemes />
+};
+
+export const SpeciesLozengeContentFormattingStory = {
+  name: 'Content formatting',
+  render: () => <SpeciesLozengeContentFormatting />
+};
