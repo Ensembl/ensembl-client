f1421b718bd390bbf94407f4e451f6e59b534201
Andrey Azov <andrey@ebi.ac.uk>
2024-05-14 16:35:42 +0100

    To show the comma, both the species type element, and the reference element should be present

--- src/content/app/species/components/species-page-sidebar/SpeciesPageSidebar.tsx
+++ src/content/app/species/components/species-page-sidebar/SpeciesPageSidebar.tsx
@@ -126,7 +126,7 @@ const SpeciesType = (props: GenomeInfo) => {
       <span className={styles.label}>Type</span>
       <span>
         {typeTextElement}
-        {referenceTextElement ? ', ' : ''}
+        {typeTextElement && referenceTextElement ? ', ' : ''}
         {referenceTextElement}
       </span>
     </div>
