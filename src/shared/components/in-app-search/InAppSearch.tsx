40928c8f95a866544da095cbd3b9f01521034a14
Andrey Azov <andrey@ebi.ac.uk>
2024-05-22 16:38:18 +0100

    Update a comment

--- src/shared/components/in-app-search/InAppSearch.tsx
+++ src/shared/components/in-app-search/InAppSearch.tsx
@@ -54,7 +54,7 @@ export type Props = {
   genomeIdForUrl: string; // this should be a temporary measure; it should be returned by search api
   mode: InAppSearchMode;
   onSearchSubmit?: (query: string) => void;
-  onMatchNavigation?: () => void; // currently, there are no requirements to this callback to pass any data
+  onMatchNavigation?: () => void; // currently, there are no requirements for this callback to receive any data
 };
 
 const InAppSearch = (props: Props) => {
