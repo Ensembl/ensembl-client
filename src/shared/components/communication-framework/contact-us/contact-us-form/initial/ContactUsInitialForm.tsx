Ensembl/ensembl-client

commit 687b870613514fd53047fbce0c11c1a73da51c36

Update dependencies (#1190)
Notable updates:
- React updated from v.18.3 to v.19
  - Types for refs seem to have been updated. For mutable refs, null is now explicit in the types.
     This required updates to the code
  - Removed all instances of using forwardRef as it has become deprecated
- Removed the react-spring (@react-spring/web) library, because we practically never used it.

src/shared/components/communication-framework/contact-us/contact-us-form/initial/ContactUsInitialForm.tsx
@@ -116,10 +116,11 @@ const reducer = (state: State, action: Action): State => {
       return { ...state, message: action.payload };
     case 'add-file':
       return { ...state, files: [...state.files, action.payload] };
-    case 'remove-file':
+    case 'remove-file': {
       const newFiles = [...state.files];
       newFiles.splice(action.payload, 1);
       return { ...state, files: newFiles };
+    }
     case 'replace-state':
       return action.payload;
     case 'clear-form':
@@ -142,8 +143,7 @@ const ContactUsInitialForm = () => {
 
   const emailFieldRef = useRef<HTMLInputElement | null>(null);
   const elementRef = useRef<HTMLDivElement | null>(null);
-  const stateRef = useRef<typeof state>();
-  stateRef.current = state;
+  const stateRef = useRef<typeof state>(state);
 
   const isFormValid = validate(state) && emailFieldValid;
 
@@ -167,7 +167,8 @@ const ContactUsInitialForm = () => {
   const callbackElementRef = useCallback((element: HTMLDivElement) => {
     // both register the top-level DOM element locally and pass it to the code that sets it up as drop area
     elementRef.current = element;
-    dropAreaRef(element);
+    const dropAreaRefCleanup = dropAreaRef(element) as () => void;
+    return () => dropAreaRefCleanup();
   }, []);
 
   const { clearSavedForm } = useSavedForm({

src/content/app/genome-browser/components/browser-cog/BrowserCog.tsx
@@ -15,7 +15,6 @@
  */
 
 import { useState } from 'react';
-import { useTransition, animated } from '@react-spring/web';
 
 import useGenomeBrowserAnalytics from 'src/content/app/genome-browser/hooks/useGenomeBrowserAnalytics';
 
@@ -44,13 +43,6 @@ const BrowserCog = (props: BrowserCogProps) => {
     icon: CogIcon
   };
 
-  const transition = useTransition(isPanelOpen, {
-    config: { duration: 100 },
-    enter: { opacity: 1 },
-    from: { opacity: 0 },
-    leave: { opacity: 0 }
-  });
-
   const openTrackSettingsPanel = () => {
     if (!isPanelOpen) {
       setIsPanelOpen(true);
@@ -59,13 +51,23 @@ const BrowserCog = (props: BrowserCogProps) => {
   };
 
   const closeTrackSettingsPanel = () => {
-    isPanelOpen && setIsPanelOpen(false);
+    if (isPanelOpen) {
+      setIsPanelOpen(false);
+    }
   };
 
   return (
     <>
       {isPanelOpen ? (
-        <CloseButton onClick={closeTrackSettingsPanel} />
+        <>
+          <CloseButton onClick={closeTrackSettingsPanel} />
+          <TrackSettingsPanel
+            trackId={trackId}
+            trackType={props.trackType}
+            onOutsideClick={closeTrackSettingsPanel}
+            className={styles.trackPanel}
+          />
+        </>
       ) : (
         <ImageButton
           className={styles.browserCog}
@@ -74,19 +76,6 @@ const BrowserCog = (props: BrowserCogProps) => {
           image={cogIconConfig.icon}
         />
       )}
-      {transition((style, item) => {
-        return (
-          item && (
-            <animated.div key="trackSettingsPanel" style={style}>
-              <TrackSettingsPanel
-                trackId={trackId}
-                trackType={props.trackType}
-                onOutsideClick={closeTrackSettingsPanel}
-              />
-            </animated.div>
-          )
-        );
-      })}
     </>
   );
 };

src/content/app/genome-browser/components/browser-cog/BrowserCogList.module.css
@@ -16,3 +16,17 @@
   height: 18px;
   padding: 1px;
 }
+
+.trackPanel {
+  animation: appearing-track-panel 0.1s ease-in;
+}
+
+@keyframes appearing-track-panel {
+  from {
+    opacity: 0;
+  }
+
+  to {
+    opacity: 100%;
+  }
+}

src/content/app/genome-browser/components/browser-location-indicator/BrowserLocationIndicator.tsx
@@ -29,8 +29,8 @@ import styles from './BrowserLocationIndicator.module.css';
 
 type Props = {
   className?: string;
-  containerRef?: RefObject<HTMLElement>;
-  nonOverlapElementRef?: RefObject<HTMLElement>;
+  containerRef?: RefObject<HTMLElement | null>;
+  nonOverlapElementRef?: RefObject<HTMLElement | null>;
 };
 
 export const BrowserLocationIndicator = (props: Props) => {
@@ -100,8 +100,8 @@ const ProximitySensor = ({
   onRegionNameVisibilityChange
 }: {
   regionName: string;
-  containerRef: RefObject<HTMLElement>;
-  nonOverlapElementRef: RefObject<HTMLElement>;
+  containerRef: RefObject<HTMLElement | null>;
+  nonOverlapElementRef: RefObject<HTMLElement | null>;
   isRegionNameVisible: boolean;
   onRegionNameVisibilityChange: (x: boolean) => void;
 }) => {

src/content/app/genome-browser/components/browser-track-legend/BrowserTrackLegend.tsx
@@ -29,7 +29,7 @@ import type { TrackLegendHotspotPayload } from 'src/content/app/genome-browser/s
 import styles from './BrowserTrackLegend.module.css';
 
 type Props = {
-  containerRef: RefObject<HTMLDivElement>;
+  containerRef: RefObject<HTMLDivElement | null>;
 };
 
 type Position = {

src/content/app/genome-browser/components/track-settings-panel/TrackSettingsPanel.tsx
@@ -28,6 +28,7 @@ type Props = {
   trackId: string;
   trackType: TrackType;
   onOutsideClick: () => void;
+  className?: string;
 };
 
 const getTrackSettingsPanelComponent = (trackType: TrackType) => {
@@ -56,7 +57,13 @@ export const TrackSettingsPanel = (props: Props) => {
 
   const Track = getTrackSettingsPanelComponent(trackType);
 
-  return <Track trackId={trackId} ref={trackSettingsRef} />;
+  return (
+    <Track
+      trackId={trackId}
+      className={props.className}
+      ref={trackSettingsRef}
+    />
+  );
 };
 
 export default TrackSettingsPanel;

src/content/app/genome-browser/components/track-settings-panel/track-settings-views/GeneTrackSettings.tsx
@@ -14,7 +14,8 @@
  * limitations under the License.
  */
 
-import { forwardRef, type ForwardedRef } from 'react';
+import classNames from 'classnames';
+import { type RefObject } from 'react';
 
 import { useAppSelector } from 'src/store';
 import useTrackSettings from '../useTrackSettings';
@@ -31,12 +32,11 @@ import styles from '../TrackSettingsPanel.module.css';
 
 type Props = {
   trackId: string;
+  className?: string;
+  ref?: RefObject<HTMLDivElement | null>;
 };
 
-export const GeneTrackSettings = (
-  props: Props,
-  ref: ForwardedRef<HTMLDivElement>
-) => {
+export const GeneTrackSettings = (props: Props) => {
   const { trackId } = props;
   const selectedTrackSettings = useAppSelector((state) =>
     getTrackSettingsForTrackId(state, trackId)
@@ -69,8 +69,13 @@ export const GeneTrackSettings = (
     updateTrackSetting('label', !shouldShowFeatureLabels);
   };
 
+  const componentStyles = classNames(
+    styles.trackSettingsPanel,
+    props.className
+  );
+
   return (
-    <div className={styles.trackSettingsPanel} ref={ref}>
+    <div className={componentStyles} ref={props.ref}>
       <div className={styles.section}>
         <div className={styles.subLabel}>All genes</div>
         <div>
@@ -122,4 +127,4 @@ export const GeneTrackSettings = (
   );
 };
 
-export default forwardRef(GeneTrackSettings);
+export default GeneTrackSettings;

src/content/app/genome-browser/components/track-settings-panel/track-settings-views/RegularTrackSettings.tsx
@@ -14,7 +14,8 @@
  * limitations under the License.
  */
 
-import { forwardRef, type ForwardedRef } from 'react';
+import classNames from 'classnames';
+import { type RefObject } from 'react';
 
 import { useAppSelector } from 'src/store';
 import useTrackSettings from '../useTrackSettings';
@@ -31,12 +32,11 @@ import styles from '../TrackSettingsPanel.module.css';
 
 type Props = {
   trackId: string;
+  className?: string;
+  ref?: RefObject<HTMLDivElement | null>;
 };
 
-export const RegularTrackSettings = (
-  props: Props,
-  ref: ForwardedRef<HTMLDivElement>
-) => {
+export const RegularTrackSettings = (props: Props) => {
   const { trackId } = props;
   const trackSettings = useAppSelector((state) =>
     getTrackSettingsForTrackId(state, trackId)
@@ -52,8 +52,13 @@ export const RegularTrackSettings = (
     updateTrackSetting('name', !shouldShowTrackName);
   };
 
+  const componentStyles = classNames(
+    styles.trackSettingsPanel,
+    props.className
+  );
+
   return (
-    <div className={styles.trackSettingsPanel} ref={ref}>
+    <div className={componentStyles} ref={props.ref}>
       <div className={styles.section}>
         <div className={styles.subLabel}>All tracks</div>
         <div>
@@ -76,4 +81,4 @@ export const RegularTrackSettings = (
   );
 };
 
-export default forwardRef(RegularTrackSettings);
+export default RegularTrackSettings;

commit 68247b22029f69446649e3ceb3d68641e6081752

Fix Contact Us form submission
This is a fix of the regression that occurred in commit 687b870
(upgrading from React 18 to React 19),
where the lines initializing and then updating a state ref:

  const stateRef = useRef<typeof state>();
  stateRef.current = state;

were replaced with a single initialization line:

  const stateRef = useRef<typeof state>(state);

As a result, the ref did not get synchronized with the value of the form state;
and so, the form was always submitting its initial value.

src/shared/components/communication-framework/contact-us/contact-us-form/initial/ContactUsInitialForm.tsx
@@ -16,6 +16,7 @@
 
 import {
   useState,
+  useEffect,
   useReducer,
   useCallback,
   useRef,
@@ -146,6 +147,10 @@ const ContactUsInitialForm = () => {
   const stateRef = useRef<typeof state>(state);
 
   const isFormValid = validate(state) && emailFieldValid;
+
+  useEffect(() => {
+    stateRef.current = state;
+  }, [state]);
 
   const onFileChange = useCallback(
     (files: File[]) => {