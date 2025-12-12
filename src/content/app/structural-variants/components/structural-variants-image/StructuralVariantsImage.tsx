21419c945e4e43539408896df7a265a275548a7a
Andrey Azov <andrey@ebi.ac.uk>
2025-12-12 17:13:56 +0000

    Enable the structural variants sandwich

--- src/content/app/structural-variants/components/structural-variants-image/StructuralVariantsImage.tsx
+++ src/content/app/structural-variants/components/structural-variants-image/StructuralVariantsImage.tsx
@@ -14,12 +14,15 @@
  * limitations under the License.
  */
 
-import '@ensembl/ensembl-structural-variants/alignments';
+import '@ensembl/ensembl-structural-variants';
 import type { DetailedHTMLProps, HTMLAttributes } from 'react';
 
 import config from 'config';
 
-import type { VariantAlignments } from '@ensembl/ensembl-structural-variants/alignments';
+import type {
+  EnsSvBrowser,
+  Endpoints
+} from '@ensembl/ensembl-structural-variants';
 
 const CHROMOSOME_LENGTH = 248956422; // length of chromosome 1
 const REGION_NAME = '1';
@@ -29,17 +32,23 @@ const INITIAL_END = 145_500_000;
 const chm13T2TGenomeId = '4c07817b-c7c5-463f-8624-982286bc4355';
 const grch38GenomeId = 'a7335667-93e7-11ec-a39d-005056b38ce3';
 
+const REFERENCE_TRACKS = ['sv-gene', '950a71e1-5229-459c-822f-d104506d24e8'];
+const ALT_TRACKS = ['sv-gene', 'a8691c70-7d68-4322-937d-938affb1b4ea'];
+
 const StructuralVariantsImage = () => {
   return (
     <div>
-      <ens-sv-alignments
+      <ens-sv-browser
         referenceGenomeId={chm13T2TGenomeId}
         altGenomeId={grch38GenomeId}
         start={INITIAL_START}
         end={INITIAL_END}
         regionName={REGION_NAME}
         regionLength={CHROMOSOME_LENGTH}
+        referenceTracks={REFERENCE_TRACKS}
+        altTracks={ALT_TRACKS}
         endpoints={{
+          genomeBrowser: 'https://dev-2020.ensembl.org/api/browser/data',
           alignments: `${config.structuralVariantsApiBaseUrl}/alignments`,
           variants: `${config.structuralVariantsApiBaseUrl}/variants`
         }}
@@ -48,9 +57,9 @@ const StructuralVariantsImage = () => {
   );
 };
 
-type EnsSVAlignmentsProps = DetailedHTMLProps<
-  HTMLAttributes<VariantAlignments>,
-  VariantAlignments
+type StructuralVariantsBrowserProps = DetailedHTMLProps<
+  HTMLAttributes<EnsSvBrowser>,
+  EnsSvBrowser
 > & {
   referenceGenomeId: string;
   altGenomeId: string;
@@ -58,17 +67,18 @@ type EnsSVAlignmentsProps = DetailedHTMLProps<
   end: number;
   regionName: string;
   regionLength: number;
-  endpoints: {
-    alignments: string;
-    variants: string;
-  };
+  altStart?: number;
+  altEnd?: number;
+  referenceTracks: string[];
+  altTracks: string[];
+  endpoints: Endpoints;
 };
 
 declare module 'react/jsx-runtime' {
   // eslint-disable-next-line @typescript-eslint/no-namespace
   namespace JSX {
     interface IntrinsicElements {
-      'ens-sv-alignments': EnsSVAlignmentsProps;
+      'ens-sv-browser': StructuralVariantsBrowserProps;
     }
   }
 }
