a1c83e4e40a71874dca6fef3aad392fff975b149
Andrey Azov <andrey@ebi.ac.uk>
2025-12-18 15:42:26 +0000

    Start url validation

--- src/content/app/structural-variants/hooks/useStructuralVariantsRouting.ts
+++ src/content/app/structural-variants/hooks/useStructuralVariantsRouting.ts
@@ -28,6 +28,7 @@ import {
   useGenomeGroupsQuery,
   useGenomesInGroupQuery
 } from 'src/content/app/structural-variants/state/api/structuralVariantsApiSlice';
+import { useGenomeKaryotypeQuery } from 'src/shared/state/genome/genomeApiSlice';
 import {
   setGenomesAndLocations,
   setLocations
@@ -38,6 +39,8 @@ import {
   getReferenceGenomeLocation,
   getAlternativeGenomeLocation
 } from 'src/content/app/structural-variants/state/general/structuralVariantsGeneralSelectors';
+
+import type { GenomeKaryotypeItem } from 'src/shared/state/genome/genomeTypes';
 
 const useStructuralVariantsRouting = () => {
   const [searchParams] = useSearchParams();
@@ -129,6 +132,137 @@ const useStructuralVariantsRouting = () => {
     referenceLocationParam,
     altLocationParam
   };
+};
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const useCheckedParams = ({
+  referenceGenomeIdParam,
+  altGenomeIdParam,
+  referenceLocationParam,
+  altLocationParam
+}: {
+  referenceGenomeIdParam: string | null;
+  altGenomeIdParam: string | null;
+  referenceLocationParam: string | null;
+  altLocationParam: string | null;
+}) => {
+  const { isFetching: isFetchingGenomeGroups, data: genomeGroupsData } =
+    useGenomeGroupsQuery();
+
+  const referenceGroup = genomeGroupsData?.genome_groups.find(
+    (group) => group.reference_genome.genome_id === referenceGenomeIdParam
+  );
+  const referenceGenome = referenceGroup?.reference_genome;
+  const isReferenceGenomeIdInvalid = genomeGroupsData && !referenceGenome;
+
+  const { isFetching: isFetchingGenomesInGroup, currentData: genomesInGroup } =
+    useGenomesInGroupQuery(referenceGroup?.id ?? '', {
+      skip: !referenceGroup || isReferenceGenomeIdInvalid
+    });
+  const altGenome = genomesInGroup?.genomes.find(
+    (genome) => genome.genome_id === altGenomeIdParam
+  );
+  const isAltGenomeIdValid = genomesInGroup && altGenome;
+
+  const {
+    isFetching: isFetchingReferenceGenomeKaryotype,
+    currentData: referenceGenomeKaryotype
+  } = useGenomeKaryotypeQuery(referenceGenome?.genome_id ?? '', {
+    skip: !referenceGenome
+  });
+
+  const {
+    isFetching: isFetchingAltGenomeKaryotype,
+    currentData: altGenomeKaryotype
+  } = useGenomeKaryotypeQuery(altGenome?.genome_id ?? '', {
+    skip: !altGenome
+  });
+
+  let referenceGenomeLocation: GenomicLocation | null = null;
+  let altGenomeLocation: GenomicLocation | null = null;
+  let isReferenceGenomeLocationValid = true;
+  let isAltGenomeLocationValid = true;
+
+  if (referenceLocationParam && referenceGenomeKaryotype) {
+    const refLocValidationResult = isLocationParameterValid({
+      locationString: referenceLocationParam,
+      karyotype: referenceGenomeKaryotype
+    });
+    if (refLocValidationResult.isValid) {
+      referenceGenomeLocation = refLocValidationResult.location;
+    } else {
+      isReferenceGenomeLocationValid = false;
+    }
+  }
+  if (altLocationParam && altGenomeKaryotype) {
+    const altLocValidationResult = isLocationParameterValid({
+      locationString: altLocationParam,
+      karyotype: altGenomeKaryotype
+    });
+    if (altLocValidationResult.isValid) {
+      altGenomeLocation = altLocValidationResult.location;
+    } else {
+      isAltGenomeLocationValid = false;
+    }
+  }
+
+  const isValidating =
+    isFetchingGenomeGroups ||
+    isFetchingGenomesInGroup ||
+    isFetchingReferenceGenomeKaryotype ||
+    isFetchingAltGenomeKaryotype;
+
+  return {
+    isValidating,
+    isReferenceGenomeIdValid: !isReferenceGenomeIdInvalid,
+    isAltGenomeIdValid,
+    referenceGenomeId: referenceGenome?.genome_id ?? null,
+    altGenomeId: altGenome?.genome_id ?? null,
+    referenceGenomeLocation,
+    altGenomeLocation,
+    isReferenceGenomeLocationValid,
+    isAltGenomeLocationValid
+  };
+};
+
+const isLocationParameterValid = ({
+  locationString,
+  karyotype
+}: {
+  locationString: string;
+  karyotype: GenomeKaryotypeItem[];
+}) => {
+  try {
+    const parsedLocation = getGenomicLocationFromString(locationString);
+    const regionFromKaryotype = karyotype.find(
+      (region) => region.name === parsedLocation.regionName
+    );
+    if (!regionFromKaryotype) {
+      throw Error('Region is not listed in karyotype');
+    }
+    const isStartValid =
+      parsedLocation.start > 0 &&
+      parsedLocation.start < regionFromKaryotype.length;
+    const isEndValid =
+      parsedLocation.end > parsedLocation.start &&
+      parsedLocation.end <= regionFromKaryotype.length;
+    if (!isStartValid) {
+      throw Error('The start coordinate is invalid');
+    } else if (!isEndValid) {
+      throw Error('The end coordinate is invalid');
+    } else {
+      return {
+        isValid: true,
+        location: parsedLocation
+      };
+    }
+  } catch {
+    // TODO: maybe use different types of errors to show different types of error messages?
+    return {
+      isValid: false,
+      location: null
+    };
+  }
 };
 
 const getLocationFromUrlParam = (locationString: string) => {
