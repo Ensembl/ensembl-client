7a7cbbecfd936c01ae4cf7e2c219c783d040198a
Andrey Azov <andrey@ebi.ac.uk>
2026-02-11 14:16:51 +0000

    Structural variants: Use proper url from the config as genome browser backend url (#1268)

--- src/content/app/structural-variants/components/structural-variants-image/StructuralVariantsImage.tsx
+++ src/content/app/structural-variants/components/structural-variants-image/StructuralVariantsImage.tsx
@@ -151,7 +151,7 @@ const StructuralVariantsImage = (props: Props) => {
         referenceTracks={trackIds.referenceGenomeTrackIds}
         altTracks={trackIds.altGenomeTrackIds}
         endpoints={{
-          genomeBrowser: 'https://dev-2020.ensembl.org/api/browser/data',
+          genomeBrowser: config.genomeBrowserBackendBaseUrl,
           alignments: `${config.structuralVariantsApiBaseUrl}/alignments`,
           variants: `${config.structuralVariantsApiBaseUrl}/variants`
         }}
