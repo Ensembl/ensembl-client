e02b6bd2557b0ec56fa0be5844093577dfcd427c
Andrey Azov <andrey@ebi.ac.uk>
2026-03-27 10:36:45 +0000

    Structural variants: updates after user feedback (#1282)
    - Hide some of the genome browser tracks by default (hard code the conditions for initial showing or hiding
      of tracks on the client; later on, track api should provide guidance)
    - Add a button next to the navigation buttons that opens tracks configuration modal in sidebar
    - Add a colour legend for short variants to the sidebar

---
+++ src/content/app/structural-variants/components/structural-variants-sidebar/short-variants-legend/ShortVariantsLegend.tsx
@@ -0,0 +1,53 @@
+/**
+ * See the NOTICE file distributed with this work for additional information
+ * regarding copyright ownership.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { Legend, LegendItem, LegendMarker } from 'src/shared/components/legend';
+
+import commonStyles from '../StructuralVariantsSidebar.module.css';
+import shortVariantsStyles from 'src/content/app/genome-browser/components/drawer/drawer-views/variant-group-legend/VariantGroupLegend.module.css';
+import styles from './ShortVariantsLegend.module.css';
+
+const ShortVariantsLegend = () => {
+  return (
+    <div className={commonStyles.legendSection}>
+      <div className={commonStyles.legendSectionHead}>Short variant groups</div>
+      <Legend className={styles.legend}>
+        <LegendItem>
+          <LegendMarker className={shortVariantsStyles.variantColour1} />
+          <span>Protein altering variants</span>
+        </LegendItem>
+        <LegendItem>
+          <LegendMarker className={shortVariantsStyles.variantColour2} />
+          <span>Splicing variants</span>
+        </LegendItem>
+        <LegendItem>
+          <LegendMarker className={shortVariantsStyles.variantColour3} />
+          <span>Transcript variants</span>
+        </LegendItem>
+        <LegendItem>
+          <LegendMarker className={shortVariantsStyles.variantColour4} />
+          <span>Regulatory region variants</span>
+        </LegendItem>
+        <LegendItem>
+          <LegendMarker className={shortVariantsStyles.variantColour5} />
+          <span>Intergenic variants</span>
+        </LegendItem>
+      </Legend>
+    </div>
+  );
+};
+
+export default ShortVariantsLegend;
