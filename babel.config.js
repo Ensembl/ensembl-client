10d691a53920347d4cf7d1b6f1f8d7339db1785c
Andrey Azov <andrey@ebi.ac.uk>
2026-07-20 16:32:32 +0100

    Complete babel update

--- babel.config.js
+++ babel.config.js
@@ -1,56 +1,19 @@
-const isTargetWeb = api =>
-  api.caller((caller) => caller && caller.target === 'web');
-
 module.exports = (api) => {
-  const babelPresetEnvOptions = {
-    // debug: true, // <-- Enable if you want to debug what babel is doing
-    // useBuiltIns: 'usage',
-    // corejs: 3.39,
-    // modules: false
-  };
-  if (!isTargetWeb(api)) {
-    // change babel compile target for node
-    babelPresetEnvOptions.targets = { node: 'current' };
-  }
+  api.cache(true);
 
   return {
     presets: [
-      ['@babel/preset-react', {
-        'runtime': 'automatic' // Starting from Babel 8, "automatic" will be the default runtime
-      }],
-      '@babel/preset-typescript',
-      // [
-      //   '@babel/preset-env',
-      //   babelPresetEnvOptions
-      // ]
+      "@babel/preset-typescript",
     ],
-    // overrides: [
-    //   {
-    //     test: /\.tsx$/,
-    //     presets: [
-    //       ['@babel/preset-react', { runtime: 'automatic' }],
-    //       '@babel/preset-typescript'
-    //     ]
-    //   },
-    //   {
-    //     test: /\.ts$/,
-    //     presets: [
-    //       '@babel/preset-typescript'
-    //     ]
-    //   }
-    // ],
-    env: {
-      test: {
+    overrides: [
+      {
+        test: /\.tsx$/,
         presets: [
-          [
-            '@babel/preset-env',
-            // {
-            //   useBuiltIns: 'usage',
-            //   corejs: 3.39
-            // }
-          ]
+          '@babel/preset-react'
         ]
-      },
+      }
+    ],
+    env: {
       production: {
         plugins: [
           ['react-remove-properties', { 'properties': [ 'data-test-id' ] }]
