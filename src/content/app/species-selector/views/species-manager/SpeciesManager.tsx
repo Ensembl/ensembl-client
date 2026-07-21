d21ac74674099717d2d0760f2e3bdf8c20aa9e79
Lucy B <112630023+luanbroo@users.noreply.github.com>
2026-07-21 15:27:04 +0100

    Delete genomes from genome manager (#1319)
    In order to make it easy to delete genomes from the genome manager:
    
    - Show checkboxes in the first column of genomes table for genome selection
      - There is a master checkbox in column heading for selecting all genomes at once
    - There is a "Remove genomes" button in top right that is activated when checkboxes for one or more genomes
      are ticked off, and deletes these genomes.
    
    ---------
    
    Co-authored-by: Andrey Azov <andrey@ebi.ac.uk>

--- src/content/app/species-selector/views/species-manager/SpeciesManager.tsx
+++ src/content/app/species-selector/views/species-manager/SpeciesManager.tsx
@@ -14,42 +14,71 @@
  * limitations under the License.
  */
 
+import { useState } from 'react';
 import { useNavigate } from 'react-router';
-import type { FormEvent } from 'react';
+import type { ChangeEvent } from 'react';
+
+import classNames from 'classnames';
 
 import * as urlFor from 'src/shared/helpers/urlHelper';
 
+import { useAppDispatch } from 'src/store';
+
 import useFilteredGenomes from './useFilteredGenomes';
+
+import { deleteSpeciesAndSave } from 'src/content/app/species-selector/state/species-selector-general-slice/speciesSelectorGeneralSlice';
 
 import ModalView from 'src/shared/components/modal-view/ModalView';
 
 import SelectedGenomesTable from './selected-genomes-table/SelectedGenomesTable';
 import SpeciesLozengeDisplaySelector from 'src/content/app/species-selector/components/species-lozenge-display-selector/SpeciesLozengeDisplaySelector';
 import AddButton from 'src/shared/components/add-button/AddButton';
+import { PrimaryButton } from 'src/shared/components/button/Button';
 import ShadedInput from 'src/shared/components/input/ShadedInput';
+
+import TrashcanIcon from 'static/icons/icon_delete.svg';
 
 import styles from './SpeciesManager.module.css';
 
 const SpeciesManager = () => {
   const navigate = useNavigate();
+  const dispatch = useAppDispatch();
   const { allSelectedGenomes, filteredGenomes, onFilterChange } =
     useFilteredGenomes();
+  const [selectedGenomeIds, setSelectedGenomeIds] = useState<string[]>([]);
+  const availableGenomeIds = new Set(
+    allSelectedGenomes.map((genome) => genome.genome_id)
+  );
+  const selectedAvailableGenomeIds = selectedGenomeIds.filter((genomeId) =>
+    availableGenomeIds.has(genomeId)
+  );
 
   const onClose = () => {
     navigate(-1);
   };
 
+  const removeSelectedGenomes = () => {
+    for (const genomeId of selectedAvailableGenomeIds) {
+      dispatch(deleteSpeciesAndSave(genomeId));
+    }
+    setSelectedGenomeIds([]);
+  };
+
   return (
     <ModalView onClose={onClose}>
       <div className={styles.container}>
-        <MainContentTop onFilterChange={onFilterChange} />
-        <div className={styles.genomesTableContainerOuter}>
-          <div className={styles.genomesTableContainerInner}>
-            <SelectedGenomesTable
-              allSelectedGenomes={allSelectedGenomes}
-              filteredGenomes={filteredGenomes}
-            />
-          </div>
+        <MainContentTop
+          onFilterChange={onFilterChange}
+          hasSelectedGenomes={!!selectedAvailableGenomeIds.length}
+          onRemoveSelectedGenomes={removeSelectedGenomes}
+        />
+        <div className={styles.genomesTableContainer}>
+          <SelectedGenomesTable
+            allSelectedGenomes={allSelectedGenomes}
+            filteredGenomes={filteredGenomes}
+            selectedGenomeIds={selectedAvailableGenomeIds}
+            onSelectedGenomeIdsChange={setSelectedGenomeIds}
+          />
         </div>
       </div>
     </ModalView>
@@ -57,7 +86,9 @@ const SpeciesManager = () => {
 };
 
 const MainContentTop = (props: {
-  onFilterChange: (event: FormEvent<HTMLInputElement>) => void;
+  onFilterChange: (event: ChangeEvent<HTMLInputElement>) => void;
+  hasSelectedGenomes: boolean;
+  onRemoveSelectedGenomes: () => void;
 }) => {
   const navigate = useNavigate();
 
@@ -68,19 +99,32 @@ const MainContentTop = (props: {
   return (
     <div className={styles.mainContentTop}>
       <SpeciesLozengeDisplaySelector />
-      <div className={styles.mainContentTopRight}>
-        <label className={styles.filterFieldWrapper}>
-          <ShadedInput
-            onChange={props.onFilterChange}
-            placeholder="Find in list"
-          />
-        </label>
-        <AddButton
-          onClick={onAddSpeciesClick}
-          className={styles.addSpeciesButton}
+      <ShadedInput
+        className={styles.filterField}
+        onChange={props.onFilterChange}
+        placeholder="Find in list"
+        aria-label="Find in list"
+      />
+      <AddButton
+        onClick={onAddSpeciesClick}
+        className={styles.addSpeciesButton}
+      >
+        Add a genome
+      </AddButton>
+      <div className={styles.removeGenomesButtonWrapper}>
+        <TrashcanIcon
+          className={classNames(styles.removeGenomesIcon, {
+            [styles.removeGenomesIconDisabled]: !props.hasSelectedGenomes
+          })}
+        />
+        <PrimaryButton
+          type="button"
+          className={styles.removeGenomesButton}
+          disabled={!props.hasSelectedGenomes}
+          onClick={props.onRemoveSelectedGenomes}
         >
-          Add a genome
-        </AddButton>
+          Remove genomes
+        </PrimaryButton>
       </div>
     </div>
   );
