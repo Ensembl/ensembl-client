52d9782d3ad9e02f8fd799f92ed9179dac8e911b
Andrey Azov <andrey@ebi.ac.uk>
2026-07-28 08:49:10 +0100

    Update Panel styles

--- stories/shared-components/panel/Panel.stories.tsx
+++ stories/shared-components/panel/Panel.stories.tsx
@@ -14,7 +14,6 @@
  * limitations under the License.
  */
 
-import { useState } from 'react';
 import { faker } from '@faker-js/faker';
 
 import {
@@ -22,51 +21,13 @@ import {
   PanelHeader,
   PanelBody
 } from 'src/shared/components/panel/Panel';
-import Tabs, { Tab } from 'src/shared/components/tabs/Tabs';
 
 import styles from './Panel.stories.module.css';
-
-type DefaultArgs = {
-  onClose: (...args: any) => void;
-  onTabChange: (...args: any) => void;
-};
-
-const tabsData: Tab[] = [
-  { title: 'Proteins' },
-  { title: 'Variants' },
-  { title: 'Phenotypes' },
-  { title: 'Gene expression' },
-  { title: 'Gene ontology', isDisabled: true },
-  { title: 'Gene pathways' }
-];
-
-const tabClassNames = {
-  selected: styles.selectedTab,
-  default: styles.defaultTab
-};
-
-const TabWrapper = (args: DefaultArgs) => {
-  const [selectedTab, setSelectedTab] = useState('Proteins');
-
-  const onTabChange = (tab: string) => {
-    setSelectedTab(tab);
-    args.onTabChange(tab);
-  };
-
-  return (
-    <Tabs
-      tabs={tabsData}
-      selectedTab={selectedTab}
-      classNames={tabClassNames}
-      onTabChange={onTabChange}
-    />
-  );
-};
 
 export const DefaultPanelStory = () => (
   <div className={styles.fullPageWrapper}>
     <Panel>
-      <PanelHeader>Default Panel</PanelHeader>
+      <PanelHeader>Proteins</PanelHeader>
       <PanelBody>
         <div>Panel Content</div>
       </PanelBody>
@@ -76,27 +37,10 @@ export const DefaultPanelStory = () => (
 
 DefaultPanelStory.storyName = 'default';
 
-export const PanelWithTabsStory = (args: DefaultArgs) => (
+export const PanelWithLongContentStory = () => (
   <div className={styles.fullPageWrapper}>
     <Panel>
-      <PanelHeader>
-        <TabWrapper {...args} />
-      </PanelHeader>
-      <PanelBody>
-        <div>Panel Content</div>
-      </PanelBody>
-    </Panel>
-  </div>
-);
-
-PanelWithTabsStory.storyName = 'with tabs';
-
-export const PanelWithLongContentStory = (args: DefaultArgs) => (
-  <div className={styles.fullPageWrapper}>
-    <Panel>
-      <PanelHeader>
-        <TabWrapper {...args} />
-      </PanelHeader>
+      <PanelHeader>Long content</PanelHeader>
       <PanelBody>
         <div className={styles.preWrap}>{faker.lorem.paragraphs(100)}</div>
       </PanelBody>
@@ -107,9 +51,5 @@ export const PanelWithLongContentStory = (args: DefaultArgs) => (
 PanelWithLongContentStory.storyName = 'long content';
 
 export default {
-  title: 'Components/Shared Components/Panel',
-  argTypes: {
-    onClose: { action: 'closed' },
-    onTabChange: { action: 'tab changed' }
-  }
+  title: 'Components/Shared Components/Panel'
 };
