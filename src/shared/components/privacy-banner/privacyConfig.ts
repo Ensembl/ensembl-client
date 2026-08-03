d8f839922fa60d58e78f3fd1319ee01c4ad2009a
Andrey Azov <andrey@ebi.ac.uk>
2026-08-03 16:59:18 +0100

    Update link to privacy policy

--- src/shared/components/privacy-banner/privacyConfig.ts
+++ src/shared/components/privacy-banner/privacyConfig.ts
@@ -17,6 +17,6 @@
 export default {
   name: 'ensembl_privacy_policy',
   version: '2.0.0',
-  policyUrl: 'https://www.ebi.ac.uk/data-protection/ensembl/privacy-notice',
+  policyUrl: '/about/articles/privacy-and-cookies',
   termsUrl: 'https://www.ebi.ac.uk/about/terms-of-use'
 };
